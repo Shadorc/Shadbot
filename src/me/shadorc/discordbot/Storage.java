ce5f6151bd854dc1aaa63e3e70c2e2c3751f1b5a
David <bi-crosseur@hotmail.fr>
2017-07-20 16:22:28 +0200

    Add API_KEYS_FILE

--- src/me/shadorc/discordbot/Storage.java
+++ src/me/shadorc/discordbot/Storage.java
@@ -12,7 +12,8 @@ import org.json.JSONObject;
 
 public class Storage {
 
-	private static final File FILE = new File("data.json");
+	private static final File API_KEYS_FILE = new File("api_keys.json");
+	private static final File DATA_FILE = new File("data.json");
 
 	public enum API_KEYS {
 		GIPHY_API_KEY, 
@@ -21,16 +22,17 @@ public class Storage {
 		TWITTER_API_KEY, 
 		TWITTER_API_SECRET, 
 		TWITTER_TOKEN, 
-		TWITTER_TOKEN_SECRET
+		TWITTER_TOKEN_SECRET,
+		CLEVERBOT_API_KEY
 	}
 
 	private static void init() {
-		if(!FILE.exists()) {
+		if(!DATA_FILE.exists()) {
 			try {
-				FILE.createNewFile();
+				DATA_FILE.createNewFile();
 				FileWriter writer = null;
 				try {
-					writer = new FileWriter(FILE);
+					writer = new FileWriter(DATA_FILE);
 					writer.write("{}");
 					writer.flush();
 				} catch (IOException e) {
@@ -51,14 +53,14 @@ public class Storage {
 	}
 
 	public static void store(Object key, Object value) {
-		if(!FILE.exists()) Storage.init();
+		if(!DATA_FILE.exists()) Storage.init();
 
 		FileWriter writer = null;
 		try {
-			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(FILE.getPath())), StandardCharsets.UTF_8));
+			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(DATA_FILE.getPath())), StandardCharsets.UTF_8));
 			obj.put(key.toString(), value.toString());
 
-			writer = new FileWriter(FILE);
+			writer = new FileWriter(DATA_FILE);
 			writer.write(obj.toString());
 			writer.flush();
 		} catch (IOException e) {
@@ -74,10 +76,10 @@ public class Storage {
 	}
 
 	public static int get(String key) {
-		if(!FILE.exists()) Storage.init();
+		if(!DATA_FILE.exists()) Storage.init();
 
 		try {
-			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(FILE.getPath())), StandardCharsets.UTF_8));
+			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(DATA_FILE.getPath())), StandardCharsets.UTF_8));
 			if(obj.has(key)) {
 				return obj.getInt(key);
 			}
@@ -90,7 +92,7 @@ public class Storage {
 
 	public static String get(API_KEYS key) {
 		try {
-			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(FILE.getPath())), StandardCharsets.UTF_8));
+			JSONObject obj = new JSONObject(new String(Files.readAllBytes(Paths.get(API_KEYS_FILE.getPath())), StandardCharsets.UTF_8));
 			return obj.getString(key.toString());
 		} catch (JSONException | IOException e) {
 			System.err.println("Error while accessing to API keys storage.");
