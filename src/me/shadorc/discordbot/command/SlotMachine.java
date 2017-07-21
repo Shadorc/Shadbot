7d029f89a35e8841685d673c0add5857bbb07b18
David <bi-crosseur@hotmail.fr>
2017-07-21 19:10:57 +0200

    Increase slot machine winning chance

--- src/me/shadorc/discordbot/command/SlotMachine.java
+++ src/me/shadorc/discordbot/command/SlotMachine.java
@@ -1,26 +1,20 @@
 package me.shadorc.discordbot.command;
 
-import me.shadorc.discordbot.Bot;
+import me.shadorc.discordbot.utility.BotUtils;
 import me.shadorc.discordbot.utility.Utils;
 import sx.blah.discord.handle.obj.IChannel;
 
 public class SlotMachine {
 
 	private enum SlotOptions {
-		GRAPES,
-		APPLE,
 		CHERRIES,
 		BELL,
-		MELON,
 		GIFT
 	}
 
 	private static final SlotOptions[] slotsArray = new SlotOptions[] {
-			SlotOptions.GRAPES, SlotOptions.GRAPES, SlotOptions.GRAPES, SlotOptions.GRAPES, SlotOptions.GRAPES, SlotOptions.GRAPES,
-			SlotOptions.APPLE, SlotOptions.APPLE, SlotOptions.APPLE, SlotOptions.APPLE, SlotOptions.APPLE, SlotOptions.APPLE,
-			SlotOptions.CHERRIES, SlotOptions.CHERRIES, SlotOptions.CHERRIES, SlotOptions.CHERRIES, SlotOptions.CHERRIES,
-			SlotOptions.BELL, SlotOptions.BELL, SlotOptions.BELL, SlotOptions.BELL,
-			SlotOptions.MELON, SlotOptions.MELON, SlotOptions.MELON, 
+			SlotOptions.CHERRIES, SlotOptions.CHERRIES, SlotOptions.CHERRIES, SlotOptions.CHERRIES,
+			SlotOptions.BELL, SlotOptions.BELL, SlotOptions.BELL,
 			SlotOptions.GIFT};
 
 	public static void play(String author, IChannel channel) {
@@ -28,31 +22,22 @@ public class SlotMachine {
 		SlotOptions slot2 = slotsArray[Utils.rand(slotsArray.length)];
 		SlotOptions slot3 = slotsArray[Utils.rand(slotsArray.length)];
 
-		int gain = -10;
+		int gain = -5;
 
-		if(slot1 == SlotOptions.GRAPES && slot2 == SlotOptions.GRAPES && slot3 == SlotOptions.GRAPES) {
-			gain = 3;
-		} 
-		else if(slot1 == SlotOptions.APPLE && slot2 == SlotOptions.APPLE && slot3 == SlotOptions.APPLE) { 
-			gain = 6;
-		} 
-		else if(slot1 == SlotOptions.CHERRIES && slot2 == SlotOptions.CHERRIES && slot3 == SlotOptions.CHERRIES) { 
-			gain = 10;
+		if(slot1 == SlotOptions.CHERRIES && slot2 == SlotOptions.CHERRIES && slot3 == SlotOptions.CHERRIES) { 
+			gain = 50;
 		} 
 		else if(slot1 == SlotOptions.BELL && slot2 == SlotOptions.BELL && slot3 == SlotOptions.BELL) { 
-			gain = 20;
-		} 
-		else if(slot1 == SlotOptions.MELON && slot2 == SlotOptions.MELON && slot3 == SlotOptions.MELON) { 
-			gain = 40;
+			gain = 100;
 		} 
 		else if(slot1 == SlotOptions.GIFT && slot2 == SlotOptions.GIFT && slot3 == SlotOptions.GIFT) { 
-			gain = 100;
+			gain = 5000;
 		} 
 		Utils.gain(author, gain);
 
 		StringBuilder message = new StringBuilder();
 		message.append(":" + slot1.toString().toLowerCase() + ": :" + slot2.toString().toLowerCase() + ": :" + slot3.toString().toLowerCase() + ":");
-		message.append("Vous avez "+ (gain > 0 ? "gagné" : "perdu") + gain + " coins !");
-		Bot.sendMessage(message.toString(), channel);
+		message.append("\nVous avez "+ (gain > 0 ? "gagné" : "perdu") + " " + Math.abs(gain) + " coins !");
+		BotUtils.sendMessage(message.toString(), channel);
 	} 
 }
