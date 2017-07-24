b430c1dd7600d363b4fc61cde514a6bce384a32a
David <bi-crosseur@hotmail.fr>
2017-07-25 01:11:20 +0200

    Reduce coins gained with russian roulette

--- src/me/shadorc/discordbot/command/game/RussianRouletteCmd.java
+++ src/me/shadorc/discordbot/command/game/RussianRouletteCmd.java
@@ -18,8 +18,8 @@ public class RussianRouletteCmd extends Command {
 			BotUtils.sendMessage(":game_die: Une goutte de sueur coule sur votre front, vous pressez la détente... **PAN** ... Désolé, vous êtes mort, vous perdez tous vos gains.", context.getChannel());
 			Storage.store(context.getGuild(), context.getAuthor().getLongID(), 0);
 		} else {
-			BotUtils.sendMessage(":game_die: Une goutte de sueur coule sur votre front, vous pressez la détente... **click** ... Pfiou, vous êtes toujours en vie, vous remportez 50 coins !", context.getChannel());
-			Utils.gain(context.getGuild(), context.getAuthor().getLongID(), 50);
+			BotUtils.sendMessage(":game_die: Une goutte de sueur coule sur votre front, vous pressez la détente... **click** ... Pfiou, vous êtes toujours en vie, vous remportez 25 coins !", context.getChannel());
+			Utils.gain(context.getGuild(), context.getAuthor().getLongID(), 25);
 		}
 	}
 
