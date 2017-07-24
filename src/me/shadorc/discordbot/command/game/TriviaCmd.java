583a9ea597e4227dc054fe7f461a65a43287ae31
David <bi-crosseur@hotmail.fr>
2017-07-24 04:38:36 +0200

    TriviaCmd is now multi guild proof

--- src/me/shadorc/discordbot/command/game/TriviaCmd.java
+++ src/me/shadorc/discordbot/command/game/TriviaCmd.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.Timer;
 
@@ -17,21 +19,13 @@ import me.shadorc.discordbot.utility.Log;
 import me.shadorc.discordbot.utility.Utils;
 import me.shadorc.infonet.Infonet;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
 public class TriviaCmd extends Command {
 
-	private static ArrayList <IUser> alreadyAnswered = new ArrayList <> ();
-	public static boolean QUIZZ_STARTED = false;
-
-	private static String CORRECT_ANSWER;
-	private static IChannel CHANNEL;
-
-	private final static Timer timer = new Timer(30*1000, e -> {
-		BotUtils.sendMessage("Temps écoulé, la bonne réponse était " + CORRECT_ANSWER, CHANNEL);
-		TriviaCmd.stop();
-	});
+	public static final Map<IGuild, GuildTriviaManager> GUILDS = new HashMap<>();
 
 	public TriviaCmd() {
 		super(false, "trivia", "quizz", "question");
@@ -40,75 +34,97 @@ public class TriviaCmd extends Command {
 	@Override
 	public void execute(Context context) {
 		try {
-			this.start(context.getChannel());
+			GUILDS.put(context.getGuild(), new GuildTriviaManager(context.getChannel()));
+			GUILDS.get(context.getGuild()).start();
 		} catch (IOException e) {
 			Log.error("Une erreur est survenue lors de la récupération de la question.", e, context.getChannel());
 		}
 	}
 
-	private void start(IChannel channel) throws MalformedURLException, IOException {
-		//Trivia API doc : https://opentdb.com/api_config.php
-		String json = Infonet.getHTML(new URL("https://opentdb.com/api.php?amount=1"));
-		JSONArray arrayResults = new JSONObject(json).getJSONArray("results");
-		JSONObject result = arrayResults.getJSONObject(0);
+	public static GuildTriviaManager getGuildTriviaManager(IGuild guild) {
+		return GUILDS.get(guild);
+	}
 
-		String category = result.getString("category");
-		String type = result.getString("type");
-		String difficulty = result.getString("difficulty");
-		String question = result.getString("question");
-		String correct_answer = result.getString("correct_answer");
+	public class GuildTriviaManager {
 
-		StringBuilder quizzMessage = new StringBuilder();
+		private IChannel channel;
+		private ArrayList <IUser> alreadyAnswered;
+		private boolean isStarted;
+		private String correctAnswer;
+		private Timer timer;
 
-		quizzMessage.append("Catégorie : " + category
-				+ ", type : " + type
-				+ ", difficulté : " + difficulty
-				+ "\nQuestion : **" + Utils.convertToUTF8(question) + "**\n");
+		private GuildTriviaManager(IChannel channel) {
+			this.channel = channel;
+			this.alreadyAnswered = new ArrayList<>();
+			this.isStarted = false;
+			this.timer = new Timer(30*1000, e -> {
+				BotUtils.sendMessage("Temps écoulé, la bonne réponse était " + correctAnswer, channel);
+				this.stop();
+			});
+		}
 
-		if(type.equals("multiple")) {
-			JSONArray incorrect_answers = result.getJSONArray("incorrect_answers");
+		private void start() throws MalformedURLException, IOException {
+			//Trivia API doc : https://opentdb.com/api_config.php
+			String json = Infonet.getHTML(new URL("https://opentdb.com/api.php?amount=1"));
+			JSONArray arrayResults = new JSONObject(json).getJSONArray("results");
+			JSONObject result = arrayResults.getJSONObject(0);
 
-			//Place the correct answer randomly in the list
-			int index = Utils.rand(incorrect_answers.length());
-			for(int i = 0; i < incorrect_answers.length(); i++) {
-				if(i == index) {
-					quizzMessage.append("\t- " + Utils.convertToUTF8(correct_answer) + "\n");
+			String category = result.getString("category");
+			String type = result.getString("type");
+			String difficulty = result.getString("difficulty");
+			String question = result.getString("question");
+			String correct_answer = result.getString("correct_answer");
+
+			StringBuilder quizzMessage = new StringBuilder();
+
+			quizzMessage.append("Catégorie : " + category
+					+ ", type : " + type
+					+ ", difficulté : " + difficulty
+					+ "\nQuestion : **" + Utils.convertToUTF8(question) + "**\n");
+
+			if(type.equals("multiple")) {
+				JSONArray incorrect_answers = result.getJSONArray("incorrect_answers");
+
+				//Place the correct answer randomly in the list
+				int index = Utils.rand(incorrect_answers.length());
+				for(int i = 0; i < incorrect_answers.length(); i++) {
+					if(i == index) {
+						quizzMessage.append("\t- " + Utils.convertToUTF8(correct_answer) + "\n");
+					}
+					quizzMessage.append("\t- " + Utils.convertToUTF8((String) incorrect_answers.get(i)) + "\n");
 				}
-				quizzMessage.append("\t- " + Utils.convertToUTF8((String) incorrect_answers.get(i)) + "\n");
+			}
+
+			BotUtils.sendMessage(quizzMessage.toString(), channel);
+
+			this.correctAnswer = Utils.convertToUTF8(correct_answer);
+			this.isStarted = true;
+			this.timer.start();
+		}
+
+		public void checkAnswer(IMessage message) {
+			if(alreadyAnswered.contains(message.getAuthor())) {
+				BotUtils.sendMessage("Désolé " + message.getAuthor().getName() + ", tu ne peux plus répondre après avoir donné une mauvaise réponse.", message.getChannel());
+			}
+			else if(Utils.getLevenshteinDistance(message.getContent().toLowerCase(), this.correctAnswer.toLowerCase()) < 2) {
+				BotUtils.sendMessage("Bonne réponse " + message.getAuthor().getName() + " ! Tu gagnes 50 coins.", channel);
+				Utils.gain(message.getGuild(), message.getAuthor().getLongID(), 10);
+				this.stop();
+			}
+			else {
+				BotUtils.sendMessage("Mauvaise réponse.", channel);
+				alreadyAnswered.add(message.getAuthor());
 			}
 		}
 
-		BotUtils.sendMessage(quizzMessage.toString(), channel);
-
-		CORRECT_ANSWER = Utils.convertToUTF8(correct_answer);
-		CHANNEL = channel;
-		this.start();
-	}
-
-	public static void checkAnswer(IMessage message) {
-		if(alreadyAnswered.contains(message.getAuthor())) {
-			BotUtils.sendMessage("Désolé " + message.getAuthor().getName() + ", tu ne peux plus répondre après avoir donné une mauvaise réponse.", message.getChannel());
+		private void stop() {
+			this.alreadyAnswered.clear();
+			this.isStarted = false;
+			this.timer.stop();
 		}
-		else if(Utils.getLevenshteinDistance(message.getContent().toLowerCase(), TriviaCmd.CORRECT_ANSWER.toLowerCase()) < 2) {
-			BotUtils.sendMessage("Bonne réponse " + message.getAuthor().getName() + " ! Tu gagnes 10 coins.", CHANNEL);
-			Utils.gain(message.getGuild(), message.getAuthor().getLongID(), 10);
-			TriviaCmd.stop();
-		}
-		else {
-			BotUtils.sendMessage("Mauvaise réponse.", CHANNEL);
-			alreadyAnswered.add(message.getAuthor());
+
+		public boolean isStarted() {
+			return isStarted;
 		}
 	}
-
-	private void start() {
-		QUIZZ_STARTED = true;
-		timer.start();
-	}
-
-	private static void stop() {
-		QUIZZ_STARTED = false;
-		alreadyAnswered.clear();
-		timer.stop();
-	}
-
 }
--- src/me/shadorc/discordbot/listener/EventListener.java
+++ src/me/shadorc/discordbot/listener/EventListener.java
@@ -1,12 +1,12 @@
 package me.shadorc.discordbot.listener;
 
 import me.shadorc.discordbot.command.game.TriviaCmd;
+import me.shadorc.discordbot.command.game.TriviaCmd.GuildTriviaManager;
 import me.shadorc.discordbot.utility.BotUtils;
 import me.shadorc.discordbot.utility.Log;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
-import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 
@@ -22,22 +22,19 @@ public class EventListener {
 
 	@EventSubscriber
 	public void onMessageReceivedEvent(MessageReceivedEvent event) {
-		IChannel channel = event.getChannel();
 		IMessage message = event.getMessage();
-		IGuild guild = event.getGuild();
 
 		//Check if the bot doesn't answer to itself or to another bot
 		if(event.getAuthor().isBot() || event.getAuthor().getStringID().equals(event.getClient().getOurUser().getStringID())) {
 			return;
 		}
 
-		if(guild.getStringID().equals("331152695006330880") || channel.getStringID().equals("275615361997471745")) {
-			if(TriviaCmd.QUIZZ_STARTED) {
-				TriviaCmd.checkAnswer(message);
-			}
-			else if(message.getContent().startsWith("/")) {
-				BotUtils.executeCommand(event);
-			}
+		GuildTriviaManager gtm = TriviaCmd.getGuildTriviaManager(event.getGuild());
+		if(gtm != null && gtm.isStarted()) {
+			gtm.checkAnswer(message);
+		}
+		else if(message.getContent().startsWith("/")) {
+			BotUtils.executeCommand(event);
 		}
 	}
 }
