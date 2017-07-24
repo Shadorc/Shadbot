f031c6fb81f705bfc70a2bf9cec4a7693c925324
David <bi-crosseur@hotmail.fr>
2017-07-25 00:11:11 +0200

    Logout on exit

--- src/me/shadorc/discordbot/Main.java
+++ src/me/shadorc/discordbot/Main.java
@@ -6,6 +6,7 @@ import me.shadorc.discordbot.Storage.ApiKeys;
 import me.shadorc.discordbot.listener.ChannelListener;
 import me.shadorc.discordbot.listener.EventListener;
 import me.shadorc.discordbot.music.GuildMusicManager;
+import me.shadorc.discordbot.utility.Log;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 
@@ -23,6 +24,15 @@ public class Main {
 
 		AudioSourceManagers.registerRemoteSources(GuildMusicManager.PLAYER_MANAGER);
 		AudioSourceManagers.registerLocalSource(GuildMusicManager.PLAYER_MANAGER);
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				client.logout();
+				Log.info("Shadbot has been disconnected from all guilds.");
+				System.exit(0);
+			}
+		});
 	}
 
 	public static IDiscordClient getClient() {
