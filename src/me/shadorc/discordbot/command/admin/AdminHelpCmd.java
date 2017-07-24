7600397956781d5728d0eba4b2140707df5a851f
David <bi-crosseur@hotmail.fr>
2017-07-24 04:10:17 +0200

    Add AdminHelpCmd

---
+++ src/me/shadorc/discordbot/command/admin/AdminHelpCmd.java
@@ -0,0 +1,32 @@
+package me.shadorc.discordbot.command.admin;
+
+import java.awt.Color;
+
+import me.shadorc.discordbot.Command;
+import me.shadorc.discordbot.Context;
+import me.shadorc.discordbot.utility.BotUtils;
+import sx.blah.discord.util.EmbedBuilder;
+
+public class AdminHelpCmd extends Command {
+
+	public AdminHelpCmd() {
+		super(true, "admin_help");
+	}
+
+	@Override
+	public void execute(Context context) {
+		//EmbedBuilder doc : https://discord4j.readthedocs.io/en/latest/Making-embedded-content-using-EmbedBuilder/
+		EmbedBuilder builder = new EmbedBuilder();
+
+		builder.withAuthorName("Shadbot Admin Aide");
+		builder.withAuthorIcon(context.getClient().getOurUser().getAvatarURL());
+		builder.withColor(new Color(170, 196, 222));
+		builder.withDesc("Aide pour les commandes administrateurs. Pour plus d'informations, utilisez /help <commande>.");
+		builder.appendField("Commandes :",
+				"`/quit`"
+						+ "\n`/allow_channel <channel | all>`", false);
+
+		BotUtils.sendEmbed(builder.build(), context.getChannel());		
+	}
+
+}
