1cce50f5b3ad5c225775b9e7d6a08d311660d48d
David <bi-crosseur@hotmail.fr>
2017-08-02 01:31:14 +0200

    Remove ShutdownHook because he throws exception

--- src/me/shadorc/discordbot/Main.java
+++ src/me/shadorc/discordbot/Main.java
@@ -6,7 +6,6 @@ import me.shadorc.discordbot.Storage.ApiKeys;
 import me.shadorc.discordbot.listener.ChannelListener;
 import me.shadorc.discordbot.listener.EventListener;
 import me.shadorc.discordbot.music.GuildMusicManager;
-import me.shadorc.discordbot.utility.Log;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 
@@ -22,13 +21,5 @@ public class Main {
 
 		AudioSourceManagers.registerRemoteSources(GuildMusicManager.PLAYER_MANAGER);
 		AudioSourceManagers.registerLocalSource(GuildMusicManager.PLAYER_MANAGER);
-
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				client.logout();
-				Log.info("Shadbot has been disconnected from all guilds.");
-			}
-		});
 	}
 }
