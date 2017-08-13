f7099c9eafdeb18b18dd9b0f0e40aa5a03c88678
David <bi-crosseur@hotmail.fr>
2017-08-13 14:43:14 +0200

    Fix Joke unescaping

--- src/me/shadorc/discordbot/command/french/JokeCmd.java
+++ src/me/shadorc/discordbot/command/french/JokeCmd.java
@@ -5,8 +5,11 @@ import java.net.URL;
 import java.util.List;
 
 import me.shadorc.discordbot.Config;
+import me.shadorc.discordbot.Emoji;
 import me.shadorc.discordbot.Log;
 import me.shadorc.discordbot.MissingArgumentException;
+import me.shadorc.discordbot.RateLimiter;
+import me.shadorc.discordbot.Shadbot;
 import me.shadorc.discordbot.command.Command;
 import me.shadorc.discordbot.command.Context;
 import me.shadorc.discordbot.utils.BotUtils;
@@ -27,7 +30,7 @@ public class JokeCmd extends Command {
 			String htmlPage = HtmlUtils.getHTML(new URL("https://www.blague-drole.net/blagues-" + MathUtils.rand(1, 10) + ".html?tri=top"));
 			List<String> jokesList = HtmlUtils.getAllSubstring(htmlPage, " \"description\": \"", "</script>");
 			String joke = jokesList.get(MathUtils.rand(jokesList.size()));
-			joke = joke.substring(0, joke.lastIndexOf("\"")).trim();
+			joke = joke.substring(0, joke.lastIndexOf("\"")).replace("&amp;", "&").trim();
 			BotUtils.sendMessage("```" + StringUtils.convertHtmlToUTF8(joke).replace("\n\n", "\n") + "```", context.getChannel());
 		} catch (IOException e) {
 			Log.error("An error occured while getting a joke.", e, context.getChannel());
@@ -38,7 +41,7 @@ public class JokeCmd extends Command {
 	public void showHelp(Context context) {
 		EmbedBuilder builder = new EmbedBuilder()
 				.withAuthorName("Help for " + this.getNames()[0] + " command")
-				.withAuthorIcon(context.getClient().getOurUser().getAvatarURL())
+				.withAuthorIcon(Shadbot.getClient().getOurUser().getAvatarURL())
 				.withColor(Config.BOT_COLOR)
 				.appendDescription("**Show a random joke from blague-drole.net.**");
 		BotUtils.sendEmbed(builder.build(), context.getChannel());
