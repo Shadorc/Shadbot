4a46ac565dad8744bded6d42ec9ef9b51b542d9b
David <bi-crosseur@hotmail.fr>
2017-08-15 18:33:02 +0200

    Bump version

--- src/me/shadorc/discordbot/Config.java
+++ src/me/shadorc/discordbot/Config.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class Config {
 
-	public static final Version VERSION = new Version(1, 2, 4, false);
+	public static final Version VERSION = new Version(1, 2, 5, false);
 
 	public static final long DEBUG_CHANNEL_ID = 342074301840752640L;
 	public static final long BUGS_REPORT_CHANNEL_ID = 346311941829951489L;
