482d683c2981ae589dd500e6ecb6171094272224
David <bi-crosseur@hotmail.fr>
2017-08-24 03:12:57 +0200

    Improve CounterStrikeCmd

--- src/me/shadorc/discordbot/command/gamestats/CounterStrikeCmd.java
+++ src/me/shadorc/discordbot/command/gamestats/CounterStrikeCmd.java
@@ -8,8 +8,6 @@ import java.time.temporal.ChronoUnit;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.HttpStatusException;
-import org.jsoup.nodes.Document;
 
 import me.shadorc.discordbot.Config;
 import me.shadorc.discordbot.Emoji;
@@ -22,7 +20,6 @@ import me.shadorc.discordbot.command.AbstractCommand;
 import me.shadorc.discordbot.command.Context;
 import me.shadorc.discordbot.utils.BotUtils;
 import me.shadorc.discordbot.utils.LogUtils;
-import me.shadorc.discordbot.utils.NetUtils;
 import me.shadorc.discordbot.utils.StringUtils;
 import sx.blah.discord.util.EmbedBuilder;
 
@@ -49,39 +46,39 @@ public class CounterStrikeCmd extends AbstractCommand {
 		}
 
 		try {
-			String steamids;
-			if(StringUtils.isInteger(context.getArg())) {
-				steamids = context.getArg();
+			String steamid = null;
+			if(context.getArg().contains("/")) {
+				String[] splittedUrl = context.getArg().split("/");
+				steamid = splittedUrl[splittedUrl.length-1].trim();
+			} else if(StringUtils.isLong(context.getArg())) {
+				steamid = context.getArg().trim();
 			} else {
-				Document userPage = NetUtils.getDoc("https://steamcommunity.com/id/" + URLEncoder.encode(context.getArg(), "UTF-8") + "/");
-
-				if(!userPage.getElementsByClass("error_ctn").isEmpty()) {
-					BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " This user doesn't exist.", context.getChannel());
-					return;
+				JSONObject mainObj = new JSONObject(IOUtils.toString(new URL("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?"
+						+ "key=" + Storage.getApiKey(ApiKeys.STEAM_API_KEY)
+						+ "&vanityurl=" + URLEncoder.encode(context.getArg(), "UTF-8")), "UTF-8"));
+				JSONObject responseObj = mainObj.getJSONObject("response");
+				if(responseObj.has("steamid")) {
+					steamid = responseObj.getString("steamid");
 				}
-
-				String html = userPage.getElementsByClass("responsive_page_template_content").html();
-				JSONObject userObj = new JSONObject(html.substring(html.indexOf('{'), html.indexOf('}') + 1));
-				steamids = userObj.getString("steamid");
 			}
 
 			JSONObject mainStatsObj = new JSONObject(IOUtils.toString(new URL(
 					"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?"
 							+ "appid=730"
 							+ "&key=" + Storage.getApiKey(ApiKeys.STEAM_API_KEY)
-							+ "&steamid=" + steamids), "UTF-8"));
+							+ "&steamid=" + steamid), "UTF-8"));
 
 			JSONObject mainUserObj = new JSONObject(IOUtils.toString(new URL(
 					"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?"
 							+ "key=" + Storage.getApiKey(ApiKeys.STEAM_API_KEY)
-							+ "&steamids=" + steamids), "UTF-8"));
+							+ "&steamids=" + steamid), "UTF-8"));
 
 			JSONArray statsArray = mainStatsObj.getJSONObject("playerstats").getJSONArray("stats");
 			JSONObject userObj = mainUserObj.getJSONObject("response").getJSONArray("players").getJSONObject(0);
 
 			EmbedBuilder builder = new EmbedBuilder()
 					.setLenient(true)
-					.withAuthorName("Statistiques Counter-Strike: Global Offensive")
+					.withAuthorName("Counter-Strike: Global Offensive Stats")
 					.withAuthorIcon("http://www.icon100.com/up/2841/256/csgo.png")
 					.withThumbnail(userObj.getString("avatarfull"))
 					.withColor(Config.BOT_COLOR)
@@ -91,11 +88,8 @@ public class CounterStrikeCmd extends AbstractCommand {
 					.appendField("Ratio", String.format("%.2f", (float) this.getValue(statsArray, "total_kills") / this.getValue(statsArray, "total_deaths")), true)
 					.appendField("Total wins", Integer.toString(this.getValue(statsArray, "total_wins")), true)
 					.appendField("Total MVP", Integer.toString(this.getValue(statsArray, "total_mvps")), true)
-					.withFooterText("Steam Profile: http://steamcommunity.com/profiles/" + context.getArg() + "/");
+					.withFooterText("Steam Profile: http://steamcommunity.com/profiles/" + steamid);
 			BotUtils.sendEmbed(builder.build(), context.getChannel());
-
-		} catch (HttpStatusException e) {
-			BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " This user doesn't play to Counter-Strike: Global Offensive or doesn't exist.", context.getChannel());
 		} catch (IOException e) {
 			if(e.getMessage().contains("400")) {
 				BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " This user doesn't play to Counter-Strike: Global Offensive or doesn't exist.", context.getChannel());
