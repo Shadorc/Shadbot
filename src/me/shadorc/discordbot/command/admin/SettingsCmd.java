3cbb7a333de7da69fa0e9500b19247337dd5828a
David <bi-crosseur@hotmail.fr>
2017-09-16 13:29:51 +0200

    SettingsCmd: Refactoring

--- src/me/shadorc/discordbot/command/admin/SettingsCmd.java
+++ src/me/shadorc/discordbot/command/admin/SettingsCmd.java
@@ -31,88 +31,99 @@ public class SettingsCmd extends AbstractCommand {
 			throw new MissingArgumentException();
 		}
 
-		String[] args = context.getArg().split(" ", 2);
-		String name = args[0];
+		String[] splitArgs = context.getArg().split(" ", 2);
+		String name = splitArgs[0];
+		String arg = splitArgs.length > 1 ? splitArgs[1] : null;
 
 		if(Setting.PREFIX.toString().equals(name)) {
-			if(args.length != 2) {
-				throw new MissingArgumentException();
-			}
-
-			String prefix = args[1].trim();
-			if(prefix.contains(" ")) {
-				BotUtils.sendMessage(Emoji.EXCLAMATION + " Prefix cannot contain space.", context.getChannel());
-				return;
-			}
-
-			if(prefix.length() > PREFIX_MAX_LENGTH) {
-				BotUtils.sendMessage(Emoji.EXCLAMATION + " Prefix cannot contain more than " + PREFIX_MAX_LENGTH + " characters.", context.getChannel());
-				return;
-			}
-
-			Storage.saveSetting(context.getGuild(), Setting.PREFIX, prefix);
-			BotUtils.sendMessage(Emoji.CHECK_MARK + " '" + prefix + "' is now the prefix for this server.", context.getChannel());
+			this.changePrefix(context, arg);
 
 		} else if(Setting.ALLOWED_CHANNELS.toString().equals(name)) {
-			List<IChannel> mentionedChannels = context.getMessage().getChannelMentions();
-			if(mentionedChannels.isEmpty()) {
-				throw new MissingArgumentException();
-			}
-
-			JSONArray allowedChannels = (JSONArray) Storage.getSetting(context.getGuild(), Setting.ALLOWED_CHANNELS);
-
-			if(args[1].startsWith("remove")) {
-				List<Long> newAllowedChannels = Utils.convertToLongList(allowedChannels);
-				for(IChannel channel : mentionedChannels) {
-					newAllowedChannels.remove(channel.getLongID());
-				}
-				allowedChannels = new JSONArray(newAllowedChannels);
-
-				BotUtils.sendMessage(Emoji.CHECK_MARK + " Channel "
-						+ StringUtils.formatList(mentionedChannels, channel -> channel.mention(), ", ")
-						+ " has been removed from allowed channels.", context.getChannel());
-
-			} else {
-				if(allowedChannels.length() == 0
-						&& mentionedChannels.stream().filter(channel -> channel.getLongID() == context.getChannel().getLongID()).count() == 0) {
-					BotUtils.sendMessage(Emoji.WARNING + " You did not mentioned this channel. "
-							+ "I will not reply until it's added to the list of allowed channels.", context.getChannel());
-				}
-
-				for(IChannel channel : mentionedChannels) {
-					allowedChannels.put(channel.getLongID());
-				}
-
-				BotUtils.sendMessage(Emoji.CHECK_MARK + " Channel "
-						+ StringUtils.formatList(mentionedChannels, channel -> channel.mention(), ", ")
-						+ " has been added to allowed channels.", context.getChannel());
-			}
-
-			Storage.saveSetting(context.getGuild(), Setting.ALLOWED_CHANNELS, allowedChannels);
+			this.changeAllowedChannels(context, arg);
 
 		} else if(Setting.DEFAULT_VOLUME.toString().equals(name)) {
-			if(args.length != 2) {
-				throw new MissingArgumentException();
-			}
-
-			String volStr = args[1].trim();
-			if(!StringUtils.isPositiveInt(volStr)) {
-				BotUtils.sendMessage(Emoji.EXCLAMATION + " Volume number isn't valid.", context.getChannel());
-				return;
-			}
-
-			int vol = Integer.parseInt(volStr);
-			if(vol < 1 || vol > 50) {
-				BotUtils.sendMessage(Emoji.EXCLAMATION + " Default volume must be between 1 and 50. ", context.getChannel());
-				return;
-			}
-
-			Storage.saveSetting(context.getGuild(), Setting.DEFAULT_VOLUME, vol);
-			BotUtils.sendMessage(Emoji.CHECK_MARK + " " + vol + "% is now the default volume for this server.", context.getChannel());
+			this.changeDefaultVolume(context, arg);
 
 		} else {
 			throw new MissingArgumentException();
 		}
+	}
+
+	private void changePrefix(Context context, String arg) throws MissingArgumentException {
+		if(arg == null) {
+			throw new MissingArgumentException();
+		}
+
+		if(arg.contains(" ")) {
+			BotUtils.sendMessage(Emoji.EXCLAMATION + " Prefix cannot contain space.", context.getChannel());
+			return;
+		}
+
+		if(arg.length() > PREFIX_MAX_LENGTH) {
+			BotUtils.sendMessage(Emoji.EXCLAMATION + " Prefix cannot contain more than " + PREFIX_MAX_LENGTH + " characters.", context.getChannel());
+			return;
+		}
+
+		Storage.saveSetting(context.getGuild(), Setting.PREFIX, arg);
+		BotUtils.sendMessage(Emoji.CHECK_MARK + " '" + arg + "' is now the prefix for this server.", context.getChannel());
+	}
+
+	private void changeAllowedChannels(Context context, String arg) throws MissingArgumentException {
+		List<IChannel> mentionedChannels = context.getMessage().getChannelMentions();
+		if(mentionedChannels.isEmpty()) {
+			throw new MissingArgumentException();
+		}
+
+		JSONArray allowedChannels = (JSONArray) Storage.getSetting(context.getGuild(), Setting.ALLOWED_CHANNELS);
+
+		if(arg.startsWith("remove")) {
+			List<Long> newAllowedChannels = Utils.convertToLongList(allowedChannels);
+			for(IChannel channel : mentionedChannels) {
+				newAllowedChannels.remove(channel.getLongID());
+			}
+			allowedChannels = new JSONArray(newAllowedChannels);
+
+			BotUtils.sendMessage(Emoji.CHECK_MARK + " Channel "
+					+ StringUtils.formatList(mentionedChannels, channel -> channel.mention(), ", ")
+					+ " has been removed from allowed channels.", context.getChannel());
+
+		} else {
+			if(allowedChannels.length() == 0
+					&& mentionedChannels.stream().filter(channel -> channel.getLongID() == context.getChannel().getLongID()).count() == 0) {
+				BotUtils.sendMessage(Emoji.WARNING + " You did not mentioned this channel. "
+						+ "I will not reply until it's added to the list of allowed channels.", context.getChannel());
+			}
+
+			for(IChannel channel : mentionedChannels) {
+				allowedChannels.put(channel.getLongID());
+			}
+
+			BotUtils.sendMessage(Emoji.CHECK_MARK + " Channel "
+					+ StringUtils.formatList(mentionedChannels, channel -> channel.mention(), ", ")
+					+ " has been added to allowed channels.", context.getChannel());
+		}
+
+		Storage.saveSetting(context.getGuild(), Setting.ALLOWED_CHANNELS, allowedChannels);
+	}
+
+	private void changeDefaultVolume(Context context, String arg) throws MissingArgumentException {
+		if(arg == null) {
+			throw new MissingArgumentException();
+		}
+
+		if(!StringUtils.isPositiveInt(arg)) {
+			BotUtils.sendMessage(Emoji.EXCLAMATION + " Invalid number, must be between 1 and 50.", context.getChannel());
+			return;
+		}
+
+		int vol = Integer.parseInt(arg);
+		if(vol < 1 || vol > 50) {
+			BotUtils.sendMessage(Emoji.EXCLAMATION + " Default volume must be between 1 and 50. ", context.getChannel());
+			return;
+		}
+
+		Storage.saveSetting(context.getGuild(), Setting.DEFAULT_VOLUME, vol);
+		BotUtils.sendMessage(Emoji.CHECK_MARK + " " + vol + "% is now the default volume for this server.", context.getChannel());
 	}
 
 	@Override
