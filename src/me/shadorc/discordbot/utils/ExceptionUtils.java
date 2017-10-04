b7f5cc123a2cba3d75479892c74b128c5cdcf0f7
David <bi-crosseur@hotmail.fr>
2017-10-04 17:41:40 +0200

    Add guild ID to message exception log

--- src/me/shadorc/discordbot/utils/ExceptionUtils.java
+++ src/me/shadorc/discordbot/utils/ExceptionUtils.java
@@ -9,11 +9,11 @@ public class ExceptionUtils {
 
 	public static void manageMessageException(Object message, IChannel channel, DiscordException err) {
 		if(err.getErrorMessage().contains("Discord didn't return a response") || err.getErrorMessage().contains("400 Bad Request")) {
-			LogUtils.info("A message could not be send now, adding it to queue.");
+			LogUtils.info("{Guild ID: " + channel.getGuild().getLongID() + "} A message could not be send now, adding it to queue.");
 			Scheduler.scheduleMessages(message, channel, Reason.API_ERROR);
 
 		} else if(err.getErrorMessage().contains("Attempt to send message before shard is ready!")) {
-			LogUtils.info("A message could not be send because shard isn't ready, adding it to queue.");
+			LogUtils.info("{Guild ID: " + channel.getGuild().getLongID() + "} A message could not be send because shard isn't ready, adding it to queue.");
 			Scheduler.scheduleMessages(message, channel, Reason.SHARD_NOT_READY);
 
 		} else {
