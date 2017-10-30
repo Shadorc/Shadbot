de574ef27ab6fd2f3af885a6dd4d07bd2942cdef
David <bi-crosseur@hotmail.fr>
2017-10-30 22:01:14 +0100

    Reduce max message cache count to 200

--- src/me/shadorc/discordbot/Shadbot.java
+++ src/me/shadorc/discordbot/Shadbot.java
@@ -27,7 +27,7 @@ public class Shadbot {
 
 		client = new ClientBuilder()
 				.withToken(Config.get(APIKey.DISCORD_TOKEN))
-				.setMaxMessageCacheCount(250)
+				.setMaxMessageCacheCount(200)
 				.setMaxReconnectAttempts(100)
 				.login();
 
