217ad41fbf0a705ae24175790966b913cc6084c7
David <bi-crosseur@hotmail.fr>
2017-11-04 20:44:23 +0100

    LottoCmd: Some fixes and improvements

--- src/me/shadorc/discordbot/command/game/LottoCmd.java
+++ src/me/shadorc/discordbot/command/game/LottoCmd.java
@@ -3,8 +3,6 @@ package me.shadorc.discordbot.command.game;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
@@ -13,6 +11,7 @@ import java.util.List;
 
 import javax.swing.Timer;
 
+import org.joda.time.Instant;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -57,40 +56,44 @@ public class LottoCmd extends AbstractCommand implements ActionListener {
 			return;
 		}
 
-		if(context.hasArg()) {
-			if(DatabaseManager.getCoins(context.getGuild(), context.getAuthor()) < PAID_COST) {
-				BotUtils.sendMessage(TextUtils.notEnoughCoins(context.getAuthor()), context.getChannel());
-				return;
-			}
-
-			if(!StringUtils.isPositiveInt(context.getArg())) {
-				BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " Invalid number.", context.getChannel());
-				return;
-			}
-
-			if(this.isPlaying(context.getAuthor())) {
-				BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " You're already participating.", context.getChannel());
-				return;
-			}
-
-			int num = Integer.parseInt(context.getArg());
-
-			DatabaseManager.addCoins(context.getGuild(), context.getAuthor(), -PAID_COST);
-			StatsManager.increment(StatCategory.MONEY_LOSSES_COMMAND, this.getFirstName(), PAID_COST);
-			LottoDataManager.addToPool(PAID_COST);
-
-			LottoDataManager.addPlayer(context.getGuild(), context.getAuthor(), num);
-
-			BotUtils.sendMessage(Emoji.TICKET + " You bought a lottery ticket and bet on number **" + num + "**. Good luck !", context.getChannel());
+		if(!context.hasArg()) {
+			EmbedBuilder builder = Utils.getDefaultEmbed()
+					.withAuthorName("Lotto")
+					.withThumbnail("http://fastraxpos.com/wp-content/uploads/2016/01/lottery.png")
+					.appendField("Number of participants", Integer.toString(LottoDataManager.getPlayers().length()), false)
+					.appendField("Prize pool", Integer.toString(LottoDataManager.getPool()), false);
+			BotUtils.sendMessage(builder.build(), context.getChannel());
 			return;
 		}
 
-		EmbedBuilder builder = Utils.getDefaultEmbed()
-				.withAuthorName("Lotto")
-				.withThumbnail("http://fastraxpos.com/wp-content/uploads/2016/01/lottery.png")
-				.appendField("Number of participants", Integer.toString(LottoDataManager.getPlayers().length()), false)
-				.appendField("Prize pool", Integer.toString(LottoDataManager.getPool()), false);
-		BotUtils.sendMessage(builder.build(), context.getChannel());
+		if(DatabaseManager.getCoins(context.getGuild(), context.getAuthor()) < PAID_COST) {
+			BotUtils.sendMessage(TextUtils.notEnoughCoins(context.getAuthor()), context.getChannel());
+			return;
+		}
+
+		if(!StringUtils.isPositiveInt(context.getArg())) {
+			BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " Invalid number, must be between 1 and 100.", context.getChannel());
+			return;
+		}
+
+		int num = Integer.parseInt(context.getArg());
+		if(num < 1 || num > 100) {
+			BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " Invalid number, must be between 1 and 100.", context.getChannel());
+			return;
+		}
+
+		if(this.isPlaying(context.getAuthor())) {
+			BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " You're already participating.", context.getChannel());
+			return;
+		}
+
+		DatabaseManager.addCoins(context.getGuild(), context.getAuthor(), -PAID_COST);
+		StatsManager.increment(StatCategory.MONEY_LOSSES_COMMAND, this.getFirstName(), PAID_COST);
+		LottoDataManager.addToPool(PAID_COST);
+
+		LottoDataManager.addPlayer(context.getGuild(), context.getAuthor(), num);
+
+		BotUtils.sendMessage(Emoji.TICKET + " You bought a lottery ticket and bet on number **" + num + "**. Good luck !", context.getChannel());
 	}
 
 	private boolean isPlaying(IUser user) {
@@ -104,12 +107,12 @@ public class LottoCmd extends AbstractCommand implements ActionListener {
 	}
 
 	private int getDelayBeforeNextCheck() {
-		ZonedDateTime nextDate = ZonedDateTime.of(LocalDateTime.now(), ZoneId.systemDefault())
+		ZonedDateTime nextDate = ZonedDateTime.now()
 				.with(TemporalAdjusters.next(DayOfWeek.SUNDAY))
 				.withHour(12)
 				.withMinute(0)
 				.withSecond(0);
-		return (int) (ZonedDateTime.now().toInstant().toEpochMilli() - nextDate.toInstant().toEpochMilli());
+		return (int) (nextDate.toInstant().toEpochMilli() - Instant.now().getMillis());
 	}
 
 	@Override
@@ -118,9 +121,11 @@ public class LottoCmd extends AbstractCommand implements ActionListener {
 				.appendDescription("**Buy a ticket for the lottery or display the current lottery status.**")
 				.appendField("Usage", "`" + context.getPrefix() + this.getFirstName() + " [<num>]`", false)
 				.appendField("Restrictions", "**num** - must be between 1 and 100", false)
-				.appendField("Info", "One winner is randomly drawn every Sunday at noon (English time). "
-						+ "If no one has won, the prize pool is put back into play, if there are multiple winners, the prize pool is split.", false)
-				.appendField("Gains", "The prize pool contains all coins lost at games during the week plus the purchase price of the lottery tickets.", false);
+				.appendField("Info", "One winner is randomly drawn every Sunday at noon (English time)."
+						+ "\nIf no one wins, the prize pool is put back into play, "
+						+ "if there are multiple winners, the prize pool is splitted between them.", false)
+				.appendField("Gains", "The prize pool contains all coins lost at games during the week plus "
+						+ "the purchase price of the lottery tickets.", false);
 		BotUtils.sendMessage(builder.build(), context.getChannel());
 	}
 
