e9e361eeb04279a9ae267940328ff2d3367cdeab
David <bi-crosseur@hotmail.fr>
2017-11-11 13:54:55 +0100

    WallpaperCmd: Refactored to use commons.cli library

--- src/me/shadorc/discordbot/command/image/WallpaperCmd.java
+++ src/me/shadorc/discordbot/command/image/WallpaperCmd.java
@@ -1,9 +1,14 @@
 package me.shadorc.discordbot.command.image;
 
 import java.awt.Dimension;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import com.ivkos.wallhaven4j.Wallhaven;
 import com.ivkos.wallhaven4j.models.misc.Ratio;
@@ -21,8 +26,10 @@ import me.shadorc.discordbot.data.Config;
 import me.shadorc.discordbot.data.Config.APIKey;
 import me.shadorc.discordbot.data.Setting;
 import me.shadorc.discordbot.utils.BotUtils;
+import me.shadorc.discordbot.utils.ExceptionUtils;
 import me.shadorc.discordbot.utils.MathUtils;
 import me.shadorc.discordbot.utils.StringUtils;
+import me.shadorc.discordbot.utils.TextUtils;
 import me.shadorc.discordbot.utils.Utils;
 import me.shadorc.discordbot.utils.command.Emoji;
 import me.shadorc.discordbot.utils.command.MissingArgumentException;
@@ -44,82 +51,82 @@ public class WallpaperCmd extends AbstractCommand {
 			wallhaven = new Wallhaven(Config.get(APIKey.WALLHAVEN_LOGIN), Config.get(APIKey.WALLHAVEN_PASSWORD));
 		}
 
-		SearchQueryBuilder queryBuilder = new SearchQueryBuilder();
+		Options options = new Options();
+		options.addOption("p", "purity", true, "nsfw, sketchy, sfw");
+		options.addOption("c", "category", true, "people, anime, general");
+		options.addOption("rat", "ratio", true, "image ratio");
+		options.addOption("res", "resolution", true, "image resolution");
 
-		List<String> invalidArgs = new ArrayList<>();
-		for(String pair : StringUtils.getSplittedArg(context.getArg())) {
-			String[] splitPair = pair.split("=");
+		Option keyOpt = new Option("k", "keyword", true, "keyword");
+		keyOpt.setValueSeparator(',');
+		options.addOption(keyOpt);
 
-			if(splitPair.length != 2) {
-				invalidArgs.add(pair);
-				continue;
-			}
-
-			String name = pair.split("=")[0];
-			String value = pair.split("=")[1];
-			switch (name) {
-				case "purity":
-					if(!Arrays.stream(Purity.values()).anyMatch(purityValue -> purityValue.toString().equalsIgnoreCase(value))) {
-						invalidArgs.add(pair);
-						continue;
-					}
-
-					if(value.matches("nsfw|sketchy") && !context.getChannel().isNSFW()) {
-						BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " This must be a NSFW-channel. If you're an admin, you can use "
-								+ "`" + context.getPrefix() + "settings " + Setting.NSFW + " toggle`", context.getChannel());
-						return;
-					}
-
-					queryBuilder.purity(Purity.valueOf(value.toUpperCase()));
-					break;
-
-				case "category":
-					if(!Arrays.stream(Category.values()).anyMatch(category -> category.toString().equalsIgnoreCase(value))) {
-						invalidArgs.add(pair);
-						continue;
-					}
-					queryBuilder.categories(Category.valueOf(value.toUpperCase()));
-					break;
-
-				case "ratio":
-				case "resolution":
-					Dimension dim = this.parseDimension(value);
-					if(dim == null) {
-						invalidArgs.add(pair);
-						continue;
-					}
-					if("ratio".equals(name)) {
-						queryBuilder.ratios(new Ratio((int) dim.getWidth(), (int) dim.getHeight()));
-					} else {
-						queryBuilder.resolutions(new Resolution((int) dim.getWidth(), (int) dim.getHeight()));
-					}
-					break;
-
-				case "keyword":
-					queryBuilder.keywords(value);
-					break;
-
-				default:
-					invalidArgs.add(pair);
-					break;
-			}
-		}
-
-		if(!invalidArgs.isEmpty()) {
-			BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " " + StringUtils.formatList(invalidArgs, str -> "`" + str + "`", ", ")
-					+ (invalidArgs.size() == 1 ? " is an invalid argument" : " are invalid arguments") + ". "
-					+ "Use `" + context.getPrefix() + "help " + this.getFirstName() + "` for more information.", context.getChannel());
+		CommandLine cmd;
+		try {
+			cmd = new DefaultParser().parse(options, StringUtils.getSplittedArg(context.getArg()));
+		} catch (ParseException err) {
+			ExceptionUtils.manageException("getting a wallpaper", context, err);
 			return;
 		}
 
-		// Set default purity to SFW
-		if(!context.getArg().contains("purity=")) {
-			queryBuilder.purity(Purity.SFW);
+		SearchQueryBuilder queryBuilder = new SearchQueryBuilder();
+
+		String purity = cmd.getOptionValue("purity", Purity.SFW.toString());
+		if(purity != null) {
+			if(Arrays.stream(Purity.values()).anyMatch(purityValue -> purityValue.toString().equalsIgnoreCase(purity))) {
+				queryBuilder.purity(Purity.valueOf(purity.toUpperCase()));
+			} else {
+				this.sendInvalidArg("purity", context);
+				return;
+			}
+		}
+
+		if(purity.matches("nsfw|sketchy") && !context.getChannel().isNSFW()) {
+			BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " This must be a NSFW-channel. If you're an admin, you can use "
+					+ "`" + context.getPrefix() + "settings " + Setting.NSFW + " toggle`", context.getChannel());
+			return;
+		}
+
+		String category = cmd.getOptionValue("category");
+		if(category != null) {
+			if(Arrays.stream(Category.values()).anyMatch(catValue -> catValue.toString().equalsIgnoreCase(category))) {
+				queryBuilder.categories(Category.valueOf(category.toUpperCase()));
+			} else {
+				this.sendInvalidArg("category", context);
+				return;
+			}
+		}
+
+		String ratio = cmd.getOptionValue("ratio");
+		if(ratio != null) {
+			Dimension dim = this.parseDimension(ratio);
+			if(dim == null) {
+				this.sendInvalidArg("ratio", context);
+				return;
+			} else {
+				queryBuilder.ratios(new Ratio((int) dim.getWidth(), (int) dim.getHeight()));
+			}
+		}
+
+		String resolution = cmd.getOptionValue("resolution");
+		if(resolution != null) {
+			Dimension dim = this.parseDimension(resolution);
+			if(dim == null) {
+				this.sendInvalidArg("resolution", context);
+				return;
+			} else {
+				queryBuilder.resolutions(new Resolution((int) dim.getWidth(), (int) dim.getHeight()));
+			}
+		}
+
+		String keyword = cmd.getOptionValue("keyword");
+		if(keyword != null) {
+			queryBuilder.keywords(keyword.split(","));
 		}
 
 		List<Wallpaper> wallpapers = wallhaven.search(queryBuilder.pages(1).build());
 		if(wallpapers.isEmpty()) {
-			BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " No result found.", context.getChannel());
+			BotUtils.sendMessage(TextUtils.noResult(context.getMessage().getContent()), context.getChannel());
 			return;
 		}
 
@@ -135,6 +142,11 @@ public class WallpaperCmd extends AbstractCommand {
 		BotUtils.sendMessage(embed.build(), context.getChannel());
 	}
 
+	private void sendInvalidArg(String arg, Context context) {
+		BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " Invalid " + arg + ". Use `" + context.getPrefix() + "help "
+				+ this.getFirstName() + "` for more information.", context.getChannel());
+	}
+
 	private Dimension parseDimension(String arg) {
 		String[] ratioArray = arg.split(arg.contains("x") ? "x" : "\\*");
 		if(ratioArray.length != 2 || !StringUtils.isPositiveInt(ratioArray[0]) || !StringUtils.isPositiveInt(ratioArray[1])) {
@@ -147,16 +159,14 @@ public class WallpaperCmd extends AbstractCommand {
 	public void showHelp(Context context) {
 		EmbedBuilder builder = Utils.getDefaultEmbed(this)
 				.withDescription("**Search for a wallpaper.**")
-				.appendField("Usage", "`" + context.getPrefix() + this.getFirstName() + " [arguments]`", false)
-				.appendField("Arguments", "**[OPTIONAL]** You can add them to refine your search, just put `<name>=<value>` after the command.", false)
-				.appendField("Name - value", "**purity** - " + StringUtils.formatArray(Purity.values(), purity -> purity.toString().toLowerCase(), ", ")
+				.appendField("Usage", "`" + context.getPrefix() + this.getFirstName() + " [-p purity] [-c category] [-rat ratio] [-res resolution] [-k keywords]`", false)
+				.appendField("Arguments", "**purity** - " + StringUtils.formatArray(Purity.values(), purity -> purity.toString().toLowerCase(), ", ")
 						+ "\n**category** - " + StringUtils.formatArray(Category.values(), cat -> cat.toString().toLowerCase(), ", ")
-						+ "\n**ratio** - Image ratio (Ex: 16x9)"
-						+ "\n**resolution** - Image resolution (Ex: 1920x1080)"
-						+ "\n**keyword** - Keyword (Ex: doom)", true)
+						+ "\n**ratio** - image ratio (e.g. 16x9)"
+						+ "\n**resolution** - image resolution (e.g. 1920x1080)"
+						+ "\n**keyword** - keywords (e.g. doom,game)", true)
 				.appendField("Example", "Search a *SFW* wallpaper in category *Anime*, with a *16x9* ratio :"
-						+ "\n`" + context.getPrefix() + this.getFirstName() + " purity=sfw category=anime ratio=16x9`", false);
+						+ "\n`" + context.getPrefix() + this.getFirstName() + " -p sfw -c anime -rat 16x9`", false);
 		BotUtils.sendMessage(builder.build(), context.getChannel());
 	}
-
 }
