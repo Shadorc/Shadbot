211e3fec82cf218fffe6d86e863bf41c97e14e17
David <bi-crosseur@hotmail.fr>
2017-11-22 22:34:35 +0100

    Catch all exceptions onMessageReceivedEvent

--- src/me/shadorc/discordbot/events/MessageListener.java
+++ src/me/shadorc/discordbot/events/MessageListener.java
@@ -18,35 +18,35 @@ public class MessageListener {
 
 	@EventSubscriber
 	public void onMessageReceivedEvent(MessageReceivedEvent event) {
-		StatsManager.increment(StatsEnum.MESSAGES_RECEIVED);
-
-		if(event.getAuthor().isBot()) {
-			return;
-		}
-
-		if(event.getChannel().isPrivate()) {
-			BotUtils.sendMessage(Emoji.INFO + " Sorry, I don't respond (yet ?) to private messages.", event.getChannel());
-			LogUtils.info("Shadbot has received a private message. (Message: " + event.getMessage().getContent() + ")");
-			StatsManager.increment(StatsEnum.PRIVATE_MESSAGES_RECEIVED);
-			return;
-		}
-
-		if(!BotUtils.isChannelAllowed(event.getGuild(), event.getChannel())) {
-			return;
-		}
-
 		IMessage message = event.getMessage();
-		if(MessageManager.isWaitingForMessage(event.getChannel()) && MessageManager.notify(message)) {
-			return;
-		}
+		try {
+			StatsManager.increment(StatsEnum.MESSAGES_RECEIVED);
 
-		String prefix = (String) DatabaseManager.getSetting(event.getGuild(), Setting.PREFIX);
-		if(message.getContent().startsWith(prefix)) {
-			try {
-				CommandManager.manage(event);
-			} catch (Exception err) {
-				LogUtils.error("{Guild ID: " + event.getGuild().getLongID() + "} An unknown error occurred while executing a command.", err, message.getContent());
+			if(event.getAuthor().isBot()) {
+				return;
 			}
+
+			if(event.getChannel().isPrivate()) {
+				BotUtils.sendMessage(Emoji.INFO + " Sorry, I don't respond (yet ?) to private messages.", event.getChannel());
+				LogUtils.info("Shadbot has received a private message. (Message: " + event.getMessage().getContent() + ")");
+				StatsManager.increment(StatsEnum.PRIVATE_MESSAGES_RECEIVED);
+				return;
+			}
+
+			if(!BotUtils.isChannelAllowed(event.getGuild(), event.getChannel())) {
+				return;
+			}
+
+			if(MessageManager.isWaitingForMessage(event.getChannel()) && MessageManager.notify(message)) {
+				return;
+			}
+
+			String prefix = (String) DatabaseManager.getSetting(event.getGuild(), Setting.PREFIX);
+			if(message.getContent().startsWith(prefix)) {
+				CommandManager.manage(event);
+			}
+		} catch (Exception err) {
+			LogUtils.error("{Guild ID: " + event.getGuild().getLongID() + "} An unknown error occurred while receiving a message.", err, message.getContent());
 		}
 	}
 }
