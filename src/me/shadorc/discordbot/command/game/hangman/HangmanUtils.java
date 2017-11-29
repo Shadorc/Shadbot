3d0883cdbb6f5b36250c79be809148036b17f510
David <bi-crosseur@hotmail.fr>
2017-11-29 21:38:02 +0100

    HangmanCmd: Use another words source

--- src/me/shadorc/discordbot/command/game/hangman/HangmanUtils.java
+++ src/me/shadorc/discordbot/command/game/hangman/HangmanUtils.java
@@ -1,19 +1,37 @@
 package me.shadorc.discordbot.command.game.hangman;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.json.JSONException;
+
+import me.shadorc.discordbot.utils.LogUtils;
+import me.shadorc.discordbot.utils.MathUtils;
 import me.shadorc.discordbot.utils.NetUtils;
-import me.shadorc.discordbot.utils.StringUtils;
 
 public class HangmanUtils {
 
-	protected static String getWord(int minWordLength, int maxWordLength) throws IOException {
-		String word;
-		do {
-			word = NetUtils.getBody("http://setgetgo.com/randomword/get.php");
-		} while(word.length() <= minWordLength || word.length() >= maxWordLength || StringUtils.capitalize(word).equals(word));
-		return word;
+	private static List<String> words;
+
+	static {
+		try {
+			words = Arrays.stream(NetUtils.getBody("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt").split("\n"))
+					.filter(word -> word.length() >= HangmanManager.MIN_WORD_LENGTH && word.length() <= HangmanManager.MAX_WORD_LENGTH)
+					.map(word -> word.trim())
+					.collect(Collectors.toList());
+		} catch (JSONException | IOException err) {
+			LogUtils.error("{ " + HangmanUtils.class.getSimpleName() + "} An error occurred while getting words list.", err);
+		}
+	}
+
+	protected static String getWord() throws IOException {
+		if(words == null || words.isEmpty()) {
+			throw new IOException("Words list has not been initialized.");
+		}
+
+		return words.get(MathUtils.rand(words.size()));
 	}
 
 	protected static String getRepresentation(String word, List<String> charsTested) {
