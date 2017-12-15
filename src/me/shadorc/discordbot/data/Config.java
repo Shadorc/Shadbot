6eb5e7ea2fb72b259d2b6e42b8197dc1c154e3d3
David <bi-crosseur@hotmail.fr>
2017-12-15 17:41:51 +0100

    Bump version

--- src/me/shadorc/discordbot/data/Config.java
+++ src/me/shadorc/discordbot/data/Config.java
@@ -16,7 +16,7 @@ import me.shadorc.discordbot.utils.LogUtils;
 
 public class Config {
 
-	public static final Version VERSION = new Version(1, 10, 5);
+	public static final Version VERSION = new Version(1, 10, 6);
 
 	public static final long LOGS_CHANNEL_ID = 346311941829951489L;
 
