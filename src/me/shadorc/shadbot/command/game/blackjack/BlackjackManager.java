5819501e5e262bc352bb857a501a2cec80627296
David <bi-crosseur@hotmail.fr>
2018-02-04 22:16:32 +0100

    BlackjackManager: Increase coins multiplier

--- src/me/shadorc/shadbot/command/game/blackjack/BlackjackManager.java
+++ src/me/shadorc/shadbot/command/game/blackjack/BlackjackManager.java
@@ -31,7 +31,7 @@ import sx.blah.discord.util.EmbedBuilder;
 public class BlackjackManager extends AbstractGameManager implements MessageListener {
 
 	private static final int GAME_DURATION = 60;
-	private static final float WIN_MULTIPLIER = 1.04f;
+	private static final float WIN_MULTIPLIER = 1.05f;
 
 	private final RateLimiter rateLimiter;
 	private final List<BlackjackPlayer> players;
