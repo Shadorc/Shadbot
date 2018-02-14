be4614032f623dd8af3126bdc983c669d9ceaca7
David <bi-crosseur@hotmail.fr>
2018-02-14 18:58:34 +0100

    LoadingMessage: Check permissions

--- src/me/shadorc/shadbot/utils/object/LoadingMessage.java
+++ src/me/shadorc/shadbot/utils/object/LoadingMessage.java
@@ -1,9 +1,12 @@
 package me.shadorc.shadbot.utils.object;
 
 import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.LogUtils;
+import me.shadorc.shadbot.utils.TextUtils;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.Permissions;
 import sx.blah.discord.util.RequestBuffer;
 import sx.blah.discord.util.RequestBuffer.RequestFuture;
 
@@ -24,6 +27,11 @@ public class LoadingMessage {
 	}
 
 	public void edit(String content) {
+		if(!BotUtils.hasPermissions(channel, Permissions.SEND_MESSAGES)) {
+			LogUtils.infof("{Guild ID: %d} Shadbot wasn't allowed to send a message.", channel.getGuild().getLongID());
+			return;
+		}
+
 		IMessage message = msgRequest == null ? null : msgRequest.get();
 		if(message == null) {
 			msgRequest = BotUtils.sendMessage(content, channel);
@@ -35,6 +43,11 @@ public class LoadingMessage {
 	}
 
 	public void edit(EmbedObject embed) {
+		if(!BotUtils.hasPermissions(channel, Permissions.SEND_MESSAGES, Permissions.EMBED_LINKS)) {
+			BotUtils.sendMessage(TextUtils.missingPerm(Permissions.EMBED_LINKS), channel);
+			LogUtils.infof("{Guild ID: %d} Shadbot wasn't allowed to send embed link.", channel.getGuild().getLongID());
+		}
+
 		IMessage message = msgRequest == null ? null : msgRequest.get();
 		if(message == null) {
 			msgRequest = BotUtils.sendMessage(embed, channel);
