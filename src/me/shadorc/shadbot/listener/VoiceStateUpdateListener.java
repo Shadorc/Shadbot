6bf54fc3eb085c306c442731674b526a0d689c18
David <bi-crosseur@hotmail.fr>
2018-06-09 13:43:20 +0200

    VoiceStateUpdateListener done
    This would probably be done in a cleaner version in the future but this work for the moment

--- src/me/shadorc/shadbot/listener/VoiceStateUpdateListener.java
+++ src/me/shadorc/shadbot/listener/VoiceStateUpdateListener.java
@@ -1,74 +1,104 @@
 package me.shadorc.shadbot.listener;
 
+import java.util.Optional;
+
 import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.util.Snowflake;
 import me.shadorc.shadbot.music.GuildMusic;
 import me.shadorc.shadbot.music.GuildMusicManager;
 import me.shadorc.shadbot.utils.BotUtils;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
 import me.shadorc.shadbot.utils.object.Emoji;
+import reactor.core.publisher.Mono;
 
 public class VoiceStateUpdateListener {
 
-	// TODO: Rework
 	public static void onVoiceStateUpdateEvent(VoiceStateUpdateEvent event) {
-		// TODO: Use DiscordClient#getSelfId
-		event.getClient().getSelf().subscribe(self -> {
-			// This is not an event from the bot
-			if(event.getCurrent().getUserId().equals(self.getId())) {
-				// TODO: && !event.getCurrent().isPresent()) {
-				final Snowflake guildId = event.getCurrent().getGuildId();
+		final Snowflake selfId = null; // TODO: event.getClient().getSelfId()
+		// TODO: Improve this in Discord4J
+		final Snowflake userId = event.getOld()
+				.map(VoiceState::getUserId)
+				.orElse(event.getCurrent().getUserId());
 
-				GuildMusic guildMusic = GuildMusicManager.GUILD_MUSIC_MAP.get(guildId);
-				if(guildMusic != null) {
-					guildMusic.destroy();
-					LogUtils.infof("{Guild ID: %s} Voice channel left.", guildId);
-				}
-				return;
-			}
-
-			check(null);
-		});
+		if(userId.equals(selfId)) {
+			onBotEvent(event);
+		} else {
+			onUserEvent(event);
+		}
 	}
 
-	private static synchronized void check(Guild guild) {
-		// TODO avoid blocking, use DiscordClient#getSelfId
-		guild.getClient()
-				.getMemberById(guild.getId(), guild.getClient().getSelf().block().getId())
-				.flatMap(member -> member.getVoiceState())
-				.subscribe(voiceState -> {
+	private static void onBotEvent(VoiceStateUpdateEvent event) {
+		// TODO: Change to !event.getCurrent.isPresent()
+		// The bot has left a voice channel
+		if(event.getCurrent() == null) {
+			final Snowflake guildId = event.getCurrent().getGuildId();
+
+			GuildMusic guildMusic = GuildMusicManager.GUILD_MUSIC_MAP.get(guildId);
+			if(guildMusic != null) {
+				guildMusic.destroy();
+				LogUtils.infof("{Guild ID: %s} Voice channel left.", guildId);
+			}
+		}
+	}
+
+	private static void onUserEvent(VoiceStateUpdateEvent event) {
+		final Snowflake guildId = event.getOld()
+				.map(VoiceState::getGuildId)
+				.orElse(event.getCurrent().getGuildId());
+		final Snowflake selfId = null; // TODO: Change to event.getClient().getSelfId()
+
+		check(event.getClient().getMemberById(guildId, selfId));
+	}
+
+	private static synchronized void check(Mono<Member> monoMember) {
+		Mono.zip(monoMember, monoMember.flatMap(Member::getVoiceState))
+				.subscribe(memberAndVoiceState -> {
+
+					final Member member = memberAndVoiceState.getT1();
+					final VoiceState voiceState = memberAndVoiceState.getT2();
 
 					// The bot is not in a voice channel
 					if(!voiceState.getChannelId().isPresent()) {
 						return;
 					}
 
-					GuildMusic guildMusic = GuildMusicManager.GUILD_MUSIC_MAP.get(guild.getId());
+					final Snowflake voiceChannelId = voiceState.getChannelId().get();
+
+					GuildMusic guildMusic = GuildMusicManager.GUILD_MUSIC_MAP.get(member.getGuildId());
 					if(guildMusic == null) {
 						return;
 					}
 
-					if(isAlone(guild) && !guildMusic.isLeavingScheduled()) {
-						BotUtils.sendMessage(Emoji.INFO + " Nobody is listening anymore, music paused. "
-								+ "I will leave the voice channel in 1 minute.",
-								guildMusic.getMessageChannel());
-						guildMusic.getScheduler().getAudioPlayer().setPaused(true);
-						guildMusic.scheduleLeave();
+					isAlone(member.getGuild(), voiceChannelId).subscribe(isAlone -> {
+						if(isAlone && !guildMusic.isLeavingScheduled()) {
+							BotUtils.sendMessage(Emoji.INFO + " Nobody is listening anymore, music paused. "
+									+ "I will leave the voice channel in 1 minute.",
+									guildMusic.getMessageChannel());
+							guildMusic.getScheduler().getAudioPlayer().setPaused(true);
+							guildMusic.scheduleLeave();
 
-					} else if(!isAlone(guild) && guildMusic.isLeavingScheduled()) {
-						BotUtils.sendMessage(Emoji.INFO + " Somebody joined me, music resumed.",
-								guildMusic.getMessageChannel());
-						guildMusic.getScheduler().getAudioPlayer().setPaused(false);
-						guildMusic.cancelLeave();
-					}
+						} else if(!isAlone && guildMusic.isLeavingScheduled()) {
+							BotUtils.sendMessage(Emoji.INFO + " Somebody joined me, music resumed.",
+									guildMusic.getMessageChannel());
+							guildMusic.getScheduler().getAudioPlayer().setPaused(false);
+							guildMusic.cancelLeave();
+						}
+					});
 				});
 	}
 
-	// TODO
-	private static boolean isAlone(Object object) {
-		return true;
-		// return voiceChannel.getConnectedUsers().stream().filter(user -> !user.isBot()).count() == 0;
+	private static Mono<Boolean> isAlone(Mono<Guild> guild, Snowflake voiceChannelId) {
+		return guild.flatMapMany(Guild::getMembers)
+				.filter(member -> !member.isBot())
+				.flatMap(Member::getVoiceState)
+				.map(VoiceState::getChannelId)
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.filter(voiceChannelId::equals)
+				.hasElements();
 	}
 
 }
