f8473096331bd236e9b1bfdd15d81642071a104d
David <bi-crosseur@hotmail.fr>
2018-06-09 16:41:57 +0200

    Implemented ban command

--- src/me/shadorc/shadbot/command/admin/BanCmd.java
+++ src/me/shadorc/shadbot/command/admin/BanCmd.java
@@ -1,93 +1,108 @@
-// package me.shadorc.shadbot.command.admin;
-//
-// import java.security.Permissions;
-// import java.util.List;
-//
-// import me.shadorc.shadbot.core.command.AbstractCommand;
-// import me.shadorc.shadbot.core.command.CommandCategory;
-// import me.shadorc.shadbot.core.command.CommandPermission;
-// import me.shadorc.shadbot.core.command.Context;
-// import me.shadorc.shadbot.core.command.annotation.Command;
-// import me.shadorc.shadbot.exception.IllegalCmdArgumentException;
-// import me.shadorc.shadbot.exception.MissingArgumentException;
-// import me.shadorc.shadbot.utils.BotUtils;
-// import me.shadorc.shadbot.utils.FormatUtils;
-// import me.shadorc.shadbot.utils.StringUtils;
-// import me.shadorc.shadbot.utils.TextUtils;
-// import me.shadorc.shadbot.utils.embed.HelpBuilder;
-// import me.shadorc.shadbot.utils.object.Emoji;
-//
-// @Command(category = CommandCategory.ADMIN, permission = CommandPermission.ADMIN, names = { "ban" })
-// public class BanCmd extends AbstractCommand {
-//
-// @Override
-// public void execute(Context context) throws MissingArgumentException, IllegalCmdArgumentException {
-// if(!context.hasArg()) {
-// throw new MissingArgumentException();
-// }
-//
-// List<IUser> mentionedUsers = context.getMessage().getMentions();
-// if(mentionedUsers.isEmpty()) {
-// throw new MissingArgumentException();
-// }
-//
-// if(!PermissionUtils.hasPermissions(context.getChannel(), context.getAuthor(), Permissions.BAN)) {
-// throw new IllegalArgumentException("You don't have permission to ban.");
-// }
-//
-// if(!BotUtils.hasPermissions(context.getChannel(), Permissions.BAN)) {
-// BotUtils.sendMessage(TextUtils.missingPerm(Permissions.BAN), context.getChannel());
-// return;
-// }
-//
-// if(mentionedUsers.contains(context.getAuthor())) {
-// throw new IllegalCmdArgumentException("You cannot ban yourself.");
-// }
-//
-// for(IUser mentionedUser : mentionedUsers) {
-// if(!PermissionUtils.isUserHigher(context.getGuild(), context.getAuthor(), mentionedUser)) {
-// throw new IllegalCmdArgumentException(String.format("You can't ban **%s** because he has the same or a higher role "
-// + "position than you in the role hierarchy.",
-// mentionedUser.getName()));
-// }
-// if(!BotUtils.canInteract(context.getGuild(), mentionedUser)) {
-// throw new IllegalCmdArgumentException(String.format("I cannot ban **%s** because he has the same or a higher role "
-// + "position than me in the role hierarchy.",
-// mentionedUser.getName()));
-// }
-// }
-//
-// StringBuilder reason = new StringBuilder();
-// reason.append(StringUtils.remove(context.getArg(), FormatUtils.format(mentionedUsers, user -> user.mention(false), " ")).trim());
-// if(reason.length() > Ban.MAX_REASON_LENGTH) {
-// throw new IllegalCmdArgumentException(String.format("Reason cannot exceed **%d characters**.", Ban.MAX_REASON_LENGTH));
-// }
-//
-// if(reason.length() == 0) {
-// reason.append("Reason not specified.");
-// }
-//
-// for(IUser user : mentionedUsers) {
-// if(!user.isBot()) {
-// BotUtils.sendMessage(String.format(Emoji.INFO + " You were banned from the server **%s** by **%s**. Reason: `%s`",
-// context.getGuild().getName(), context.getUsername(), reason), user.getOrCreatePMChannel());
-// }
-// RequestBuffer.request(() -> {
-// context.getGuild().banUser(user, reason.toString(), 7);
-// }).get();
-// }
-//
-// BotUtils.sendMessage(String.format(Emoji.INFO + " (Requested by **%s**) **%s** got banned. Reason: `%s`",
-// context.getUsername(), FormatUtils.format(mentionedUsers, IUser::getName, ", "), reason), context.getChannel());
-// }
-//
-// @Override
-// public EmbedObject getHelp(String prefix) {
-// return new HelpBuilder(this, prefix)
-// .setDescription("Ban user(s) and delete his/their messages from the last 7 days.")
-// .addArg("@user(s)", false)
-// .addArg("reason", true)
-// .build();
-// }
-//
-// }
+package me.shadorc.shadbot.command.admin;
+
+import java.util.Set;
+
+import discord4j.core.object.entity.MessageChannel;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.util.Permission;
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.core.command.AbstractCommand;
+import me.shadorc.shadbot.core.command.CommandCategory;
+import me.shadorc.shadbot.core.command.CommandPermission;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.command.annotation.Command;
+import me.shadorc.shadbot.exception.IllegalCmdArgumentException;
+import me.shadorc.shadbot.exception.MissingArgumentException;
+import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.DiscordUtils;
+import me.shadorc.shadbot.utils.ExceptionUtils;
+import me.shadorc.shadbot.utils.FormatUtils;
+import me.shadorc.shadbot.utils.StringUtils;
+import me.shadorc.shadbot.utils.TextUtils;
+import me.shadorc.shadbot.utils.embed.HelpBuilder;
+import me.shadorc.shadbot.utils.embed.log.LogUtils;
+import me.shadorc.shadbot.utils.object.Emoji;
+
+@Command(category = CommandCategory.ADMIN, permission = CommandPermission.ADMIN, names = { "ban" })
+public class BanCmd extends AbstractCommand {
+
+	@Override
+	public void execute(Context context) throws MissingArgumentException, IllegalCmdArgumentException {
+		context.requireArg();
+
+		Set<Snowflake> mentionedUserIds = context.getMessage().getUserMentionIds();
+		if(mentionedUserIds.isEmpty()) {
+			throw new MissingArgumentException();
+		}
+
+		DiscordUtils.hasPermissions(context.getMember(), Permission.BAN_MEMBERS)
+				.subscribe(canUserBan -> {
+
+					if(!canUserBan) {
+						throw new IllegalArgumentException("You don't have permission to ban.");
+					}
+
+					final Snowflake guildId = context.getGuildId().get();
+
+					DiscordUtils.hasPermissions(context.getSelf(), guildId, Permission.BAN_MEMBERS)
+							.subscribe(canBotBan -> {
+
+								if(!canBotBan) {
+									BotUtils.sendMessage(TextUtils.missingPerm(Permission.BAN_MEMBERS), context.getChannel());
+									return;
+								}
+
+								if(mentionedUserIds.contains(context.getAuthorId())) {
+									throw new IllegalCmdArgumentException("You cannot ban yourself.");
+								}
+
+								context.getMessage().getUserMentions().buffer().subscribe(mentionedUsers -> {
+									StringBuilder reason = new StringBuilder();
+									reason.append(StringUtils.remove(context.getArg().get(), FormatUtils.format(mentionedUsers, User::getMention, " ")).trim());
+									if(reason.length() > DiscordUtils.MAX_REASON_LENGTH) {
+										throw new IllegalCmdArgumentException(String.format("Reason cannot exceed **%d characters**.", DiscordUtils.MAX_REASON_LENGTH));
+									}
+
+									if(reason.length() == 0) {
+										reason.append("Reason not specified.");
+									}
+
+									context.getGuild().subscribe(guild -> {
+										context.getAuthor().map(User::getUsername).subscribe(authorName -> {
+
+											for(User user : mentionedUsers) {
+												if(!user.isBot()) {
+													BotUtils.sendMessage(String.format(Emoji.INFO + " You were banned from the server **%s** by **%s**. Reason: `%s`",
+															guild.getName(), authorName, reason), user.getPrivateChannel().cast(MessageChannel.class));
+												}
+
+												// TODO: Add reason
+												user.asMember(guildId)
+														.doOnError(ExceptionUtils::isForbidden, err -> {
+															BotUtils.sendMessage(TextUtils.missingPerm(Permission.BAN_MEMBERS), context.getChannel());
+															LogUtils.infof("{Guild ID: %d} Shadbot wasn't allowed to ban a member.", guildId);
+														})
+														.subscribe(/* TODO Member::ban */);
+											}
+
+											BotUtils.sendMessage(String.format(Emoji.INFO + " (Requested by **%s**) **%s** got banned. Reason: `%s`",
+													authorName, FormatUtils.format(mentionedUsers, User::getUsername, ", "), reason), context.getChannel());
+										});
+									});
+								});
+							});
+				});
+
+	}
+
+	@Override
+	public EmbedCreateSpec getHelp(String prefix) {
+		return new HelpBuilder(this, prefix)
+				.setDescription("Ban user(s) and delete his/their messages from the last 7 days.")
+				.addArg("@user(s)", false)
+				.addArg("reason", true)
+				.build();
+	}
+
+}
