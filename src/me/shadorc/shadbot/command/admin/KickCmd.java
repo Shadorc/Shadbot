035576500d9569f2b0256ab60db2ea1bde86ca98
David <bi-crosseur@hotmail.fr>
2018-06-09 16:40:57 +0200

    Fix MickCmd

--- src/me/shadorc/shadbot/command/admin/KickCmd.java
+++ src/me/shadorc/shadbot/command/admin/KickCmd.java
@@ -2,6 +2,7 @@ package me.shadorc.shadbot.command.admin;
 
 import java.util.Set;
 
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.MessageChannel;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Permission;
@@ -20,7 +21,6 @@ import me.shadorc.shadbot.utils.ExceptionUtils;
 import me.shadorc.shadbot.utils.FormatUtils;
 import me.shadorc.shadbot.utils.StringUtils;
 import me.shadorc.shadbot.utils.TextUtils;
-import me.shadorc.shadbot.utils.Utils;
 import me.shadorc.shadbot.utils.embed.HelpBuilder;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
 import me.shadorc.shadbot.utils.object.Emoji;
@@ -39,79 +39,64 @@ public class KickCmd extends AbstractCommand {
 
 		Snowflake guildId = context.getGuildId().get();
 
-		DiscordUtils.hasPermissions(context.getAuthor(), guildId, Permission.KICK_MEMBERS).subscribe(canUserKick -> {
+		DiscordUtils.hasPermissions(context.getMember(), Permission.KICK_MEMBERS)
+				.subscribe(canUserKick -> {
 
-			if(!canUserKick) {
-				throw new IllegalArgumentException("You don't have permission to kick.");
-			}
-
-			DiscordUtils.hasPermissions(context.getSelf(), guildId, Permission.KICK_MEMBERS).subscribe(canBotKick -> {
-
-				if(!canBotKick) {
-					BotUtils.sendMessage(TextUtils.missingPerm(Permission.KICK_MEMBERS), context.getMessageChannel());
-					return;
-				}
-
-				if(mentionedUserIds.contains(context.getAuthorId())) {
-					throw new IllegalCmdArgumentException("You cannot kick yourself.");
-				}
-
-				// TODO
-				// for(IUser mentionedUser : mentionedUsers) {
-				// if(!PermissionUtils.isUserHigher(context.getGuild(), context.getAuthor(), mentionedUser)) {
-				// throw new IllegalCmdArgumentException(String.format("You can't kick **%s** because he has the same or a higher role "
-				// + "position than you in the role hierarchy.",
-				// mentionedUser.getName()));
-				// }
-				// if(!BotUtils.canInteract(context.getGuild(), mentionedUser)) {
-				// throw new IllegalCmdArgumentException(String.format("I cannot kick **%s** because he has the same or a higher role "
-				// + "position than me in the role hierarchy.",
-				// mentionedUser.getName()));
-				// }
-				// }
-
-				context.getMessage().getUserMentions().buffer().subscribe(mentionedUsers -> {
-
-					StringBuilder reason = new StringBuilder();
-					reason.append(StringUtils.remove(context.getArg().get(),
-							FormatUtils.format(mentionedUsers, User::getMention, " ")).trim());
-					if(reason.length() > Utils.MAX_REASON_LENGTH) {
-						throw new IllegalCmdArgumentException(String.format("Reason cannot exceed **%d characters**.", Utils.MAX_REASON_LENGTH));
+					if(!canUserKick) {
+						throw new IllegalArgumentException("You don't have permission to kick.");
 					}
 
-					if(reason.length() == 0) {
-						reason.append("Reason not specified.");
-					}
+					DiscordUtils.hasPermissions(context.getSelf(), guildId, Permission.KICK_MEMBERS).subscribe(canBotKick -> {
 
-					context.getGuild().subscribe(guild -> {
-						context.getAuthor().map(User::getUsername).subscribe(authorName -> {
+						if(!canBotKick) {
+							BotUtils.sendMessage(TextUtils.missingPerm(Permission.KICK_MEMBERS), context.getChannel());
+							return;
+						}
 
-							for(User user : mentionedUsers) {
-								if(!user.isBot()) {
-									BotUtils.sendMessage(String.format(Emoji.INFO + " You were kicked from the server **%s** by **%s**. Reason: `%s`",
-											guild.getName(), authorName, reason), user.getPrivateChannel().cast(MessageChannel.class));
-								}
+						if(mentionedUserIds.contains(context.getAuthorId())) {
+							throw new IllegalCmdArgumentException("You cannot kick yourself.");
+						}
 
-								// TODO: Add reason
-								context.getMember()
-										.doOnError(ExceptionUtils::isForbidden, err -> {
-											BotUtils.sendMessage(TextUtils.missingPerm(Permission.KICK_MEMBERS), context.getMessageChannel());
-											LogUtils.infof("{Guild ID: %d} Shadbot wasn't allowed to kick a member.", guildId);
-										})
-										.subscribe(member -> {
-											member.kick();
-										});
+						context.getMessage().getUserMentions().buffer().subscribe(mentionedUsers -> {
+
+							StringBuilder reason = new StringBuilder();
+							reason.append(StringUtils.remove(context.getArg().get(),
+									FormatUtils.format(mentionedUsers, User::getMention, " ")).trim());
+							if(reason.length() > DiscordUtils.MAX_REASON_LENGTH) {
+								throw new IllegalCmdArgumentException(String.format("Reason cannot exceed **%d characters**.", DiscordUtils.MAX_REASON_LENGTH));
 							}
 
-							BotUtils.sendMessage(String.format(Emoji.INFO + " (Requested by **%s**) **%s** got kicked. Reason: `%s`",
-									authorName,
-									FormatUtils.format(mentionedUsers, User::getUsername, ", "),
-									reason), context.getMessageChannel());
+							if(reason.length() == 0) {
+								reason.append("Reason not specified.");
+							}
+
+							context.getGuild().subscribe(guild -> {
+								context.getAuthor().map(User::getUsername).subscribe(authorName -> {
+
+									for(User user : mentionedUsers) {
+										if(!user.isBot()) {
+											BotUtils.sendMessage(String.format(Emoji.INFO + " You were kicked from the server **%s** by **%s**. Reason: `%s`",
+													guild.getName(), authorName, reason), user.getPrivateChannel().cast(MessageChannel.class));
+										}
+
+										// TODO: Add reason
+										user.asMember(guildId)
+												.doOnError(ExceptionUtils::isForbidden, err -> {
+													BotUtils.sendMessage(TextUtils.missingPerm(Permission.KICK_MEMBERS), context.getChannel());
+													LogUtils.infof("{Guild ID: %d} Shadbot wasn't allowed to kick a member.", guildId);
+												})
+												.subscribe(Member::kick);
+									}
+
+									BotUtils.sendMessage(String.format(Emoji.INFO + " (Requested by **%s**) **%s** got kicked. Reason: `%s`",
+											authorName,
+											FormatUtils.format(mentionedUsers, User::getUsername, ", "),
+											reason), context.getChannel());
+								});
+							});
 						});
 					});
 				});
-			});
-		});
 	}
 
 	@Override
