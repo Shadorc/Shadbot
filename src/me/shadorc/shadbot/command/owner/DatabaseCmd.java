b7419357a8776e8552303b92c39893b072d28645
David <bi-crosseur@hotmail.fr>
2018-06-16 23:34:32 +0200

    Fix DatabaseCmd

--- src/me/shadorc/shadbot/command/owner/DatabaseCmd.java
+++ src/me/shadorc/shadbot/command/owner/DatabaseCmd.java
@@ -1,7 +1,9 @@
 package me.shadorc.shadbot.command.owner;
 
 import java.util.List;
+import java.util.Optional;
 
+import discord4j.core.object.entity.Guild;
 import discord4j.core.object.util.Snowflake;
 import discord4j.core.spec.EmbedCreateSpec;
 import me.shadorc.shadbot.Config;
@@ -26,37 +28,42 @@ public class DatabaseCmd extends AbstractCommand {
 	public void execute(Context context) {
 		List<String> args = context.requireArgs(2);
 
-		Long guildId = NumberUtils.asPositiveLong(args.get(0));
+		final Long guildId = NumberUtils.asPositiveLong(args.get(0));
 		if(guildId == null) {
 			throw new IllegalCmdArgumentException(String.format("`%s` is not a valid guild ID.", args.get(0)));
 		}
 
-		context.getClient().getGuildById(Snowflake.of(guildId)).defaultIfEmpty(null).subscribe(guild -> {
-			if(guild == null) {
-				throw new IllegalCmdArgumentException("Guild not found.");
-			}
+		context.getClient().getGuildById(Snowflake.of(guildId))
+				.map(Optional::of)
+				.defaultIfEmpty(Optional.empty())
+				.subscribe(optGuild -> {
+					if(!optGuild.isPresent()) {
+						throw new IllegalCmdArgumentException("Guild not found.");
+					}
+					
+					final Guild guild = optGuild.get();
 
-			String json = null;
-			if(args.size() == 1) {
-				DBGuild dbGuild = Database.getDBGuild(guild.getId());
-				json = dbGuild.toJSON().toString(Config.JSON_INDENT_FACTOR);
+					String json = null;
+					if(args.size() == 1) {
+						DBGuild dbGuild = Database.getDBGuild(guild.getId());
+						json = dbGuild.toJSON().toString(Config.JSON_INDENT_FACTOR);
 
-			} else if(args.size() == 2) {
-				Long userId = NumberUtils.asPositiveLong(args.get(1));
-				if(userId == null) {
-					throw new IllegalCmdArgumentException(String.format("`%s` is not a valid user ID.", args.get(0)));
-				}
+					} else if(args.size() == 2) {
+						Long userId = NumberUtils.asPositiveLong(args.get(1));
+						if(userId == null) {
+							throw new IllegalCmdArgumentException(String.format("`%s` is not a valid user ID.", args.get(0)));
+						}
 
-				DBMember dbUser = new DBMember(guild.getId(), Snowflake.of(userId));
-				json = dbUser.toJSON().toString(Config.JSON_INDENT_FACTOR);
-			}
+						DBMember dbUser = new DBMember(guild.getId(), Snowflake.of(userId));
+						json = dbUser.toJSON().toString(Config.JSON_INDENT_FACTOR);
+					}
 
-			if(json == null || json.length() == 2) {
-				BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " Nothing found.", context.getChannel());
-			} else {
-				BotUtils.sendMessage(json, context.getChannel());
-			}
-		});
+					if(json == null || json.length() == 2) {
+						BotUtils.sendMessage(Emoji.MAGNIFYING_GLASS + " Nothing found.", context.getChannel());
+					} else {
+						BotUtils.sendMessage(json, context.getChannel());
+					}
+				});
 	}
 
 	@Override
