aa3ece8fab0a89556806ad444d9482574284b783
David <bi-crosseur@hotmail.fr>
2018-07-10 13:29:30 +0200

    Comment non-working commands

--- src/me/shadorc/shadbot/command/gamestats/FortniteCmd.java
+++ src/me/shadorc/shadbot/command/gamestats/FortniteCmd.java
@@ -1,179 +1,180 @@
-//TODO
-//package me.shadorc.shadbot.command.gamestats;
+// TODO
+// package me.shadorc.shadbot.command.gamestats;
 //
-//import java.io.IOException;
-//import java.net.URL;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
+// import java.io.IOException;
+// import java.net.URL;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
 //
-//import org.apache.http.HttpStatus;
-//import org.json.JSONObject;
-//import org.jsoup.Connection.Method;
-//import org.jsoup.Connection.Response;
-//import org.jsoup.HttpStatusException;
-//import org.jsoup.Jsoup;
+// import org.apache.http.HttpStatus;
+// import org.json.JSONObject;
+// import org.jsoup.Connection.Method;
+// import org.jsoup.Connection.Response;
+// import org.jsoup.HttpStatusException;
+// import org.jsoup.Jsoup;
 //
-//import discord4j.core.spec.EmbedCreateSpec;
-//import me.shadorc.shadbot.core.command.AbstractCommand;
-//import me.shadorc.shadbot.core.command.CommandCategory;
-//import me.shadorc.shadbot.core.command.Context;
-//import me.shadorc.shadbot.core.command.annotation.Command;
-//import me.shadorc.shadbot.core.command.annotation.RateLimited;
-//import me.shadorc.shadbot.data.APIKeys;
-//import me.shadorc.shadbot.data.APIKeys.APIKey;
-//import me.shadorc.shadbot.exception.CommandException;
-//import me.shadorc.shadbot.utils.FormatUtils;
-//import me.shadorc.shadbot.utils.NetUtils;
-//import me.shadorc.shadbot.utils.Utils;
-//import me.shadorc.shadbot.utils.command.Emoji;
-//import me.shadorc.shadbot.utils.embed.EmbedUtils;
-//import me.shadorc.shadbot.utils.embed.HelpBuilder;
-//import me.shadorc.shadbot.utils.message.LoadingMessage;
-//import reactor.core.Exceptions;
-//import reactor.core.publisher.Mono;
+// import discord4j.core.spec.EmbedCreateSpec;
+// import me.shadorc.shadbot.core.command.AbstractCommand;
+// import me.shadorc.shadbot.core.command.CommandCategory;
+// import me.shadorc.shadbot.core.command.Context;
+// import me.shadorc.shadbot.core.command.annotation.Command;
+// import me.shadorc.shadbot.core.command.annotation.RateLimited;
+// import me.shadorc.shadbot.data.APIKeys;
+// import me.shadorc.shadbot.data.APIKeys.APIKey;
+// import me.shadorc.shadbot.exception.CommandException;
+// import me.shadorc.shadbot.utils.FormatUtils;
+// import me.shadorc.shadbot.utils.NetUtils;
+// import me.shadorc.shadbot.utils.Utils;
+// import me.shadorc.shadbot.utils.command.Emoji;
+// import me.shadorc.shadbot.utils.embed.EmbedUtils;
+// import me.shadorc.shadbot.utils.embed.HelpBuilder;
+// import me.shadorc.shadbot.utils.message.LoadingMessage;
+// import reactor.core.Exceptions;
+// import reactor.core.publisher.Mono;
 //
-//@RateLimited
-//@Command(category = CommandCategory.GAMESTATS, names = { "fortnite" })
-//public class FortniteCmd extends AbstractCommand {
+// @RateLimited
+// @Command(category = CommandCategory.GAMESTATS, names = { "fortnite" })
+// public class FortniteCmd extends AbstractCommand {
 //
-//	private final static String SOLO_STATS = "p2";
-//	private final static String DUO_STATS = "p10";
-//	private final static String SQUAD_STATS = "p9";
-//	private final static String SEASON = "curr_";
+// private final static String SOLO_STATS = "p2";
+// private final static String DUO_STATS = "p10";
+// private final static String SQUAD_STATS = "p9";
+// private final static String SEASON = "curr_";
 //
-//	private enum Platform {
-//		PC, XBL, PSN;
-//	}
+// private enum Platform {
+// PC, XBL, PSN;
+// }
 //
-//	private enum Stats {
-//		TOP1_SOLO,
-//		TOP1_DUO,
-//		TOP1_SQUAD,
-//		KD_S3_SOLO,
-//		KD_S3_DUO,
-//		KD_S3_SQUAD,
-//		KD_LT_SOLO,
-//		KD_LT_DUO,
-//		KD_LT_SQUAD;
-//	}
+// private enum Stats {
+// TOP1_SOLO,
+// TOP1_DUO,
+// TOP1_SQUAD,
+// KD_S3_SOLO,
+// KD_S3_DUO,
+// KD_S3_SQUAD,
+// KD_LT_SOLO,
+// KD_LT_DUO,
+// KD_LT_SQUAD;
+// }
 //
-//	@Override
-//	public Mono<Void> execute(Context context) {
-//		final List<String> args = context.requireArgs(2);
+// @Override
+// public Mono<Void> execute(Context context) {
+// final List<String> args = context.requireArgs(2);
 //
-//		Platform platform = Utils.getValueOrNull(Platform.class, args.get(0));
-//		if(platform == null) {
-//			throw new CommandException(String.format("`%s` is not a valid Platform. %s",
-//					args.get(0), FormatUtils.formatOptions(Platform.class)));
-//		}
+// Platform platform = Utils.getValueOrNull(Platform.class, args.get(0));
+// if(platform == null) {
+// throw new CommandException(String.format("`%s` is not a valid Platform. %s",
+// args.get(0), FormatUtils.formatOptions(Platform.class)));
+// }
 //
-//		final String epicNickname = args.get(1);
+// final String epicNickname = args.get(1);
 //
-//		LoadingMessage loadingMsg = new LoadingMessage(context.getClient(), context.getChannelId());
+// LoadingMessage loadingMsg = new LoadingMessage(context.getClient(), context.getChannelId());
 //
-//		context.getAuthorAvatarUrl()
-//				.subscribe(avatarUrl -> {
-//					try {
-//						final URL url = new URL(String.format("https://api.fortnitetracker.com/v1/profile/%s/%s",
-//								platform.toString().toLowerCase(), epicNickname));
+// context.getAuthorAvatarUrl()
+// .subscribe(avatarUrl -> {
+// try {
+// final URL url = new URL(String.format("https://api.fortnitetracker.com/v1/profile/%s/%s",
+// platform.toString().toLowerCase(), epicNickname));
 //
-//						Response response = Jsoup.connect(url.toString())
-//								.method(Method.GET)
-//								.ignoreContentType(true)
-//								.ignoreHttpErrors(true)
-//								.header("TRN-Api-Key", APIKeys.get(APIKey.FORTNITE_API_KEY))
-//								.execute();
+// Response response = Jsoup.connect(url.toString())
+// .method(Method.GET)
+// .ignoreContentType(true)
+// .ignoreHttpErrors(true)
+// .header("TRN-Api-Key", APIKeys.get(APIKey.FORTNITE_API_KEY))
+// .execute();
 //
-//						if(response.statusCode() != 200) {
-//							throw new HttpStatusException("Fortnite API did not return a valid status code.", HttpStatus.SC_SERVICE_UNAVAILABLE, url.toString());
-//						}
+// if(response.statusCode() != 200) {
+// throw new HttpStatusException("Fortnite API did not return a valid status code.", HttpStatus.SC_SERVICE_UNAVAILABLE, url.toString());
+// }
 //
-//						JSONObject mainObj = new JSONObject(response.parse().body().html());
+// JSONObject mainObj = new JSONObject(response.parse().body().html());
 //
-//						System.err.println(mainObj.toString(2));
+// System.err.println(mainObj.toString(2));
 //
-//						// Useful: http://pojo.sodhanalibrary.com/
-//						if("Player Not Found".equals(mainObj.optString("error"))) {
-//							loadingMsg.send(Emoji.MAGNIFYING_GLASS + " This user doesn't play Fortnite on this platform or doesn't exist.");
-//						}
+// // Useful: http://pojo.sodhanalibrary.com/
+// if("Player Not Found".equals(mainObj.optString("error"))) {
+// loadingMsg.send(Emoji.MAGNIFYING_GLASS + " This user doesn't play Fortnite on this platform or doesn't exist.");
+// }
 //
-//						JSONObject statsObj = mainObj.getJSONObject("stats");
+// JSONObject statsObj = mainObj.getJSONObject("stats");
 //
-//						Map<Stats, String> statsMap = new HashMap<>();
+// Map<Stats, String> statsMap = new HashMap<>();
 //
-//						JSONObject soloStatsObj = statsObj.optJSONObject(SOLO_STATS);
-//						statsMap.put(Stats.TOP1_SOLO, this.getTop1(soloStatsObj));
-//						statsMap.put(Stats.KD_LT_SOLO, this.getKD(soloStatsObj));
+// JSONObject soloStatsObj = statsObj.optJSONObject(SOLO_STATS);
+// statsMap.put(Stats.TOP1_SOLO, this.getTop1(soloStatsObj));
+// statsMap.put(Stats.KD_LT_SOLO, this.getKD(soloStatsObj));
 //
-//						JSONObject duoStatsObj = statsObj.optJSONObject(DUO_STATS);
-//						statsMap.put(Stats.TOP1_DUO, this.getTop1(duoStatsObj));
-//						statsMap.put(Stats.KD_LT_DUO, this.getKD(duoStatsObj));
+// JSONObject duoStatsObj = statsObj.optJSONObject(DUO_STATS);
+// statsMap.put(Stats.TOP1_DUO, this.getTop1(duoStatsObj));
+// statsMap.put(Stats.KD_LT_DUO, this.getKD(duoStatsObj));
 //
-//						JSONObject squadStatsObj = statsObj.optJSONObject(SQUAD_STATS);
-//						statsMap.put(Stats.TOP1_SQUAD, this.getTop1(squadStatsObj));
-//						statsMap.put(Stats.KD_LT_SQUAD, this.getKD(squadStatsObj));
+// JSONObject squadStatsObj = statsObj.optJSONObject(SQUAD_STATS);
+// statsMap.put(Stats.TOP1_SQUAD, this.getTop1(squadStatsObj));
+// statsMap.put(Stats.KD_LT_SQUAD, this.getKD(squadStatsObj));
 //
-//						JSONObject soloSeasonStatsObj = statsObj.optJSONObject(SEASON + SOLO_STATS);
-//						statsMap.put(Stats.KD_S3_SOLO, this.getKD(soloSeasonStatsObj));
+// JSONObject soloSeasonStatsObj = statsObj.optJSONObject(SEASON + SOLO_STATS);
+// statsMap.put(Stats.KD_S3_SOLO, this.getKD(soloSeasonStatsObj));
 //
-//						JSONObject duoSeasonStatsObj = statsObj.optJSONObject(SEASON + DUO_STATS);
-//						statsMap.put(Stats.KD_S3_DUO, this.getKD(duoSeasonStatsObj));
+// JSONObject duoSeasonStatsObj = statsObj.optJSONObject(SEASON + DUO_STATS);
+// statsMap.put(Stats.KD_S3_DUO, this.getKD(duoSeasonStatsObj));
 //
-//						JSONObject squadSeasonStatsObj = statsObj.optJSONObject(SEASON + SQUAD_STATS);
-//						statsMap.put(Stats.KD_S3_SQUAD, this.getKD(squadSeasonStatsObj));
+// JSONObject squadSeasonStatsObj = statsObj.optJSONObject(SEASON + SQUAD_STATS);
+// statsMap.put(Stats.KD_S3_SQUAD, this.getKD(squadSeasonStatsObj));
 //
-//						int length = 10;
-//						String format = "%n%-" + (length + 5) + "s %-" + length + "s %-" + length + "s %-" + length + "s";
+// int length = 10;
+// String format = "%n%-" + (length + 5) + "s %-" + length + "s %-" + length + "s %-" + length + "s";
 //
-//						String description = String.format("Stats for user **%s**%n", epicNickname)
-//								+ "```prolog"
-//								+ String.format(format, " ", "Solo", "Duo", "Squad")
-//								+ String.format(format, "Top 1", statsMap.get(Stats.TOP1_SOLO), statsMap.get(Stats.TOP1_DUO), statsMap.get(Stats.TOP1_SQUAD))
-//								+ String.format(format, "K/D season", statsMap.get(Stats.KD_S3_SOLO), statsMap.get(Stats.KD_S3_DUO), statsMap.get(Stats.KD_S3_SQUAD))
-//								+ String.format(format, "K/D lifetime", statsMap.get(Stats.KD_LT_SOLO), statsMap.get(Stats.KD_LT_DUO), statsMap.get(Stats.KD_LT_SQUAD))
-//								+ "```";
+// String description = String.format("Stats for user **%s**%n", epicNickname)
+// + "```prolog"
+// + String.format(format, " ", "Solo", "Duo", "Squad")
+// + String.format(format, "Top 1", statsMap.get(Stats.TOP1_SOLO), statsMap.get(Stats.TOP1_DUO), statsMap.get(Stats.TOP1_SQUAD))
+// + String.format(format, "K/D season", statsMap.get(Stats.KD_S3_SOLO), statsMap.get(Stats.KD_S3_DUO), statsMap.get(Stats.KD_S3_SQUAD))
+// + String.format(format, "K/D lifetime", statsMap.get(Stats.KD_LT_SOLO), statsMap.get(Stats.KD_LT_DUO), statsMap.get(Stats.KD_LT_SQUAD))
+// + "```";
 //
-//						EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
-//								.setAuthor("Fortnite Stats",
-//										String.format("https://fortnitetracker.com/profile/%s/%s", platform.toString().toLowerCase(), NetUtils.encode(epicNickname)),
-//										avatarUrl)
-//								.setThumbnail("https://orig00.deviantart.net/9517/f/2017/261/9/f/fortnite___icon_by_blagoicons-dbnu8a0.png")
-//								.setDescription(description);
-//						loadingMsg.send(embed);
+// EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
+// .setAuthor("Fortnite Stats",
+// String.format("https://fortnitetracker.com/profile/%s/%s", platform.toString().toLowerCase(), NetUtils.encode(epicNickname)),
+// avatarUrl)
+// .setThumbnail("https://orig00.deviantart.net/9517/f/2017/261/9/f/fortnite___icon_by_blagoicons-dbnu8a0.png")
+// .setDescription(description);
+// loadingMsg.send(embed);
 //
-//					} catch (IOException err) {
-//						loadingMsg.stopTyping();
-//						throw Exceptions.propagate(err);
-//					}
-//				});
+// } catch (IOException err) {
+// loadingMsg.stopTyping();
+// throw Exceptions.propagate(err);
+// }
+// });
 //
-//		return Mono.empty();
+// return Mono.empty();
 //
-//	}
+// }
 //
-//	private String getTop1(JSONObject statsObj) {
-//		if(statsObj == null) {
-//			return "0";
-//		}
-//		return Integer.toString(statsObj.getJSONObject("top1").getInt("valueInt"));
-//	}
+// private String getTop1(JSONObject statsObj) {
+// if(statsObj == null) {
+// return "0";
+// }
+// return Integer.toString(statsObj.getJSONObject("top1").getInt("valueInt"));
+// }
 //
-//	private String getKD(JSONObject statsObj) {
-//		if(statsObj == null) {
-//			return "0";
-//		}
-//		return Double.toString(statsObj.getJSONObject("kd").getDouble("valueDec"));
-//	}
+// private String getKD(JSONObject statsObj) {
+// if(statsObj == null) {
+// return "0";
+// }
+// return Double.toString(statsObj.getJSONObject("kd").getDouble("valueDec"));
+// }
 //
-//	@Override
-//	public Mono<EmbedCreateSpec> getHelp(Context context) {
-//		return new HelpBuilder(this, context)
-//				.setDescription("Show player's stats for Fortnite.")
-//				.addArg("platform", String.format("user's platform (%s)", FormatUtils.format(Platform.values(), region -> region.toString().toLowerCase(), ", ")), false)
-//				.addArg("epic-nickname", false)
-//				.setExample(String.format("`%s%s pc Shadbot`", context.getPrefix(), this.getName()))
-//				.build();
-//	}
+// @Override
+// public Mono<EmbedCreateSpec> getHelp(Context context) {
+// return new HelpBuilder(this, context)
+// .setDescription("Show player's stats for Fortnite.")
+// .addArg("platform", String.format("user's platform (%s)", FormatUtils.format(Platform.values(), region -> region.toString().toLowerCase(), ", ")),
+// false)
+// .addArg("epic-nickname", false)
+// .setExample(String.format("`%s%s pc Shadbot`", context.getPrefix(), this.getName()))
+// .build();
+// }
 //
-//}
+// }
