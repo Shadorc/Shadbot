5f04e21575fcae180adc62c6cd01161258abb12a
David <bi-crosseur@hotmail.fr>
2018-07-18 16:14:21 +0200

    RouletteCmd is back

--- src/me/shadorc/shadbot/command/game/roulette/RouletteCmd.java
+++ src/me/shadorc/shadbot/command/game/roulette/RouletteCmd.java
@@ -1,76 +1,74 @@
-// TODO
-// package me.shadorc.shadbot.command.game.roulette;
-//
-// import java.util.List;
-// import java.util.concurrent.ConcurrentHashMap;
-//
-// import me.shadorc.shadbot.core.command.AbstractCommand;
-// import me.shadorc.shadbot.core.command.CommandCategory;
-// import me.shadorc.shadbot.core.command.Context;
-// import me.shadorc.shadbot.core.command.annotation.Command;
-// import me.shadorc.shadbot.core.command.annotation.RateLimited;
-// import me.shadorc.shadbot.exception.IllegalCmdArgumentException;
-// import me.shadorc.shadbot.exception.MissingArgumentException;
-// import me.shadorc.shadbot.ratelimiter.RateLimiter;
-// import me.shadorc.shadbot.utils.BotUtils;
-// import me.shadorc.shadbot.utils.FormatUtils;
-// import me.shadorc.shadbot.utils.StringUtils;
-// import me.shadorc.shadbot.utils.Utils;
-// import me.shadorc.shadbot.utils.embed.HelpBuilder;
-// import me.shadorc.shadbot.utils.object.Emoji;
-//
-// @RateLimited(cooldown = RateLimiter.GAME_COOLDOWN, max = 1)
-// @Command(category = CommandCategory.GAME, names = { "roulette" })
-// public class RouletteCmd extends AbstractCommand {
-//
-// public enum Place {
-// RED, BLACK, ODD, EVEN, LOW, HIGH;
-// }
-//
-// protected static final ConcurrentHashMap<Long, RouletteManager> MANAGERS = new ConcurrentHashMap<>();
-//
-// private static final int MAX_BET = 250_000;
-//
-// @Override
-// public void execute(Context context) {
-// List<String> args = context.requireArgs(2);
-//
-// Integer bet = Utils.checkAndGetBet(context.getChannel(), context.getAuthor(), splitArgs.get(0), MAX_BET);
-// if(bet == null) {
-// return;
-// }
-//
-// String place = splitArgs.get(1).toLowerCase();
-// // Match [1-36], red, black, odd, even, high or low
-// if(!place.matches("^([1-9]|1[0-9]|2[0-9]|3[0-6])$") && Utils.getValueOrNull(Place.class, place) == null) {
-// throw new IllegalCmdArgumentException(String.format("`%s` is not a valid place, must be a number between **1 and 36**, %s.",
-// place, FormatUtils.format(Place.values(), value -> String.format("**%s**", value.toString().toLowerCase()), ", ")));
-// }
-//
-// RouletteManager rouletteManager = MANAGERS.get(context.getChannel().getLongID());
-// if(rouletteManager == null) {
-// rouletteManager = new RouletteManager(this, context.getPrefix(), context.getChannel(), context.getAuthor());
-// }
-//
-// if(MANAGERS.putIfAbsent(context.getChannel().getLongID(), rouletteManager) == null) {
-// rouletteManager.start();
-// }
-//
-// if(!rouletteManager.addPlayer(context.getAuthor(), bet, place)) {
-// BotUtils.sendMessage(String.format(Emoji.INFO + " (**%s**) You're already participating.",
-// context.getUsername()), context.getChannel());
-// }
-// }
-//
-// @Override
-// public EmbedObject getHelp(String prefix) {
-// return new HelpBuilder(this, context)
-// .setDescription("Play a roulette game in which everyone can participate.")
-// .addArg("bet", false)
-// .addArg("place", String.format("number between 1 and 36, %s",
-// FormatUtils.format(Place.values(), value -> value.toString().toLowerCase(), ", ")), false)
-// .addField("Info", "**low** - numbers between 1 and 18"
-// + "\n**high** - numbers between 19 and 36", false)
-// .build();
-// }
-// }
+package me.shadorc.shadbot.command.game.roulette;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.core.command.AbstractCommand;
+import me.shadorc.shadbot.core.command.CommandCategory;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.command.annotation.Command;
+import me.shadorc.shadbot.core.command.annotation.RateLimited;
+import me.shadorc.shadbot.core.ratelimiter.RateLimiter;
+import me.shadorc.shadbot.exception.CommandException;
+import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.FormatUtils;
+import me.shadorc.shadbot.utils.Utils;
+import me.shadorc.shadbot.utils.command.Emoji;
+import me.shadorc.shadbot.utils.embed.HelpBuilder;
+import reactor.core.publisher.Mono;
+
+@RateLimited(cooldown = RateLimiter.GAME_COOLDOWN, max = 1)
+@Command(category = CommandCategory.GAME, names = { "roulette" })
+public class RouletteCmd extends AbstractCommand {
+
+	public enum Place {
+		RED, BLACK, ODD, EVEN, LOW, HIGH;
+	}
+
+	protected static final ConcurrentHashMap<Snowflake, RouletteManager> MANAGERS = new ConcurrentHashMap<>();
+
+	private static final int MAX_BET = 250_000;
+
+	@Override
+	public Mono<Void> execute(Context context) {
+		List<String> args = context.requireArgs(2);
+
+		final int bet = Utils.requireBet(context.getMember(), args.get(0), MAX_BET);
+
+		final String place = args.get(1).toLowerCase();
+
+		// Match [1-36], red, black, odd, even, high or low
+		if(!place.matches("^([1-9]|1[0-9]|2[0-9]|3[0-6])$") && Utils.getEnum(Place.class, place) == null) {
+			throw new CommandException(String.format("`%s` is not a valid place, must be a number between **1 and 36**, %s.",
+					place, FormatUtils.format(Place.values(), value -> String.format("**%s**", value.toString().toLowerCase()), ", ")));
+		}
+
+		RouletteManager rouletteManager = MANAGERS.putIfAbsent(context.getChannelId(), new RouletteManager(context));
+		if(rouletteManager == null) {
+			rouletteManager = MANAGERS.get(context.getChannelId());
+			rouletteManager.start();
+		}
+
+		if(rouletteManager.addPlayer(context.getAuthorId(), bet, place)) {
+			return rouletteManager.show();
+		} else {
+			return BotUtils.sendMessage(String.format(Emoji.INFO + " (**%s**) You're already participating.",
+					context.getUsername()), context.getChannel())
+					.then();
+		}
+	}
+
+	@Override
+	public Mono<EmbedCreateSpec> getHelp(Context context) {
+		return new HelpBuilder(this, context)
+				.setDescription("Play a roulette game in which everyone can participate.")
+				.addArg("bet", false)
+				.addArg("place", String.format("number between 1 and 36, %s",
+						FormatUtils.format(Place.values(), value -> value.toString().toLowerCase(), ", ")), false)
+				.addField("Info", "**low** - numbers between 1 and 18"
+						+ "\n**high** - numbers between 19 and 36", false)
+				.build();
+	}
+}
--- src/me/shadorc/shadbot/command/game/roulette/RouletteManager.java
+++ src/me/shadorc/shadbot/command/game/roulette/RouletteManager.java
@@ -1,130 +1,149 @@
-// package me.shadorc.shadbot.command.game.roulette;
-//
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.concurrent.ConcurrentHashMap;
-// import java.util.concurrent.ThreadLocalRandom;
-// import java.util.concurrent.TimeUnit;
-// import java.util.stream.Collectors;
-//
-// import me.shadorc.shadbot.core.command.AbstractCommand;
-// import me.shadorc.shadbot.core.game.AbstractGameManager;
-// import me.shadorc.shadbot.data.db.DatabaseManager;
-// import me.shadorc.shadbot.data.stats.MoneyStatsManager;
-// import me.shadorc.shadbot.data.stats.MoneyStatsManager.MoneyEnum;
-// import me.shadorc.shadbot.utils.BotUtils;
-// import me.shadorc.shadbot.utils.FormatUtils;
-// import me.shadorc.shadbot.utils.Utils;
-// import me.shadorc.shadbot.utils.command.Emoji;
-// import me.shadorc.shadbot.utils.embed.EmbedUtils;
-// import me.shadorc.shadbot.utils.message.UpdateableMessage;
-// import twitter4j.Place;
-//
-// public class RouletteManager extends AbstractGameManager {
-//
-// protected static final List<Integer> RED_NUMS = List.of(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36);
-//
-// private static final int GAME_DURATION = 30;
-//
-// // User, Pair<Bet, Place>
-// private final ConcurrentHashMap<IUser, Pair<Integer, String>> playersPlace;
-// private final UpdateableMessage message;
-//
-// private String results;
-//
-// public RouletteManager(AbstractCommand cmd, String prefix, IChannel channel, IUser author) {
-// super(cmd, prefix, channel, author);
-// this.playersPlace = new ConcurrentHashMap<>();
-// this.message = new UpdateableMessage(channel);
-// }
-//
-// @Override
-// public void start() {
-// this.schedule(() -> this.stop(), GAME_DURATION, TimeUnit.SECONDS);
-// }
-//
-// public void show() {
-// EmbedBuilder embed = EmbedUtils.getDefaultEmbed()
-// .setLenient(true)
-// .withAuthorName("Roulette Game")
-// .withThumbnail("http://icongal.com/gallery/image/278586/roulette_baccarat_casino.png")
-// .withDescription(String.format("**Use `%s%s <bet> <place>` to join the game.**"
-// + "%n%n**Place** is a `number between 1 and 36`, %s",
-// this.getPrefix(), this.getCmdName(),
-// FormatUtils.format(Place.values(), value -> String.format("`%s`", value.toString().toLowerCase()), ", ")))
-// .addField("Player (Bet)", FormatUtils.format(playersPlace.keySet().stream(),
-// user -> String.format("**%s** (%s)", user.getName(), FormatUtils.formatCoins(playersPlace.get(user).getFirst())), "\n"), true)
-// .addField("Place", playersPlace.values().stream().map(Pair::getSecond).collect(Collectors.joining("\n")), true)
-// .addField("Results", results, false)
-// .withFooterText(String.format("You have %d seconds to make your bets.", GAME_DURATION));
-//
-// RequestFuture<IMessage> msgRequest = message.send(embed.build());
-// if(msgRequest != null) {
-// msgRequest.get();
-// }
-// }
-//
-// @Override
-// public void stop() {
-// this.cancelScheduledTask();
-//
-// int winningPlace = ThreadLocalRandom.current().nextInt(1, 37);
-//
-// List<String> list = new ArrayList<>();
-// for(IUser user : playersPlace.keySet()) {
-// int gains = playersPlace.get(user).getFirst();
-// String place = playersPlace.get(user).getSecond();
-// Place placeEnum = Utils.getValueOrNull(Place.class, place);
-//
-// Map<Place, Boolean> testsMap = new HashMap<>();
-// testsMap.put(Place.RED, RED_NUMS.contains(winningPlace));
-// testsMap.put(Place.BLACK, !RED_NUMS.contains(winningPlace));
-// testsMap.put(Place.LOW, Utils.isInRange(winningPlace, 1, 19));
-// testsMap.put(Place.HIGH, Utils.isInRange(winningPlace, 19, 37));
-// testsMap.put(Place.EVEN, winningPlace % 2 == 0);
-// testsMap.put(Place.ODD, winningPlace % 2 != 0);
-//
-// int multiplier = 0;
-// if(place.equals(Integer.toString(winningPlace))) {
-// multiplier = 36;
-// } else if(placeEnum != null && testsMap.get(placeEnum)) {
-// multiplier = 2;
-// } else {
-// multiplier = -1;
-// }
-//
-// testsMap.clear();
-//
-// gains *= multiplier;
-// if(gains > 0) {
-// list.add(0, String.format("**%s** (Gains: **%s**)", user.getName(), FormatUtils.formatCoins(gains)));
-// MoneyStatsManager.log(MoneyEnum.MONEY_GAINED, this.getCmdName(), gains);
-// } else {
-// list.add(String.format("**%s** (Losses: **%s**)", user.getName(), FormatUtils.formatCoins(Math.abs(gains))));
-// MoneyStatsManager.log(MoneyEnum.MONEY_LOST, this.getCmdName(), Math.abs(gains));
-// }
-// DatabaseManager.getDBUser(this.getGuild(), user).addCoins(gains);
-// }
-//
-// BotUtils.sendMessage(String.format(Emoji.DICE + " No more bets. *The wheel is spinning...* **%d (%s)** !",
-// winningPlace, RED_NUMS.contains(winningPlace) ? "Red" : "Black"),
-// this.getMessageChannel()).get();
-//
-// this.results = FormatUtils.format(list, Object::toString, ", ");
-// this.show();
-//
-// playersPlace.clear();
-// RouletteCmd.MANAGERS.remove(this.getMessageChannel().getLongID());
-// }
-//
-// protected boolean addPlayer(IUser user, Integer bet, String place) {
-// if(playersPlace.putIfAbsent(user, new Pair<Integer, String>(bet, place)) == null) {
-// this.show();
-// return true;
-// }
-// return false;
-// }
-//
-// }
+package me.shadorc.shadbot.command.game.roulette;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import discord4j.core.object.entity.User;
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.command.game.roulette.RouletteCmd.Place;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.game.AbstractGameManager;
+import me.shadorc.shadbot.data.db.DatabaseManager;
+import me.shadorc.shadbot.data.stats.MoneyStatsManager;
+import me.shadorc.shadbot.data.stats.MoneyStatsManager.MoneyEnum;
+import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.FormatUtils;
+import me.shadorc.shadbot.utils.NumberUtils;
+import me.shadorc.shadbot.utils.Utils;
+import me.shadorc.shadbot.utils.command.Emoji;
+import me.shadorc.shadbot.utils.embed.EmbedUtils;
+import me.shadorc.shadbot.utils.message.UpdateableMessage;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+public class RouletteManager extends AbstractGameManager {
+
+	protected static final List<Integer> RED_NUMS = List.of(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36);
+
+	private static final int GAME_DURATION = 30;
+
+	// User ID, Tuple2<Bet, Place>
+	private final ConcurrentHashMap<Snowflake, Tuple2<Integer, String>> playersPlace;
+	private final UpdateableMessage updateableMessage;
+
+	private String results;
+
+	public RouletteManager(Context context) {
+		super(context);
+		this.playersPlace = new ConcurrentHashMap<>();
+		this.updateableMessage = new UpdateableMessage(context.getClient(), context.getChannelId());
+	}
+
+	@Override
+	public Mono<Void> start() {
+		this.schedule(() -> this.stop().subscribe(), GAME_DURATION, TimeUnit.SECONDS);
+		return Mono.empty();
+	}
+
+	public Mono<Void> show() {
+		Mono<List<User>> usernamesMono = Flux.fromIterable(playersPlace.keySet())
+				.flatMap(userId -> this.getContext().getClient().getUserById(userId))
+				.collectList();
+
+		return this.getContext().getAvatarUrl()
+				.zipWith(usernamesMono)
+				.map(avatarUrlAndUsers -> {
+					final String avatarUrl = avatarUrlAndUsers.getT1();
+					final List<User> users = avatarUrlAndUsers.getT2();
+
+					EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
+							.setAuthor("Roulette Game", null, avatarUrl)
+							.setThumbnail("http://icongal.com/gallery/image/278586/roulette_baccarat_casino.png")
+							.setDescription(String.format("**Use `%s%s <bet> <place>` to join the game.**"
+									+ "%n%n**Place** is a `number between 1 and 36`, %s",
+									this.getContext().getPrefix(), this.getContext().getCommandName(),
+									FormatUtils.format(Place.values(), value -> String.format("`%s`", value.toString().toLowerCase()), ", ")))
+							.addField("Player (Bet)", FormatUtils.format(users,
+									user -> String.format("**%s** (%s)", user.getUsername(), FormatUtils.formatCoins(playersPlace.get(user.getId()).getT1())), "\n"), true)
+							.addField("Place", playersPlace.values().stream().map(Tuple2::getT2).collect(Collectors.joining("\n")), true);
+
+					if(results != null) {
+						embed.addField("Results", results, false);
+					}
+
+					if(!this.isTaskDone()) {
+						embed.setFooter(String.format("You have %d seconds to make your bets.", GAME_DURATION), null);
+					}
+
+					return embed;
+				})
+				.flatMap(embed -> updateableMessage.send(embed))
+				.then();
+	}
+
+	@Override
+	public Mono<Void> stop() {
+		this.cancelScheduledTask();
+		RouletteCmd.MANAGERS.remove(this.getContext().getChannelId());
+
+		final int winningPlace = ThreadLocalRandom.current().nextInt(1, 37);
+
+		return Flux.fromIterable(playersPlace.keySet())
+				.flatMap(userId -> this.getContext().getClient().getUserById(userId))
+				.map(user -> {
+					final int bet = playersPlace.get(user.getId()).getT1();
+					final String place = playersPlace.get(user.getId()).getT2();
+					final Place placeEnum = Utils.getEnum(Place.class, place);
+
+					Map<Place, Boolean> testsMap = Map.of(
+							Place.RED, RED_NUMS.contains(winningPlace),
+							Place.BLACK, !RED_NUMS.contains(winningPlace),
+							Place.LOW, NumberUtils.isInRange(winningPlace, 1, 19),
+							Place.HIGH, NumberUtils.isInRange(winningPlace, 19, 37),
+							Place.EVEN, winningPlace % 2 == 0,
+							Place.ODD, winningPlace % 2 != 0);
+
+					int multiplier = 0;
+					if(place.equals(Integer.toString(winningPlace))) {
+						multiplier = 36;
+					} else if(placeEnum != null && testsMap.get(placeEnum)) {
+						multiplier = 2;
+					} else {
+						multiplier = -1;
+					}
+
+					final int gains = bet * multiplier;
+					DatabaseManager.getDBMember(this.getContext().getGuildId(), user.getId()).addCoins(gains);
+
+					if(gains > 0) {
+						MoneyStatsManager.log(MoneyEnum.MONEY_GAINED, this.getContext().getCommandName(), gains);
+						return String.format("**%s** (Gains: **%s**)", user.getUsername(), FormatUtils.formatCoins(gains));
+					} else {
+						MoneyStatsManager.log(MoneyEnum.MONEY_LOST, this.getContext().getCommandName(), Math.abs(gains));
+						return String.format("**%s** (Losses: **%s**)", user.getUsername(), FormatUtils.formatCoins(Math.abs(gains)));
+					}
+				})
+				.collectSortedList()
+				.map(list -> FormatUtils.format(list, Object::toString, ", "))
+				.map(results -> this.results = results)
+				.then(BotUtils.sendMessage(String.format(Emoji.DICE + " No more bets. *The wheel is spinning...* **%d (%s)** !",
+						winningPlace, RED_NUMS.contains(winningPlace) ? "Red" : "Black"),
+						this.getContext().getChannel()))
+				.then(this.show());
+	}
+
+	/**
+	 * @return true if the user was not participating, false otherwise
+	 */
+	protected boolean addPlayer(Snowflake userId, Integer bet, String place) {
+		return playersPlace.putIfAbsent(userId, Tuples.of(bet, place)) == null;
+	}
+
+}
