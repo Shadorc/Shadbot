55d254fdf5f68cae102d2778c64c8cf5bcd249d3
David <bi-crosseur@hotmail.fr>
2018-07-19 20:07:45 +0200

    DiceCmd is back

--- src/me/shadorc/shadbot/command/game/dice/DiceCmd.java
+++ src/me/shadorc/shadbot/command/game/dice/DiceCmd.java
@@ -1,99 +1,99 @@
-// TODO
-// package me.shadorc.shadbot.command.game.dice;
-//
-// import java.util.List;
-// import java.util.concurrent.ConcurrentHashMap;
-//
-// import me.shadorc.shadbot.core.command.AbstractCommand;
-// import me.shadorc.shadbot.core.command.CommandCategory;
-// import me.shadorc.shadbot.core.command.Context;
-// import me.shadorc.shadbot.core.command.annotation.Command;
-// import me.shadorc.shadbot.core.command.annotation.RateLimited;
-// import me.shadorc.shadbot.exception.IllegalCmdArgumentException;
-// import me.shadorc.shadbot.exception.MissingArgumentException;
-// import me.shadorc.shadbot.ratelimiter.RateLimiter;
-// import me.shadorc.shadbot.utils.BotUtils;
-// import me.shadorc.shadbot.utils.NumberUtils;
-// import me.shadorc.shadbot.utils.StringUtils;
-// import me.shadorc.shadbot.utils.Utils;
-// import me.shadorc.shadbot.utils.embed.HelpBuilder;
-// import me.shadorc.shadbot.utils.object.Emoji;
-//
-// @RateLimited(cooldown = RateLimiter.GAME_COOLDOWN, max = 2)
-// @Command(category = CommandCategory.GAME, names = { "dice" })
-// public class DiceCmd extends AbstractCommand {
-//
-// protected static final ConcurrentHashMap<Long, DiceManager> MANAGERS = new ConcurrentHashMap<>();
-//
-// protected static final float MULTIPLIER = 4.5f;
-// private static final int MAX_BET = 250_000;
-//
-// @Override
-// public void execute(Context context) {
-// if(!context.hasArg()) {
-// throw new MissingArgumentException();
-// }
-//
-// List<String> args = context.requireArgs(1, 2);
-//
-// String numStr = splitArgs.get(splitArgs.size() == 1 ? 0 : 1);
-// Integer num = NumberUtils.asIntBetween(numStr, 1, 6);
-// if(num == null) {
-// throw new IllegalCmdArgumentException(String.format("`%s` is not a valid number, must be between 1 and 6.", numStr));
-// }
-//
-// DiceManager diceManager = MANAGERS.get(context.getChannel().getLongID());
-//
-// // The user tries to join a game and no game are currently playing
-// if(splitArgs.size() == 1 && diceManager == null) {
-// throw new MissingArgumentException();
-// }
-//
-// String betStr = splitArgs.size() == 1 ? Integer.toString(diceManager.getBet()) : splitArgs.get(0);
-// Integer bet = Utils.checkAndGetBet(context.getChannel(), context.getAuthor(), betStr, MAX_BET);
-// if(bet == null) {
-// return;
-// }
-//
-// if(splitArgs.size() == 2) {
-// // The user tries to start a game and it has already been started
-// if(diceManager != null) {
-// BotUtils.sendMessage(String.format(Emoji.INFO + " A **Dice Game** has already been started. Use `%s%s <num>` to join it.",
-// context.getPrefix(), this.getName()), context.getChannel());
-// return;
-// }
-//
-// diceManager = new DiceManager(this, context.getPrefix(), context.getChannel(), context.getAuthor(), bet);
-// }
-//
-// if(MANAGERS.putIfAbsent(context.getChannel().getLongID(), diceManager) == null) {
-// diceManager.start();
-// }
-//
-// if(diceManager.getPlayersCount() == 6) {
-// BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " Sorry, there are already 6 players.", context.getChannel());
-// return;
-// }
-//
-// if(diceManager.isNumBet(num)) {
-// BotUtils.sendMessage(Emoji.GREY_EXCLAMATION + " This number has already been bet, please try with another one.", context.getChannel());
-// return;
-// }
-//
-// if(!diceManager.addPlayer(context.getAuthor(), num)) {
-// BotUtils.sendMessage(String.format(Emoji.INFO + " (**%s**) You're already participating.",
-// context.getUsername()), context.getChannel());
-// return;
-// }
-// }
-//
-// @Override
-// public EmbedObject getHelp(String prefix) {
-// return new HelpBuilder(this, context)
-// .setDescription("Start a dice game with a common bet.")
-// .addArg("bet", false)
-// .addArg("num", "number between 1 and 6\nYou can't bet on a number that has already been chosen by another player.", false)
-// .setGains("The winner gets the prize pool plus %.1f times his bet", MULTIPLIER)
-// .build();
-// }
-// }
+package me.shadorc.shadbot.command.game.dice;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.core.command.AbstractCommand;
+import me.shadorc.shadbot.core.command.CommandCategory;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.command.annotation.Command;
+import me.shadorc.shadbot.core.command.annotation.RateLimited;
+import me.shadorc.shadbot.core.ratelimiter.RateLimiter;
+import me.shadorc.shadbot.exception.CommandException;
+import me.shadorc.shadbot.exception.MissingArgumentException;
+import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.NumberUtils;
+import me.shadorc.shadbot.utils.Utils;
+import me.shadorc.shadbot.utils.command.Emoji;
+import me.shadorc.shadbot.utils.embed.HelpBuilder;
+import reactor.core.publisher.Mono;
+
+@RateLimited(cooldown = RateLimiter.GAME_COOLDOWN, max = 2)
+@Command(category = CommandCategory.GAME, names = { "dice" })
+public class DiceCmd extends AbstractCommand {
+
+	protected static final ConcurrentHashMap<Snowflake, DiceManager> MANAGERS = new ConcurrentHashMap<>();
+
+	protected static final float MULTIPLIER = 4.5f;
+	private static final int MAX_BET = 250_000;
+
+	@Override
+	public Mono<Void> execute(Context context) {
+		List<String> args = context.requireArgs(1, 2);
+
+		final String numStr = args.get(args.size() == 1 ? 0 : 1);
+		Integer num = NumberUtils.asIntBetween(numStr, 1, 6);
+		if(num == null) {
+			throw new CommandException(String.format("`%s` is not a valid number, must be between 1 and 6.", numStr));
+		}
+
+		DiceManager diceManager = MANAGERS.get(context.getChannelId());
+
+		// The user tries to join a game and no game are currently playing
+		if(args.size() == 1 && diceManager == null) {
+			throw new MissingArgumentException();
+		}
+
+		final String betStr = args.size() == 1 ? Integer.toString(diceManager.getBet()) : args.get(0);
+		final Integer bet = Utils.requireBet(context.getMember(), betStr, MAX_BET);
+
+		if(args.size() == 2) {
+			// The user tries to start a game and it has already been started
+			if(diceManager != null) {
+				return BotUtils.sendMessage(String.format(Emoji.INFO + " (**%s**) A **Dice Game** has already been started. "
+						+ "Use `%s%s <num>` to join it.",
+						context.getUsername(), context.getPrefix(), this.getName()), context.getChannel())
+						.then();
+			}
+
+			diceManager = new DiceManager(context, bet);
+		}
+
+		if(MANAGERS.putIfAbsent(context.getChannelId(), diceManager) == null) {
+			diceManager.start();
+		}
+
+		if(diceManager.getPlayersCount() == 6) {
+			return BotUtils.sendMessage(String.format(Emoji.GREY_EXCLAMATION + " (**%s**) Sorry, there are already 6 players.", 
+					context.getUsername()), context.getChannel())
+					.then();
+		}
+
+		if(diceManager.isNumBet(num)) {
+			return BotUtils.sendMessage(
+					String.format(Emoji.GREY_EXCLAMATION + " (**%s**) This number has already been bet, please try with another one.", 
+							context.getUsername()), context.getChannel())
+					.then();
+		}
+
+		if(diceManager.addPlayer(context.getAuthorId(), num)) {
+			return diceManager.show().then();
+		} else {
+			return BotUtils.sendMessage(String.format(Emoji.INFO + " (**%s**) You're already participating.",
+					context.getUsername()), context.getChannel())
+					.then();
+		}
+	}
+
+	@Override
+	public Mono<EmbedCreateSpec> getHelp(Context context) {
+		return new HelpBuilder(this, context)
+				.setDescription("Start a dice game with a common bet.")
+				.addArg("bet", false)
+				.addArg("num", "number between 1 and 6\nYou can't bet on a number that has already been chosen by another player.", false)
+				.setGains("The winner gets the prize pool plus %.1f times his bet", MULTIPLIER)
+				.build();
+	}
+}
--- src/me/shadorc/shadbot/command/game/dice/DiceManager.java
+++ src/me/shadorc/shadbot/command/game/dice/DiceManager.java
@@ -1,117 +1,128 @@
-// package me.shadorc.shadbot.command.game.dice;
-//
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.concurrent.ConcurrentHashMap;
-// import java.util.concurrent.ThreadLocalRandom;
-// import java.util.concurrent.TimeUnit;
-// import java.util.stream.Collectors;
-//
-// import me.shadorc.shadbot.core.command.AbstractCommand;
-// import me.shadorc.shadbot.core.game.AbstractGameManager;
-// import me.shadorc.shadbot.data.db.DatabaseManager;
-// import me.shadorc.shadbot.data.stats.MoneyStatsManager;
-// import me.shadorc.shadbot.data.stats.MoneyStatsManager.MoneyEnum;
-// import me.shadorc.shadbot.utils.BotUtils;
-// import me.shadorc.shadbot.utils.FormatUtils;
-// import me.shadorc.shadbot.utils.command.Emoji;
-// import me.shadorc.shadbot.utils.embed.EmbedUtils;
-// import me.shadorc.shadbot.utils.message.UpdateableMessage;
-//
-// public class DiceManager extends AbstractGameManager {
-//
-// private static final int GAME_DURATION = 30;
-//
-// private final ConcurrentHashMap<Integer, IUser> numsPlayers;
-// private final int bet;
-// private final UpdateableMessage message;
-//
-// private String results;
-//
-// public DiceManager(AbstractCommand cmd, String prefix, IChannel channel, IUser author, int bet) {
-// super(cmd, prefix, channel, author);
-// this.bet = bet;
-// this.numsPlayers = new ConcurrentHashMap<>();
-// this.message = new UpdateableMessage(channel);
-// }
-//
-// @Override
-// public void start() {
-// this.schedule(() -> this.stop(), GAME_DURATION, TimeUnit.SECONDS);
-// }
-//
-// private void show() {
-// EmbedBuilder embed = EmbedUtils.getDefaultEmbed()
-// .setLenient(true)
-// .withAuthorName("Dice Game")
-// .withThumbnail("http://findicons.com/files/icons/2118/nuvola/128/package_games_board.png")
-// .withDescription(String.format("**Use `%s%s <num>` to join the game.**%n**Bet:** %s",
-// this.getPrefix(), this.getCmdName(), FormatUtils.formatCoins(bet)))
-// .addField("Player", numsPlayers.values().stream().map(IUser::getName).collect(Collectors.joining("\n")), true)
-// .addField("Number", numsPlayers.keySet().stream().map(Object::toString).collect(Collectors.joining("\n")), true)
-// .addField("Results", results, false)
-// .withFooterText(String.format("You have %d seconds to make your bets.", GAME_DURATION));
-//
-// RequestFuture<IMessage> msgRequest = message.send(embed.build());
-// if(msgRequest != null) {
-// msgRequest.get();
-// }
-// }
-//
-// @Override
-// public void stop() {
-// this.cancelScheduledTask();
-//
-// int winningNum = ThreadLocalRandom.current().nextInt(1, 7);
-//
-// List<String> list = new ArrayList<>();
-// for(int num : numsPlayers.keySet()) {
-// IUser user = numsPlayers.get(num);
-// int gains = bet;
-// if(num == winningNum) {
-// gains *= numsPlayers.size() + DiceCmd.MULTIPLIER;
-// MoneyStatsManager.log(MoneyEnum.MONEY_GAINED, this.getCmdName(), gains);
-// } else {
-// gains *= -1;
-// MoneyStatsManager.log(MoneyEnum.MONEY_LOST, this.getCmdName(), Math.abs(gains));
-// }
-// list.add(gains > 0 ? 0 : list.size(), String.format("%s (**%s**)", user.getName(), FormatUtils.formatCoins(gains)));
-//
-// DatabaseManager.getDBUser(this.getGuild(), user).addCoins(gains);
-// }
-//
-// RequestFuture<IMessage> msgRequest = BotUtils.sendMessage(String.format(Emoji.DICE + " The dice is rolling... **%s** !", winningNum),
-// this.getMessageChannel());
-// if(msgRequest != null) {
-// msgRequest.get();
-// }
-//
-// this.results = FormatUtils.format(list, Object::toString, "\n");
-// this.show();
-//
-// numsPlayers.clear();
-// DiceCmd.MANAGERS.remove(this.getMessageChannel().getLongID());
-// }
-//
-// public int getBet() {
-// return bet;
-// }
-//
-// public int getPlayersCount() {
-// return numsPlayers.size();
-// }
-//
-// public boolean addPlayer(IUser user, int num) {
-// if(numsPlayers.containsValue(user)) {
-// return false;
-// }
-//
-// numsPlayers.putIfAbsent(num, user);
-// this.show();
-// return true;
-// }
-//
-// protected boolean isNumBet(int num) {
-// return numsPlayers.containsKey(num);
-// }
-// }
+package me.shadorc.shadbot.command.game.dice;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.game.AbstractGameManager;
+import me.shadorc.shadbot.data.db.DatabaseManager;
+import me.shadorc.shadbot.data.stats.MoneyStatsManager;
+import me.shadorc.shadbot.data.stats.MoneyStatsManager.MoneyEnum;
+import me.shadorc.shadbot.utils.BotUtils;
+import me.shadorc.shadbot.utils.FormatUtils;
+import me.shadorc.shadbot.utils.command.Emoji;
+import me.shadorc.shadbot.utils.embed.EmbedUtils;
+import me.shadorc.shadbot.utils.message.UpdateableMessage;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class DiceManager extends AbstractGameManager {
+
+	private static final int GAME_DURATION = 30;
+
+	private final ConcurrentHashMap<Integer, Snowflake> numsPlayers;
+	private final int bet;
+	private final UpdateableMessage updateableMessage;
+
+	private String results;
+
+	public DiceManager(Context context, int bet) {
+		super(context);
+		this.bet = bet;
+		this.numsPlayers = new ConcurrentHashMap<>();
+		this.updateableMessage = new UpdateableMessage(context.getClient(), context.getChannelId());
+	}
+
+	@Override
+	public Mono<Void> start() {
+		this.schedule(() -> this.stop().subscribe(), GAME_DURATION, TimeUnit.SECONDS);
+		return Mono.empty();
+	}
+
+	protected Mono<Message> show() {
+		return this.getContext().getAvatarUrl()
+				.zipWith(Flux.fromIterable(numsPlayers.values())
+						.flatMap(userId -> this.getContext().getClient().getUserById(userId))
+						.map(User::getUsername)
+						.collectList())
+				.map(avatarUrlAndUsers -> {
+					final String avatarUrl = avatarUrlAndUsers.getT1();
+					final List<String> usernames = avatarUrlAndUsers.getT2();
+					EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
+							.setAuthor("Dice Game", null, avatarUrl)
+							.setThumbnail("http://findicons.com/files/icons/2118/nuvola/128/package_games_board.png")
+							.setDescription(String.format("**Use `%s%s <num>` to join the game.**%n**Bet:** %s",
+									this.getContext().getPrefix(), this.getContext().getCommandName(), FormatUtils.formatCoins(bet)))
+							.addField("Player", FormatUtils.format(usernames, Object::toString, "\n"), true)
+							.addField("Number", FormatUtils.format(numsPlayers.keySet(), Object::toString, "\n"), true)
+							.setFooter(String.format("You have %d seconds to make your bets.", GAME_DURATION), null);
+
+					if(results != null) {
+						embed.addField("Results", results, false);
+					}
+
+					return embed;
+				})
+				.flatMap(embed -> updateableMessage.send(embed));
+	}
+
+	@Override
+	public Mono<Void> stop() {
+		this.cancelScheduledTask();
+		DiceCmd.MANAGERS.remove(this.getContext().getChannelId());
+
+		int winningNum = ThreadLocalRandom.current().nextInt(1, 7);
+
+		return Flux.fromIterable(numsPlayers.values())
+				.flatMap(userId -> this.getContext().getClient().getUserById(userId))
+				.map(user -> {
+					int num = numsPlayers.keySet().stream()
+							.filter(number -> numsPlayers.get(number).equals(user.getId()))
+							.findFirst()
+							.get();
+					int gains = bet;
+
+					if(num == winningNum) {
+						gains *= numsPlayers.size() + DiceCmd.MULTIPLIER;
+						MoneyStatsManager.log(MoneyEnum.MONEY_GAINED, this.getContext().getCommandName(), gains);
+					} else {
+						gains *= -1;
+						MoneyStatsManager.log(MoneyEnum.MONEY_LOST, this.getContext().getCommandName(), Math.abs(gains));
+					}
+					DatabaseManager.getDBMember(this.getContext().getGuildId(), user.getId()).addCoins(gains);
+					return String.format("%s (**%s**)", user.getUsername(), FormatUtils.formatCoins(gains));
+				})
+				.collectList()
+				.map(list -> this.results = FormatUtils.format(list,Object::toString,"\n"))
+				.then(BotUtils.sendMessage(String.format(Emoji.DICE + " The dice is rolling... **%s** !", winningNum), this.getContext().getChannel()))
+				.then(this.show())
+				.then();
+	}
+
+	public int getBet() {
+		return bet;
+	}
+
+	public int getPlayersCount() {
+		return numsPlayers.size();
+	}
+
+	public boolean addPlayer(Snowflake userId, int num) {
+		if(numsPlayers.containsValue(userId)) {
+			return false;
+		}
+
+		numsPlayers.putIfAbsent(num, userId);
+		return true;
+	}
+
+	protected boolean isNumBet(int num) {
+		return numsPlayers.containsKey(num);
+	}
+}
