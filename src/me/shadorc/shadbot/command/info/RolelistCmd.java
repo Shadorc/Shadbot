8c2ffd427f2bb5b5b3c3ccb1e2f985b85809758a
David <bi-crosseur@hotmail.fr>
2018-07-31 14:45:16 +0200

    Fix RolelistCmd

--- src/me/shadorc/shadbot/command/info/RolelistCmd.java
+++ src/me/shadorc/shadbot/command/info/RolelistCmd.java
@@ -3,10 +3,11 @@ package me.shadorc.shadbot.command.info;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Role;
-import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Snowflake;
 import discord4j.core.spec.EmbedCreateSpec;
 import me.shadorc.shadbot.core.command.AbstractCommand;
@@ -19,7 +20,6 @@ import me.shadorc.shadbot.utils.BotUtils;
 import me.shadorc.shadbot.utils.FormatUtils;
 import me.shadorc.shadbot.utils.embed.EmbedUtils;
 import me.shadorc.shadbot.utils.embed.HelpBuilder;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RateLimited
@@ -33,28 +33,32 @@ public class RolelistCmd extends AbstractCommand {
 			throw new MissingArgumentException();
 		}
 
-		final Mono<List<Role>> roles = Flux.fromIterable(roleIds)
-				.flatMap(roleId -> context.getClient().getRoleById(context.getGuildId(), roleId))
-				.collectList();
-
-		EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
-				.setAuthor("Role List", null, null);
-
 		return context.getGuild()
 				.flatMapMany(Guild::getMembers)
 				.filter(member -> !Collections.disjoint(member.getRoleIds(), roleIds))
-				.map(User::getUsername)
+				.map(Member::getUsername)
 				.collectList()
-				.zipWith(roles)
-				.map(membersAndRoles -> {
-					FormatUtils.createColumns(membersAndRoles.getT1(), 25).stream()
+				.zipWith(context.getMessage().getRoleMentions().collectList())
+				.map(usernamesAndRoles -> {
+					final List<String> usernames = usernamesAndRoles.getT1().stream().distinct().collect(Collectors.toList());
+					final List<Role> roles = usernamesAndRoles.getT2();
+					
+					EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed();
+					
+					if(usernames.isEmpty()) {
+						return embed.setDescription(
+								String.format("There is nobody with %s.", roleIds.size() == 1 ? "this role" : "these roles"));
+					}
+					
+					FormatUtils.createColumns(usernames, 25).stream()
 							.forEach(field -> embed.addField(field.getName(), field.getValue(), false));
 
 					return embed.setDescription(String.format("Members with role(s) **%s**",
-							FormatUtils.format(membersAndRoles.getT2(), Role::getName, ", ")));
+							FormatUtils.format(roles, Role::getName, ", ")));
 				})
-				.defaultIfEmpty(embed.setDescription(String.format("There is nobody with %s.", roleIds.size() == 1 ? "this role" : "these roles")))
-				.then(BotUtils.sendMessage(embed, context.getChannel()))
+				.zipWith(context.getAvatarUrl())
+				.map(embedAndAvatarUrl -> embedAndAvatarUrl.getT1().setAuthor("Role List", null, embedAndAvatarUrl.getT2()))
+				.flatMap(embed -> BotUtils.sendMessage(embed, context.getChannel()))
 				.then();
 	}
 
