faaee05bc6a40dc116b00f0b40143080123eaaed
David <bi-crosseur@hotmail.fr>
2018-09-22 19:39:18 +0200

    IamCommand: WIP

--- src/me/shadorc/shadbot/command/admin/IamCommand.java
+++ src/me/shadorc/shadbot/command/admin/IamCommand.java
@@ -1,25 +1,66 @@
 package me.shadorc.shadbot.command.admin;
 
+import java.util.List;
+import java.util.Map;
+
+import discord4j.core.object.entity.Role;
 import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.reaction.ReactionEmoji.Unicode;
 import discord4j.core.object.util.Permission;
+import discord4j.core.object.util.Snowflake;
 import discord4j.core.spec.EmbedCreateSpec;
 import me.shadorc.shadbot.core.command.AbstractCommand;
 import me.shadorc.shadbot.core.command.CommandCategory;
 import me.shadorc.shadbot.core.command.CommandPermission;
 import me.shadorc.shadbot.core.command.Context;
 import me.shadorc.shadbot.core.command.annotation.Command;
+import me.shadorc.shadbot.core.setting.SettingEnum;
+import me.shadorc.shadbot.data.database.DBGuild;
+import me.shadorc.shadbot.data.database.DatabaseManager;
+import me.shadorc.shadbot.exception.MissingArgumentException;
+import me.shadorc.shadbot.utils.DiscordUtils;
+import me.shadorc.shadbot.utils.FormatUtils;
+import me.shadorc.shadbot.utils.embed.EmbedUtils;
+import me.shadorc.shadbot.utils.message.ReactionMessage;
 import reactor.core.publisher.Mono;
 
-@Command(category = CommandCategory.ADMIN, permission = CommandPermission.ADMIN, names = { "iam" }, permissions = { Permission.MANAGE_ROLES })
+@Command(category = CommandCategory.ADMIN, permission = CommandPermission.ADMIN, names = { "iam" }, permissions = { Permission.ADD_REACTIONS })
 public class IamCommand extends AbstractCommand {
 
-	public static final ReactionEmoji ADD_REACTION = ReactionEmoji.unicode("✅");
-	public static final ReactionEmoji REMOVE_REACTION = ReactionEmoji.unicode("❌");
+	public static final Unicode REACTION = ReactionEmoji.unicode("✅");
 
 	@Override
 	public Mono<Void> execute(Context context) {
-		// TODO Auto-generated method stub
-		return null;
+		return DiscordUtils.getRoles(context.getMessage())
+				.flatMap(roleId -> context.getClient().getRoleById(context.getGuildId(), roleId))
+				.collectList()
+				.zipWith(context.getAvatarUrl())
+				.flatMap(rolesAndAvatarUrl -> {
+					final List<Role> roles = rolesAndAvatarUrl.getT1();
+					final String avatarUrl = rolesAndAvatarUrl.getT2();
+
+					if(roles.isEmpty()) {
+						throw new MissingArgumentException();
+					}
+
+					final EmbedCreateSpec embed = EmbedUtils.getDefaultEmbed()
+							.setAuthor("Iam", null, avatarUrl)
+							.setDescription(String.format("Click on %s to add you role(s): %s",
+									REACTION.getRaw(),
+									FormatUtils.format(roles, role -> String.format("`%s`", role.getMention()), "\n")));
+
+					return new ReactionMessage(context.getClient(), context.getChannelId(), List.of(REACTION))
+							.sendMessage(embed)
+							.doOnSuccess(message -> {
+								final DBGuild dbGuild = DatabaseManager.getDBGuild(context.getGuildId());
+								final Map<Snowflake, Snowflake> setting = dbGuild.getIamMessages();
+								roles.stream()
+										.map(Role::getId)
+										.forEach(roleId -> setting.put(message.getId(), roleId));
+								dbGuild.setSetting(SettingEnum.IAM_MESSAGES, setting);
+							});
+				})
+				.then();
 	}
 
 	@Override
--- src/me/shadorc/shadbot/listener/ReactionListener.java
+++ src/me/shadorc/shadbot/listener/ReactionListener.java
@@ -1,38 +1,53 @@
 package me.shadorc.shadbot.listener;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+import discord4j.core.event.domain.message.MessageEvent;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.event.domain.message.ReactionRemoveEvent;
+import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.util.Permission;
 import discord4j.core.object.util.Snowflake;
 import me.shadorc.shadbot.command.admin.IamCommand;
+import me.shadorc.shadbot.core.ExceptionHandler;
 import me.shadorc.shadbot.data.database.DatabaseManager;
+import me.shadorc.shadbot.utils.message.TemporaryMessage;
 
 public class ReactionListener {
 
-	// TODO: Manage errors
-	public static void onReactionAddEvent(ReactionAddEvent event) {
-		event.getGuildId().ifPresent(guildId -> {
-			final Snowflake roleId = DatabaseManager.getDBGuild(guildId)
-					.getIamMessages()
-					.get(event.getMessageId());
-			if(roleId != null && event.getEmoji().equals(IamCommand.ADD_REACTION)) {
-				event.getUser()
-						.flatMap(user -> user.asMember(guildId))
-						.flatMap(member -> member.addRole(roleId))
-						.subscribe();
-			}
-		});
+	private enum Action {
+		ADD, REMOVE;
 	}
 
-	// TODO: Manage errors
+	public static void onReactionAddEvent(ReactionAddEvent event) {
+		iam(event, event.getGuildId(), event.getChannelId(), event.getUserId(), event.getMessageId(), event.getEmoji(), Action.ADD);
+	}
+
 	public static void onReactionRemoveEvent(ReactionRemoveEvent event) {
-		event.getGuildId().ifPresent(guildId -> {
+		iam(event, event.getGuildId(), event.getChannelId(), event.getUserId(), event.getMessageId(), event.getEmoji(), Action.REMOVE);
+	}
+
+	public static void iam(MessageEvent event, Optional<Snowflake> guildIdOpt, Snowflake channelId, Snowflake userId,
+			Snowflake messageId, ReactionEmoji reaction, Action action) {
+
+		if(event.getClient().getSelfId().map(userId::equals).orElse(false)) {
+			return;
+		}
+
+		guildIdOpt.ifPresent(guildId -> {
 			final Snowflake roleId = DatabaseManager.getDBGuild(guildId)
 					.getIamMessages()
-					.get(event.getMessageId());
-			if(roleId != null && event.getEmoji().equals(IamCommand.REMOVE_REACTION)) {
-				event.getUser()
-						.flatMap(user -> user.asMember(guildId))
-						.flatMap(member -> member.removeRole(roleId))
+					.get(messageId);
+			if(roleId != null && reaction.equals(IamCommand.REACTION)) {
+				event.getClient()
+						.getMemberById(guildId, userId)
+						.flatMap(member -> action == Action.ADD ? member.addRole(roleId) : member.removeRole(roleId))
+						.onErrorResume(ExceptionHandler::isForbidden,
+								err -> new TemporaryMessage(event.getClient(), channelId, 10, ChronoUnit.SECONDS)
+										.send(String.format("I can't execute this command due to the lack of permission."
+												+ "%nPlease, check my permissions and channel-specific ones to verify that %s is checked.",
+												Permission.MANAGE_ROLES)))
 						.subscribe();
 			}
 		});
