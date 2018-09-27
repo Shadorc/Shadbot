6c7670211801cece3cebe7cf2010b886f7e0b77c
David <bi-crosseur@hotmail.fr>
2018-09-27 10:59:51 +0200

    Clan-up AudioEventListener

--- src/me/shadorc/shadbot/listener/music/AudioEventListener.java
+++ src/me/shadorc/shadbot/listener/music/AudioEventListener.java
@@ -13,6 +13,7 @@ import me.shadorc.shadbot.utils.FormatUtils;
 import me.shadorc.shadbot.utils.TextUtils;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
 import me.shadorc.shadbot.utils.object.Emoji;
+import reactor.core.publisher.Mono;
 
 public class AudioEventListener extends AudioEventAdapter {
 
@@ -29,7 +30,8 @@ public class AudioEventListener extends AudioEventAdapter {
 	public void onTrackStart(AudioPlayer player, AudioTrack track) {
 		BotUtils.sendMessage(String.format(Emoji.MUSICAL_NOTE + " Currently playing: **%s**",
 				FormatUtils.trackName(track.getInfo())), this.guildMusic.getMessageChannel())
-				.doOnError(ExceptionHandler::isForbidden, error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
+				.doOnError(ExceptionHandler::isForbidden,
+						error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
 				.subscribe();
 	}
 
@@ -37,7 +39,10 @@ public class AudioEventListener extends AudioEventAdapter {
 	public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
 		if(endReason.mayStartNext) {
 			this.errorCount = 0; // Everything seems to be fine, reset error counter.
-			this.nextOrEnd();
+			this.nextOrEnd()
+					.doOnError(ExceptionHandler::isForbidden,
+							error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
+					.subscribe();
 		}
 	}
 
@@ -47,44 +52,47 @@ public class AudioEventListener extends AudioEventAdapter {
 
 		final String errMessage = TextUtils.cleanLavaplayerErr(err);
 
+		LogUtils.infof("{Guild ID: %d} %sTrack exception: %s", this.guildMusic.getGuildId().asLong(),
+				this.errorCount > 3 ? "(Ignored) " : "", errMessage);
+
+		final StringBuilder strBuilder = new StringBuilder();
+
 		if(this.errorCount <= 3) {
-			BotUtils.sendMessage(String.format(Emoji.RED_CROSS + " Sorry, %s. I'll try to play the next available song.",
-					errMessage.toLowerCase()), this.guildMusic.getMessageChannel())
-					.doOnError(ExceptionHandler::isForbidden, error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
-					.subscribe();
+			strBuilder.append(String.format(Emoji.RED_CROSS + " Sorry, %s. I'll try to play the next available song.",
+					errMessage.toLowerCase()));
 		}
 
 		if(this.errorCount == 3) {
-			BotUtils.sendMessage(Emoji.RED_FLAG + " Too many errors in a row, I will ignore them until I find a music that can be played.",
-					this.guildMusic.getMessageChannel())
-					.doOnError(ExceptionHandler::isForbidden, error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
-					.subscribe();
 			LogUtils.infof("{Guild ID: %d} Too many errors in a row. They will be ignored until a music can be played.",
 					this.guildMusic.getGuildId().asLong());
+			strBuilder.append("\n" + Emoji.RED_FLAG + " Too many errors in a row, I will ignore them until I find a music that can be played.");
 		}
-
-		LogUtils.infof("{Guild ID: %d} %sTrack exception: %s",
-				this.guildMusic.getGuildId().asLong(), this.errorCount > 3 ? "(Ignored) " : "", errMessage);
-
-		this.nextOrEnd();
+		
+		BotUtils.sendMessage(strBuilder.toString(), this.guildMusic.getMessageChannel())
+				.then(this.nextOrEnd())
+				.doOnError(ExceptionHandler::isForbidden,
+						error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
+				.subscribe();
 	}
 
 	@Override
 	public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
+		LogUtils.infof("{Guild ID: %d} Music stuck, skipping it.", this.guildMusic.getGuildId().asLong());
+
+		// TODO: Does the music end if an error (for example permission exception) is thrown by the first sendMessage ?
 		BotUtils.sendMessage(Emoji.RED_EXCLAMATION + " Music seems stuck, I'll try to play the next available song.",
 				this.guildMusic.getMessageChannel())
-				.doOnError(ExceptionHandler::isForbidden, error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
+				.then(this.nextOrEnd())
+				.doOnError(ExceptionHandler::isForbidden,
+						error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
 				.subscribe();
-		LogUtils.warn(this.guildMusic.getClient(), String.format("{Guild ID: %d} Music stuck, skipping it.", this.guildMusic.getGuildId().asLong()));
-
-		this.nextOrEnd();
 	}
 
-	private void nextOrEnd() {
-		if(!this.guildMusic.getScheduler().nextTrack()) {
-			this.guildMusic.end()
-					.doOnError(ExceptionHandler::isForbidden, error -> LogUtils.cannotSpeak(this.getClass(), this.guildMusic.getGuildId()))
-					.subscribe();
+	private Mono<Void> nextOrEnd() {
+		// If the next track could be started
+		if(this.guildMusic.getScheduler().nextTrack()) {
+			return Mono.empty();
 		}
+		return this.guildMusic.end();
 	}
 }
