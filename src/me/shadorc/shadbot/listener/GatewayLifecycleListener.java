c4a539d743bf5287d95c49efc468eba2e5085f7c
David <bi-crosseur@hotmail.fr>
2018-11-12 21:49:20 +0100

    Only log onGatewayLifecycleEvent when the bot is ready

--- src/me/shadorc/shadbot/listener/GatewayLifecycleListener.java
+++ src/me/shadorc/shadbot/listener/GatewayLifecycleListener.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.lifecycle.GatewayLifecycleEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import me.shadorc.shadbot.Shadbot;
 import me.shadorc.shadbot.utils.DiscordUtils;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
 import reactor.core.publisher.Flux;
@@ -12,10 +13,12 @@ import reactor.core.publisher.Flux;
 public class GatewayLifecycleListener {
 
 	public static void onGatewayLifecycleEvent(GatewayLifecycleEvent event) {
-		LogUtils.info("{Shard %d} %s",
-				event.getClient().getConfig().getShardIndex(),
-				// Add space before uppercase letters
-				event.getClass().getSimpleName().replaceAll("([^_])([A-Z])", "$1 $2"));
+		if(Shadbot.isReady()) {
+			LogUtils.info("{Shard %d} %s",
+					event.getClient().getConfig().getShardIndex(),
+					// Add space before uppercase letters
+					event.getClass().getSimpleName().replaceAll("([^_])([A-Z])", "$1 $2"));
+		}
 	}
 
 	public static void onReadyEvent(ReadyEvent event) {
