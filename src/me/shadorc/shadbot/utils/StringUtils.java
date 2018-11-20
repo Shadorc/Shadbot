ef656c3c31e11410ca55aa7f5fe92613751e9262
David <bi-crosseur@hotmail.fr>
2018-11-20 10:00:15 +0100

    StringUtils: Add remove(String, List<String>)

--- src/me/shadorc/shadbot/utils/StringUtils.java
+++ src/me/shadorc/shadbot/utils/StringUtils.java
@@ -14,13 +14,12 @@ public class StringUtils {
 	 * @param str - the string to split
 	 * @param limit - the result threshold
 	 * @param delimiter - the delimiting regular expression
-	 * @return A list with a maximum number of {@code limit} elements containing all the results of {@code str} splitted using {@code delimiter} excluding
+	 * @return A list with a maximum number of {@code limit} elements containing all
+	 *         the results of {@code str} splitted using {@code delimiter} excluding
 	 *         empty results
 	 */
 	public static List<String> split(String str, int limit, String delimiter) {
-		return Arrays.stream(str.split(delimiter, limit))
-				.map(String::trim)
-				.filter(word -> !word.isEmpty())
+		return Arrays.stream(str.split(delimiter, limit)).map(String::trim).filter(word -> !word.isEmpty())
 				.collect(Collectors.toList());
 	}
 
@@ -28,7 +27,8 @@ public class StringUtils {
 	 * @param str - the string to split
 	 * @param limit - the result threshold
 	 * @param delimiter - the delimiting regular expression
-	 * @return A endless list containing all the elements resulting of {@code str} splitted using space excluding empty results
+	 * @return A endless list containing all the elements resulting of {@code str}
+	 *         splitted using space excluding empty results
 	 */
 	public static List<String> split(String str, int limit) {
 		return StringUtils.split(str, limit, " ");
@@ -37,7 +37,8 @@ public class StringUtils {
 	/**
 	 * @param str - the string to split
 	 * @param delimiter - the delimiting regular expression
-	 * @return A endless list all the elements resulting of {@code str} splitted using {@code delimiter} excluding empty results
+	 * @return A endless list all the elements resulting of {@code str} splitted
+	 *         using {@code delimiter} excluding empty results
 	 */
 	public static List<String> split(String str, String delimiter) {
 		return StringUtils.split(str, -1, delimiter);
@@ -45,7 +46,8 @@ public class StringUtils {
 
 	/**
 	 * @param str - the string to split
-	 * @return A list without limits containing all the elements resulting of {@code str} splitted using space excluding empty results
+	 * @return A list without limits containing all the elements resulting of
+	 *         {@code str} splitted using space excluding empty results
 	 */
 	public static List<String> split(String str) {
 		return StringUtils.split(str, -1);
@@ -56,7 +58,7 @@ public class StringUtils {
 	 * @return The capitalized String, null if null String input
 	 */
 	public static String capitalize(String str) {
-		if(str == null || str.isEmpty()) {
+		if (str == null || str.isEmpty()) {
 			return str;
 		}
 		return str.substring(0, 1).toUpperCase() + str.toLowerCase().substring(1);
@@ -64,7 +66,8 @@ public class StringUtils {
 
 	/**
 	 * @param enumeration - the enumeration to format
-	 * @return The enumeration converted to a capitalized string with underscores replaced with spaces
+	 * @return The enumeration converted to a capitalized string with underscores
+	 *         replaced with spaces
 	 */
 	public static <E extends Enum<E>> String capitalizeEnum(E enumeration) {
 		return StringUtils.capitalize(StringUtils.toLowerCase(enumeration).replace("_", " "));
@@ -81,10 +84,11 @@ public class StringUtils {
 	/**
 	 * @param count - the number of elements
 	 * @param str - the string to get plural from
-	 * @return {@code String.format("%d %ss", count, str)} if count > 1, String.format("%d %s", count, str) otherwise
+	 * @return {@code String.format("%d %ss", count, str)} if count > 1,
+	 *         String.format("%d %s", count, str) otherwise
 	 */
 	public static String pluralOf(long count, String str) {
-		if(count > 1) {
+		if (count > 1) {
 			return String.format("%d %ss", count, str);
 		}
 		return String.format("%d %s", count, str);
@@ -96,21 +100,30 @@ public class StringUtils {
 	 * @return The resulting {@code String}
 	 */
 	public static String remove(String str, String... toRemove) {
-		return str.replaceAll(Arrays.stream(toRemove)
-				.filter(replacement -> !replacement.isEmpty())
-				.map(Pattern::quote)
+		return str.replaceAll(Arrays.stream(toRemove).filter(replacement -> !replacement.isEmpty()).map(Pattern::quote)
 				.collect(Collectors.joining("|")), "");
+	}
+	
+	/**
+	 * @param str - the string from which to remove patterns
+	 * @param toRemove - the strings to be substituted for each match
+	 * @return The resulting {@code String}
+	 */
+	public static String remove(String str, List<String> toRemove) {
+		return StringUtils.remove(str, toRemove.toArray(new String[toRemove.size()]));
 	}
 
 	/**
-	 * The function returns the argument string with whitespace normalized by using {@link #trim(String)} to remove leading and trailing whitespace and
-	 * then replacing sequences of whitespace characters by a single space.
+	 * The function returns the argument string with whitespace normalized by using
+	 * {@link #trim(String)} to remove leading and trailing whitespace and then
+	 * replacing sequences of whitespace characters by a single space.
 	 *
 	 * @param str - the source String to normalize whitespaces from, may be null
-	 * @return the modified string with whitespace normalized, {@code null} if null String input
+	 * @return the modified string with whitespace normalized, {@code null} if null
+	 *         String input
 	 */
 	public static String normalizeSpace(String str) {
-		if(str == null || str.isEmpty()) {
+		if (str == null || str.isEmpty()) {
 			return str;
 		}
 		return str.trim().replaceAll(" +", " ");
@@ -123,7 +136,7 @@ public class StringUtils {
 	public static List<String> getQuotedElements(String text) {
 		final List<String> matches = new ArrayList<>();
 		final Matcher matcher = Pattern.compile("\"([^\"]*)\"").matcher(text);
-		while(matcher.find()) {
+		while (matcher.find()) {
 			matches.add(matcher.group(1));
 		}
 		matches.removeAll(Collections.singleton(""));
