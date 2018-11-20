bd53c5fb50af4853ac614c884cb3f54fd7b27ba4
David <bi-crosseur@hotmail.fr>
2018-11-20 16:25:33 +0100

    LyricsCmd: Code clean-up and fix

--- src/me/shadorc/shadbot/command/utils/LyricsCmd.java
+++ src/me/shadorc/shadbot/command/utils/LyricsCmd.java
@@ -3,7 +3,6 @@ package me.shadorc.shadbot.command.utils;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import org.jsoup.Connection.Response;
 import org.jsoup.HttpStatusException;
@@ -11,8 +10,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Document.OutputSettings;
 import org.jsoup.nodes.Element;
 
-import discord4j.core.object.Embed;
+import discord4j.core.DiscordClient;
 import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.api.musixmatch.Musixmatch;
 import me.shadorc.shadbot.core.command.AbstractCommand;
 import me.shadorc.shadbot.core.command.CommandCategory;
 import me.shadorc.shadbot.core.command.Context;
@@ -34,7 +34,6 @@ public class LyricsCmd extends AbstractCommand {
 	// Make html() preserve linebreaks and spacing
 	private static final OutputSettings PRESERVE_FORMAT = new Document.OutputSettings().prettyPrint(false);
 	private static final String HOME_URL = "https://www.musixmatch.com";
-	private static final int MAX_LYRICS_LENGTH = Embed.MAX_DESCRIPTION_LENGTH / 2;
 	private static final int MAX_RETRY = 5;
 
 	@Override
@@ -44,62 +43,34 @@ public class LyricsCmd extends AbstractCommand {
 		final LoadingMessage loadingMsg = new LoadingMessage(context.getClient(), context.getChannelId());
 
 		try {
-			final String artistSrch = NetUtils.encode(args.get(0).replaceAll("[^A-Za-z0-9]", "-"));
-			final String titleSrch = NetUtils.encode(args.get(1).replaceAll("[^A-Za-z0-9]", "-"));
+			final String artist = NetUtils.encode(args.get(0).replace(" ", "-"));
+			final String title = NetUtils.encode(args.get(1).replace(" ", "-"));
 
 			// Make a direct search with the artist and the title
-			String url = String.format("%s/lyrics/%s/%s", HOME_URL, artistSrch, titleSrch);
-
-			// Sometimes musixmatch redirects to a wrong page, check it and reload it if necessary
-			int retryCount = 0;
-			Response response = null;
-			do {
-				if(response != null) {
-					retryCount++;
-					if(retryCount == MAX_RETRY) {
-						LogUtils.warn(context.getClient(), String.format("[%s] %d retries, abort attempt to reload page.",
-								this.getClass().getSimpleName(), retryCount));
-						throw new HttpStatusException("musixmatch does not redirect to the correct page.", HttpStatus.SC_SERVICE_UNAVAILABLE, url);
-					}
-					LogUtils.info("[%s] URLs do not match (%s / %s), reloading page.",
-							this.getClass().getSimpleName(), response.url().toString(), url);
-				}
-				response = NetUtils.getResponse(url);
-			} while(!response.url().toString().equalsIgnoreCase(url));
-
-			Document doc = response.parse().outputSettings(PRESERVE_FORMAT);
-
+			String url = String.format("%s/lyrics/%s/%s", HOME_URL, artist, title);
+			Response response = this.getLyricsResponse(context.getClient(), url);
+			
 			// If the direct search found nothing
-			if(response.statusCode() == HttpStatus.SC_NOT_FOUND || doc.text().contains("Oops! We couldn't find that page.")) {
-
-				final String searchUrl = String.format("%s/search/%s-%s?", HOME_URL, artistSrch, titleSrch);
-
-				// Make a search request on the site
-				final Document searchDoc = NetUtils.getDoc(searchUrl);
-				final Element trackListElement = searchDoc.getElementsByClass("tracks list").first();
-				if(trackListElement == null) {
+			if(response.statusCode() == HttpStatus.SC_NOT_FOUND || response.body().contains("Oops! We couldn't find that page.")) {
+				url = this.getCorrectedUrl(artist, title);
+				if(url == null) {
 					return loadingMsg.send(String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) No Lyrics found for `%s`",
 							context.getUsername(), context.getArg().get()))
 							.then();
 				}
-
-				// Find the first element containing "title" (generally the best result) and get its URL
-				url = HOME_URL + trackListElement.getElementsByClass("title").attr("href");
-				doc = NetUtils.getDoc(url).outputSettings(PRESERVE_FORMAT);
+				response = this.getLyricsResponse(context.getClient(), url);
 			}
-
-			final String artist = doc.getElementsByClass("mxm-track-title__artist").text();
-			final String title = StringUtils.remove(doc.getElementsByClass("mxm-track-title__track ").text(), "Lyrics");
-			final String albumImg = "https:" + doc.getElementsByClass("banner-album-image").select("img").first().attr("src");
-			final String lyrics = StringUtils.abbreviate(
-					NetUtils.br2nl(doc.getElementsByClass("mxm-lyrics__content ").html()), MAX_LYRICS_LENGTH);
+			
+			final Document doc = response.parse().outputSettings(PRESERVE_FORMAT);
+			final Musixmatch musixmatch = new Musixmatch(doc);
 			final String finalUrl = url;
 
 			return context.getAvatarUrl()
 					.map(avatarUrl -> EmbedUtils.getDefaultEmbed()
-							.setAuthor(String.format("Lyrics: %s - %s", artist, title), finalUrl, avatarUrl)
-							.setThumbnail(albumImg)
-							.setDescription(lyrics)
+							.setAuthor(String.format("Lyrics: %s - %s", 
+									musixmatch.getArtist(), musixmatch.getTitle()), finalUrl, avatarUrl)
+							.setThumbnail(musixmatch.getImageUrl())
+							.setDescription(musixmatch.getLyrics())
 							.setFooter("Click on the title to see the full version", "https://www.shareicon.net/download/2015/09/11/99440_info_512x512.png"))
 					.flatMap(loadingMsg::send)
 					.then();
@@ -108,6 +79,39 @@ public class LyricsCmd extends AbstractCommand {
 			loadingMsg.stopTyping();
 			throw Exceptions.propagate(err);
 		}
+	}
+	
+	private final Response getLyricsResponse(DiscordClient client, String url) throws IOException {
+		// Sometimes Musixmatch redirects to a wrong page
+		// If the response URL and the requested URL are different, retry
+		int retryCount = 0;
+		Response response = null;
+		do {
+			if(retryCount == MAX_RETRY) {
+				LogUtils.warn(client, String.format("[%s] Too many retries, abort attempt to reload page.",
+						this.getClass().getSimpleName()));
+				throw new HttpStatusException("Musixmatch does not redirect to the correct page.", HttpStatus.SC_SERVICE_UNAVAILABLE, url);
+			}
+			
+			response = NetUtils.getResponse(url);
+			retryCount++;
+		} while(!response.url().toString().equalsIgnoreCase(url));
+
+		return response;
+	}
+	
+	private final String getCorrectedUrl(String artist, String title) throws IOException {
+		final String url = String.format("%s/search/%s-%s?", HOME_URL, artist, title);
+
+		// Make a search request on the site
+		final Document doc = NetUtils.getDoc(url);
+		final Element trackList = doc.getElementsByClass("tracks list").first();
+		if(trackList == null) {
+			return null;
+		}
+
+		// Find the first element containing "title" (generally the best result) and get its URL
+		return HOME_URL + trackList.getElementsByClass("title").attr("href");
 	}
 
 	@Override
