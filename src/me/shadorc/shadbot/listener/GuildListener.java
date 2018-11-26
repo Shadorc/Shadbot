2c779133489899084a11ccf7a00ec762931b67eb
David <bi-crosseur@hotmail.fr>
2018-11-26 10:57:41 +0100

    Update GuildListener.java

--- src/me/shadorc/shadbot/listener/GuildListener.java
+++ src/me/shadorc/shadbot/listener/GuildListener.java
@@ -2,17 +2,14 @@ package me.shadorc.shadbot.listener;
 
 import discord4j.core.event.domain.guild.GuildCreateEvent;
 import discord4j.core.event.domain.guild.GuildDeleteEvent;
-import me.shadorc.shadbot.Shadbot;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
 
 public class GuildListener {
 
 	public static void onGuildCreate(GuildCreateEvent event) {
-		if(Shadbot.isReady()) {
-			LogUtils.info("Shadbot connected to a guild. (ID: %d | Users: %d)",
-					event.getGuild().getId().asLong(),
-					event.getGuild().getMemberCount().orElse(-1));
-		}
+		LogUtils.info("Shadbot connected to a guild. (ID: %d | Users: %d)",
+				event.getGuild().getId().asLong(),
+				event.getGuild().getMemberCount().orElse(-1));
 	}
 
 	public static void onGuildDelete(GuildDeleteEvent event) {
