b776b279f49e4e894307d183bfabd6b828bdf4f3
David <bi-crosseur@hotmail.fr>
2018-11-26 08:44:16 +0100

    Create StatisticEnum.java

---
+++ src/me/shadorc/shadbot/data/stats/StatisticEnum.java
@@ -0,0 +1,20 @@
+package me.shadorc.shadbot.data.stats;
+
+import me.shadorc.shadbot.data.stats.core.Statistic;
+
+public enum StatisticEnum {
+	VARIOUS(StatsManager.VARIOUS_STATS),
+	COMMAND(StatsManager.COMMAND_STATS),
+	MONEY(StatsManager.MONEY_STATS);
+
+	private final Statistic<?> stat;
+
+	StatisticEnum(Statistic<?> stat) {
+		this.stat = stat;
+	}
+
+	public Statistic<?> getStat() {
+		return this.stat;
+	}
+
+}
