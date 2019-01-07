b240d31d63bd3beeae4d2db743170de1dbe9f3c3
David <bi-crosseur@hotmail.fr>
2019-01-07 15:57:07 +0100

    ReadyListener: Improve error handling

--- src/me/shadorc/shadbot/listener/ReadyListener.java
+++ src/me/shadorc/shadbot/listener/ReadyListener.java
@@ -2,10 +2,9 @@ package me.shadorc.shadbot.listener;
 
 import java.time.Duration;
 
-import discord4j.core.event.domain.lifecycle.GatewayLifecycleEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import me.shadorc.shadbot.utils.DiscordUtils;
-import me.shadorc.shadbot.utils.embed.log.LogUtils;
+import me.shadorc.shadbot.utils.exception.ExceptionHandler;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -14,7 +13,7 @@ public class ReadyListener {
 	public static Mono<Void> onReadyEvent(ReadyEvent event) {
 		return Flux.interval(Duration.ZERO, Duration.ofMinutes(30))
 				.flatMap(ignored -> DiscordUtils.updatePresence(event.getClient()))
-				.onErrorContinue((err, obj) -> LogUtils.error(err, "An unknown error occurred while updating presence."))
+				.onErrorContinue((err, obj) -> ExceptionHandler.handleUnknownError(err, event.getClient()))
 				.then();
 	}
 
