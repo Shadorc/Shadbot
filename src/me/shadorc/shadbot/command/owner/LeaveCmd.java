28b91c7fb8b509dee6fa8c6265e8ef4ecc766009
David <bi-crosseur@hotmail.fr>
2019-01-17 20:58:35 +0100

    Create LeaveCmd

---
+++ src/me/shadorc/shadbot/command/owner/LeaveCmd.java
@@ -0,0 +1,46 @@
+package me.shadorc.shadbot.command.owner;
+
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import me.shadorc.shadbot.core.command.AbstractCommand;
+import me.shadorc.shadbot.core.command.CommandCategory;
+import me.shadorc.shadbot.core.command.CommandPermission;
+import me.shadorc.shadbot.core.command.Context;
+import me.shadorc.shadbot.core.command.annotation.Command;
+import me.shadorc.shadbot.exception.CommandException;
+import me.shadorc.shadbot.utils.DiscordUtils;
+import me.shadorc.shadbot.utils.NumberUtils;
+import me.shadorc.shadbot.utils.embed.help.HelpBuilder;
+import me.shadorc.shadbot.utils.exception.ExceptionUtils;
+import me.shadorc.shadbot.utils.object.Emoji;
+import reactor.core.publisher.Mono;
+
+@Command(category = CommandCategory.OWNER, permission = CommandPermission.OWNER, names = { "leave" })
+public class LeaveCmd extends AbstractCommand {
+
+	@Override
+	public Mono<Void> execute(Context context) {
+		final String arg = context.requireArg();
+
+		final Long guildId = NumberUtils.asPositiveLong(arg);
+		if(guildId == null) {
+			throw new CommandException(String.format("`%s` is not a valid guild ID.", arg));
+		}
+
+		return context.getClient().getGuildById(Snowflake.of(guildId))
+				.onErrorMap(ExceptionUtils::isForbidden, err -> new CommandException("Guild not found."))
+				.flatMap(Guild::leave)
+				.and(context.getChannel()
+						.flatMap(channel -> DiscordUtils.sendMessage(Emoji.INFO + " Guild left.", channel)));
+	}
+
+	@Override
+	public Mono<EmbedCreateSpec> getHelp(Context context) {
+		return new HelpBuilder(this, context)
+				.setDescription("Leave a guild.")
+				.addArg("guildID", false)
+				.build();
+	}
+
+}
