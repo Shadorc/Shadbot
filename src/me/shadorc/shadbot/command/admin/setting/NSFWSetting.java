d72e2d9e9f513476ee4d026a72ee47ab44f94359
David <bi-crosseur@hotmail.fr>
2019-01-21 00:23:14 +0100

    NSFWSetting: Fix missing permission

--- src/me/shadorc/shadbot/command/admin/setting/NSFWSetting.java
+++ src/me/shadorc/shadbot/command/admin/setting/NSFWSetting.java
@@ -1,15 +1,16 @@
 package me.shadorc.shadbot.command.admin.setting;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.util.Permission;
 import discord4j.core.spec.EmbedCreateSpec;
 import me.shadorc.shadbot.core.command.Context;
 import me.shadorc.shadbot.core.setting.AbstractSetting;
 import me.shadorc.shadbot.core.setting.Setting;
 import me.shadorc.shadbot.core.setting.SettingEnum;
 import me.shadorc.shadbot.exception.CommandException;
+import me.shadorc.shadbot.exception.MissingPermissionException.UserType;
 import me.shadorc.shadbot.utils.DiscordUtils;
 import me.shadorc.shadbot.utils.FormatUtils;
 import me.shadorc.shadbot.utils.Utils;
@@ -33,26 +34,21 @@ public class NSFWSetting extends AbstractSetting {
 			throw new CommandException(String.format("`%s` is not a valid action. %s", args.get(1), FormatUtils.options(Action.class)));
 		}
 
-		return context.getChannel()
-				.cast(TextChannel.class)
-				.flatMap(channel -> {
-					final AtomicBoolean isNSFW = new AtomicBoolean(false);
-					switch (action) {
-						case TOGGLE:
-							isNSFW.set(!channel.isNsfw());
-							break;
-						case ENABLE:
-							isNSFW.set(true);
-							break;
-						case DISABLE:
-							isNSFW.set(false);
-							break;
-					}
-
-					return channel.edit(spec -> spec.setNsfw(isNSFW.get()));
-				})
-				.flatMap(channel -> DiscordUtils.sendMessage(String.format(Emoji.CHECK_MARK + " (**%s**) This channel is now **%sSFW**.",
-						context.getUsername(), channel.isNsfw() ? "N" : ""), channel))
+		return context.getChannel().cast(TextChannel.class)
+				.flatMap(channel -> DiscordUtils.requirePermissions(channel, context.getSelfId(), UserType.BOT, Permission.MANAGE_CHANNELS)
+						.then(Mono.fromSupplier(() -> {
+							switch (action) {
+								case TOGGLE:
+									return !channel.isNsfw();
+								case ENABLE:
+									return true;
+								default:
+									return false;
+							}
+						}))
+						.flatMap(nsfw -> channel.edit(spec -> spec.setNsfw(nsfw)))
+						.then(DiscordUtils.sendMessage(String.format(Emoji.CHECK_MARK + " (**%s**) %s is now **%sSFW**.",
+								context.getUsername(), channel.getMention(), channel.isNsfw() ? "N" : ""), channel)))
 				.then();
 	}
 
