2383aa39afa8b7171e110d267239aca2ac547e17
David <bi-crosseur@hotmail.fr>
2019-01-23 11:11:53 +0100

    AllowedRolesSetting: Fix exceptions catching and improve role mentions

--- src/me/shadorc/shadbot/command/admin/setting/AllowedRolesSetting.java
+++ src/me/shadorc/shadbot/command/admin/setting/AllowedRolesSetting.java
@@ -1,9 +1,8 @@
 package me.shadorc.shadbot.command.admin.setting;
 
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.Role;
 import discord4j.core.object.util.Snowflake;
 import discord4j.core.spec.EmbedCreateSpec;
@@ -14,13 +13,11 @@ import me.shadorc.shadbot.core.setting.Setting;
 import me.shadorc.shadbot.core.setting.SettingEnum;
 import me.shadorc.shadbot.data.database.DBGuild;
 import me.shadorc.shadbot.exception.CommandException;
-import me.shadorc.shadbot.exception.MissingArgumentException;
 import me.shadorc.shadbot.utils.DiscordUtils;
 import me.shadorc.shadbot.utils.FormatUtils;
 import me.shadorc.shadbot.utils.Utils;
 import me.shadorc.shadbot.utils.embed.EmbedUtils;
 import me.shadorc.shadbot.utils.object.Emoji;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Setting(description = "Manage role(s) that can interact with Shadbot.", setting = SettingEnum.ALLOWED_ROLES)
@@ -39,50 +36,46 @@ public class AllowedRolesSetting extends AbstractSetting {
 			throw new CommandException(String.format("`%s` is not a valid action. %s", args.get(1), FormatUtils.options(Action.class)));
 		}
 
-		final Set<Snowflake> mentionedRoles = context.getMessage().getRoleMentionIds();
-		if(mentionedRoles.isEmpty()) {
-			throw new MissingArgumentException();
-		}
+		return context.getGuild()
+				.flatMapMany(guild -> DiscordUtils.extractRoles(guild, args.get(2)))
+				.flatMap(roleId -> context.getClient().getRoleById(context.getGuildId(), roleId))
+				.collectList()
+				.map(mentionedRoles -> {
+					if(mentionedRoles.isEmpty()) {
+						throw new CommandException(String.format("Role `%s` not found.", args.get(2)));
+					}
 
-		final DBGuild dbGuild = Shadbot.getDatabase().getDBGuild(context.getGuildId());
-		final List<Snowflake> allowedRoles = dbGuild.getAllowedRoles();
+					final DBGuild dbGuild = Shadbot.getDatabase().getDBGuild(context.getGuildId());
+					final List<Snowflake> allowedRoles = dbGuild.getAllowedRoles();
+					
+					System.err.println(mentionedRoles);
 
-		Mono<Message> message;
-		if(Action.ADD.equals(action)) {
-			allowedRoles.addAll(mentionedRoles);
-			message = Flux.fromIterable(allowedRoles)
-					.flatMap(roleId -> context.getClient().getRoleById(context.getGuildId(), roleId))
-					.map(Role::getName)
-					.collectList()
-					.flatMap(roleNames -> context.getChannel()
-							.flatMap(channel -> DiscordUtils.sendMessage(String.format(Emoji.CHECK_MARK + " %s will now be able to interact with me.",
-									String.join(", ", roleNames)), channel)));
-		} else {
-			allowedRoles.removeAll(mentionedRoles);
-			message = Flux.fromIterable(mentionedRoles)
-					.flatMap(roleId -> context.getClient().getRoleById(context.getGuildId(), roleId))
-					.map(Role::getName)
-					.collectList()
-					.map(roleNames -> {
-						final StringBuilder text = new StringBuilder(String.format(Emoji.CHECK_MARK + " %s will not be able to interact with me anymore.",
-								String.join(", ", roleNames)));
-						if(roleNames.isEmpty()) {
-							text.append("\n" + Emoji.INFO + " There are no more roles set, everyone can now interact with me.");
+					final StringBuilder strBuilder = new StringBuilder();
+					if(Action.ADD.equals(action)) {
+						allowedRoles.addAll(mentionedRoles.stream().map(Role::getId).collect(Collectors.toList()));
+						strBuilder.append(String.format(Emoji.CHECK_MARK + " %s will now be able to interact with me.",
+								FormatUtils.format(mentionedRoles, role -> String.format("`@%s`", role.getName()), ", ")));
+					} else {
+						allowedRoles.removeAll(mentionedRoles.stream().map(Role::getId).collect(Collectors.toList()));
+						strBuilder.append(String.format(Emoji.CHECK_MARK + " %s will not be able to interact with me anymore.",
+								FormatUtils.format(mentionedRoles, role -> String.format("`@%s`", role.getName()), ", ")));
+						if(allowedRoles.isEmpty()) {
+							strBuilder.append("\n" + Emoji.INFO + " There are no more roles set, everyone can now interact with me.");
 						}
-						return text.toString();
-					})
-					.flatMap(text -> context.getChannel()
-							.flatMap(channel -> DiscordUtils.sendMessage(text, channel)));
-		}
+					}
 
-		dbGuild.setSetting(this.getSetting(), allowedRoles);
-		return message.then();
+					dbGuild.setSetting(this.getSetting(), allowedRoles);
+					return strBuilder.toString();
+				})
+				.flatMap(text -> context.getChannel()
+						.flatMap(channel -> DiscordUtils.sendMessage(text, channel)))
+				.then();
 	}
 
 	@Override
 	public EmbedCreateSpec getHelp(Context context) {
 		return EmbedUtils.getDefaultEmbed()
-				.addField("Usage", String.format("`%s%s <action> <@role(s)>`", context.getPrefix(), this.getCommandName()), false)
+				.addField("Usage", String.format("`%s%s <action> <role(s)>`", context.getPrefix(), this.getCommandName()), false)
 				.addField("Argument", String.format("**action** - %s",
 						FormatUtils.format(Action.class, "/")), false)
 				.addField("Example", String.format("`%s%s add @admin`", context.getPrefix(), this.getCommandName()), false)
