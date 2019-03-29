ffb4946ffdeedcfcb1756812dfb5353cd18169b4
David <bi-crosseur@hotmail.fr>
2019-03-29 16:48:07 +0100

    RussianRoulette: Now has a constant bet

--- src/me/shadorc/shadbot/command/game/RussianRouletteCmd.java
+++ src/me/shadorc/shadbot/command/game/RussianRouletteCmd.java
@@ -22,8 +22,11 @@ public class RussianRouletteCmd extends BaseCmd {
 
 	private static final int PAID_COST = 250;
 
-	private static final float WIN_MULTIPLIER = 4f;
-	private static final float LOSE_MULTIPLIER = 14f;
+	private static final int MIN_GAINS = 1500;
+	private static final int MAX_GAINS = 6000;
+
+	private static final int MIN_LOSE = 4000;
+	private static final int MAX_LOSE = 12000;
 
 	public RussianRouletteCmd() {
 		super(CommandCategory.GAME, List.of("russian_roulette", "russian-roulette", "russianroulette"), "rr");
@@ -32,21 +35,31 @@ public class RussianRouletteCmd extends BaseCmd {
 
 	@Override
 	public Mono<Void> execute(Context context) {
+		// TODO: Remove this in the next update
+		if(context.getArg().isPresent()) {
+			return context.getChannel()
+					.flatMap(channel -> DiscordUtils.sendMessage(String.format(Emoji.INFO +
+							" To avoid making a lot of coins very easily and quickly, **the Russian "
+							+ "roulette now has a constant bet of %d coins.** Retry without "
+							+ "specyfying a bet.", PAID_COST), channel))
+					.then();
+		}
+
 		Utils.requireValidBet(context.getMember(), Integer.toString(PAID_COST));
 
 		final StringBuilder strBuilder = new StringBuilder(
 				String.format(Emoji.DICE + " (**%s**) You break a sweat, you pull the trigger... ", context.getUsername()));
 
-		int gains;
+		int gains = -PAID_COST;
 		if(ThreadLocalRandom.current().nextInt(6) == 0) {
-			gains = (int) -Math.ceil(PAID_COST * LOSE_MULTIPLIER);
+			gains -= ThreadLocalRandom.current().nextInt(MIN_LOSE, MAX_LOSE + 1);
 			StatsManager.MONEY_STATS.log(MoneyEnum.MONEY_LOST, this.getName(), Math.abs(gains));
 			Shadbot.getLottery().addToJackpot(Math.abs(gains));
-			strBuilder.append(String.format("**PAN** ... Sorry, you died. You lose **%s**.", FormatUtils.coins(Math.abs(gains))));
+			strBuilder.append(String.format("**PAN** ... Sorry, you died.%nYou lose **%s**.", FormatUtils.coins(Math.abs(gains))));
 		} else {
-			gains = (int) Math.ceil(PAID_COST * WIN_MULTIPLIER);
+			gains += ThreadLocalRandom.current().nextInt(MIN_GAINS, MAX_GAINS + 1);
 			StatsManager.MONEY_STATS.log(MoneyEnum.MONEY_GAINED, this.getName(), gains);
-			strBuilder.append(String.format("**click** ... Phew, you are still alive ! You get **%s**.", FormatUtils.coins(gains)));
+			strBuilder.append(String.format("**click** ... Phew, you are still alive !%nYou get **%s**.", FormatUtils.coins(gains)));
 		}
 
 		Shadbot.getDatabase().getDBMember(context.getGuildId(), context.getAuthorId()).addCoins(gains);
