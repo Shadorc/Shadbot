3a77c6acbc047def31539bb46f657536b2943c07
David <bi-crosseur@hotmail.fr>
2019-05-28 15:56:16 +0200

    ChatCmd: Improved and now fully reactive

--- src/me/shadorc/shadbot/command/fun/ChatCmd.java
+++ src/me/shadorc/shadbot/command/fun/ChatCmd.java
@@ -13,34 +13,29 @@ import me.shadorc.shadbot.utils.NetUtils;
 import me.shadorc.shadbot.utils.Utils;
 import me.shadorc.shadbot.utils.embed.help.HelpBuilder;
 import me.shadorc.shadbot.utils.embed.log.LogUtils;
-import org.json.JSONObject;
 import org.json.XML;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class ChatCmd extends BaseCmd {
 
+    private static final String HOME_URl = "https://www.pandorabots.com/pandora/talk-xml";
     private static final Map<String, String> BOTS = Map.of("Marvin", "efc39100ce34d038", "Chomsky", "b0dafd24ee35a477",
             "R.I.V.K.A", "ea373c261e3458c6", "Lisa", "b0a6a41a5e345c23");
-    private static final int MAX_ERROR_COUNT = 10;
     private static final int MAX_CHARACTERS = 250;
 
     private final Map<Snowflake, String> channelsCustid;
-    private final AtomicInteger errorCount;
 
     public ChatCmd() {
         super(CommandCategory.FUN, List.of("chat"));
         this.setDefaultRateLimiter();
 
         this.channelsCustid = new ConcurrentHashMap<>();
-        this.errorCount = new AtomicInteger(0);
     }
 
     @Override
@@ -52,38 +47,34 @@ public class ChatCmd extends BaseCmd {
                     MAX_CHARACTERS)));
         }
 
-        for (final Entry<String, String> bot : BOTS.entrySet()) {
-            try {
-                final String response = this.talk(context.getChannelId(), bot.getValue(), arg);
-                this.errorCount.set(0);
-                return context.getChannel()
-                        .flatMap(channel -> DiscordUtils.sendMessage(String.format(Emoji.SPEECH + " **%s**: %s", bot.getKey(), response), channel))
-                        .then();
-            } catch (final IOException err) {
-                LogUtils.info("{%s} %s is not reachable, trying another one.", this.getClass().getSimpleName(), bot.getKey());
-            }
-        }
-
-        if (this.errorCount.incrementAndGet() >= MAX_ERROR_COUNT) {
-            LogUtils.error(context.getClient(),
-                    String.format("{%s} No artificial intelligence responds (Error count: %d).",
-                            this.getClass().getSimpleName(), this.errorCount.get()));
-        }
-
-        return context.getChannel()
-                .flatMap(channel -> DiscordUtils.sendMessage(
-                        String.format(Emoji.SLEEPING + " (**%s**) Sorry, A.L.I.C.E. seems to be AFK, she'll probably come back later.",
-                                context.getUsername()), channel))
+        return this.getResponse(context.getChannelId(), arg)
+                .flatMap(response -> context.getChannel()
+                        .flatMap(channel -> DiscordUtils.sendMessage(Emoji.SPEECH + " " + response, channel)))
                 .then();
     }
 
-    private String talk(Snowflake channelId, String botId, String input) throws IOException {
-        final String url = String.format("https://www.pandorabots.com/pandora/talk-xml?botid=%s&input=%s&custid=%s",
-                botId, NetUtils.encode(input), this.channelsCustid.getOrDefault(channelId, ""));
-        final JSONObject resultObj = XML.toJSONObject(NetUtils.get(url).block()).getJSONObject("result");
-        final ChatBotResponse chat = Utils.MAPPER.readValue(resultObj.toString(), ChatBotResponse.class);
-        this.channelsCustid.put(channelId, chat.getCustId());
-        return chat.getResponse();
+    private Mono<String> getResponse(Snowflake channelId, String input) {
+        return Flux.fromIterable(BOTS.entrySet())
+                .flatMap(bot -> this.talk(channelId, bot.getValue(), input)
+                        .map(response -> String.format("**%s**: %s", bot.getKey(), response)))
+                .takeUntil(str -> !str.isBlank())
+                .next();
+    }
+
+    private Mono<String> talk(Snowflake channelId, String botId, String input) {
+        final String url = String.format("%s?botid=%s&input=%s&custid=%s",
+                HOME_URl, botId, NetUtils.encode(input), this.channelsCustid.getOrDefault(channelId, ""));
+
+        return NetUtils.get(url)
+                .map(XML::toJSONObject)
+                .map(jsonObj -> jsonObj.getJSONObject("result"))
+                .flatMap(resultObj -> Mono.fromCallable(() -> Utils.MAPPER.readValue(resultObj.toString(), ChatBotResponse.class)))
+                .doOnNext(chat -> this.channelsCustid.put(channelId, chat.getCustId()))
+                .map(ChatBotResponse::getResponse)
+                .onErrorResume(err -> {
+                    LogUtils.info("{%s} %s is not reachable, trying another one.", this.getClass().getSimpleName(), botId);
+                    return Mono.empty();
+                });
     }
 
     @Override
