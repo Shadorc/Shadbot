0023378b6f79ac0a117acc0e059a614f00fec6eb
David <bi-crosseur@hotmail.fr>
2019-06-17 22:23:13 +0200

    UrbanCmd: Now fully reactive

--- src/me/shadorc/shadbot/command/utils/UrbanCmd.java
+++ src/me/shadorc/shadbot/command/utils/UrbanCmd.java
@@ -31,31 +31,30 @@ public class UrbanCmd extends BaseCmd {
         final String arg = context.requireArg();
 
         final LoadingMessage loadingMsg = new LoadingMessage(context.getClient(), context.getChannelId());
-        return Mono.fromCallable(() -> {
-            final String url = String.format("https://api.urbandictionary.com/v0/define?term=%s", NetUtils.encode(arg));
 
-            final UrbanDictionaryResponse urbanDictionary = NetUtils.get(url, UrbanDictionaryResponse.class).block();
+        final String url = String.format("https://api.urbandictionary.com/v0/define?term=%s", NetUtils.encode(arg));
+        return NetUtils.get(url, UrbanDictionaryResponse.class)
+                .map(urbanDictionary -> {
+                    if (urbanDictionary.getDefinitions().isEmpty()) {
+                        return loadingMsg.setContent(String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) No urban definitions found for `%s`",
+                                context.getUsername(), arg));
+                    }
 
-            if (urbanDictionary.getDefinitions().isEmpty()) {
-                return loadingMsg.setContent(String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) No urban definitions found for `%s`",
-                        context.getUsername(), arg));
-            }
+                    final UrbanDefinition urbanDefinition = urbanDictionary.getDefinitions().get(0);
 
-            final UrbanDefinition urbanDefinition = urbanDictionary.getDefinitions().get(0);
+                    final String definition = StringUtils.abbreviate(urbanDefinition.getDefinition(), Embed.MAX_DESCRIPTION_LENGTH);
+                    final String example = StringUtils.abbreviate(urbanDefinition.getExample(), Field.MAX_VALUE_LENGTH);
 
-            final String definition = StringUtils.abbreviate(urbanDefinition.getDefinition(), Embed.MAX_DESCRIPTION_LENGTH);
-            final String example = StringUtils.abbreviate(urbanDefinition.getExample(), Field.MAX_VALUE_LENGTH);
-
-            return loadingMsg.setEmbed(EmbedUtils.getDefaultEmbed()
-                    .andThen(embed -> {
-                        embed.setAuthor(String.format("Urban Dictionary: %s", urbanDefinition.getWord()), urbanDefinition.getPermalink(), context.getAvatarUrl())
-                                .setThumbnail("http://www.packal.org/sites/default/files/public/styles/icon_large/public/workflow-files/florianurban/icon/icon.png")
-                                .setDescription(definition);
-                        if (!example.isBlank()) {
-                            embed.addField("Example", example, false);
-                        }
-                    }));
-        })
+                    return loadingMsg.setEmbed(EmbedUtils.getDefaultEmbed()
+                            .andThen(embed -> {
+                                embed.setAuthor(String.format("Urban Dictionary: %s", urbanDefinition.getWord()), urbanDefinition.getPermalink(), context.getAvatarUrl())
+                                        .setThumbnail("http://www.packal.org/sites/default/files/public/styles/icon_large/public/workflow-files/florianurban/icon/icon.png")
+                                        .setDescription(definition);
+                                if (!example.isBlank()) {
+                                    embed.addField("Example", example, false);
+                                }
+                            }));
+                })
                 .flatMap(LoadingMessage::send)
                 .doOnTerminate(loadingMsg::stopTyping)
                 .then();
