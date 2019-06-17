739d6a12ca73b80e9ecf9353a1329a870711eba6
David <bi-crosseur@hotmail.fr>
2019-06-17 21:54:22 +0200

    JokeCmd: Now fully reactive

--- src/me/shadorc/shadbot/command/french/JokeCmd.java
+++ src/me/shadorc/shadbot/command/french/JokeCmd.java
@@ -12,11 +12,11 @@ import me.shadorc.shadbot.utils.embed.EmbedUtils;
 import me.shadorc.shadbot.utils.embed.help.HelpBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class JokeCmd extends BaseCmd {
 
@@ -30,22 +30,23 @@ public class JokeCmd extends BaseCmd {
     @Override
     public Mono<Void> execute(Context context) {
         final LoadingMessage loadingMsg = new LoadingMessage(context.getClient(), context.getChannelId());
-
-        return Mono.fromCallable(() -> {
-            final List<String> jokes = Jsoup.parse(NetUtils.get(HOME_URL).block())
-                    .getElementsByClass("gag__content")
-                    .stream()
-                    .map(Element::html)
-                    .filter(elmt -> elmt.length() < 1000)
-                    .collect(Collectors.toList());
-
-            final String joke = FormatUtils.format(Utils.randValue(jokes).split("<br>"),
-                    line -> Jsoup.parse(line).text().trim(), "\n");
-
-            return loadingMsg.setEmbed(EmbedUtils.getDefaultEmbed()
-                    .andThen(embed -> embed.setAuthor("Blague", "https://www.humour.com/blagues/", context.getAvatarUrl())
-                            .setDescription(joke)));
-        })
+        return NetUtils.get(HOME_URL)
+                .map(Jsoup::parse)
+                // Get all elements representing a joke
+                .map(doc -> doc.getElementsByClass("gag__content"))
+                .flatMapMany(Flux::fromIterable)
+                .map(Element::html)
+                // Filter joke with more than 1000 characters
+                .filter(element -> element.length() < 1000)
+                .collectList()
+                // Pick a random joke
+                .map(Utils::randValue)
+                .map(joke -> joke.split("<br>"))
+                // Remove HTML codes in joke string
+                .map(lines -> FormatUtils.format(lines, line -> Jsoup.parse(line).text().trim(), "\n"))
+                .map(joke -> loadingMsg.setEmbed(EmbedUtils.getDefaultEmbed()
+                        .andThen(embed -> embed.setAuthor("Blague", HOME_URL, context.getAvatarUrl())
+                                .setDescription(joke))))
                 .flatMap(LoadingMessage::send)
                 .doOnTerminate(loadingMsg::stopTyping)
                 .then();
@@ -55,7 +56,7 @@ public class JokeCmd extends BaseCmd {
     public Consumer<EmbedCreateSpec> getHelp(Context context) {
         return new HelpBuilder(this, context)
                 .setDescription("Show a random French joke.")
-                .setSource("https://www.humour.com/blagues/")
+                .setSource(HOME_URL)
                 .build();
     }
 }
