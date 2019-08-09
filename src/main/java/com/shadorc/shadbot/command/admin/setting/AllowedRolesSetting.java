be5a9884f2c227f7e1b355918c5e3ab18bbd9d3b
David <bi-crosseur@hotmail.fr>
2019-08-09 14:55:42 +0200

    AllowedRolesSetting: Improve message consistency

--- src/main/java/com/shadorc/shadbot/command/admin/setting/AllowedRolesSetting.java
+++ src/main/java/com/shadorc/shadbot/command/admin/setting/AllowedRolesSetting.java
@@ -62,8 +62,9 @@ public class AllowedRolesSetting extends BaseSetting {
                         allowedRoles.removeAll(mentionedRoleIds);
                         strBuilder.append(String.format(Emoji.CHECK_MARK + " %s will not be able to interact with me anymore.",
                                 FormatUtils.format(mentionedRoles, role -> String.format("`@%s`", role.getName()), ", ")));
+                        
                         if (allowedRoles.isEmpty()) {
-                            strBuilder.append("\n" + Emoji.INFO + " There are no more roles set, everyone can now interact with me.");
+                            strBuilder.append("\n" + Emoji.INFO + " There are no more allowed roles set, everyone can now interact with me.");
                         }
                     }
 
