b53d7c4ebfbca17c9838f6e4102cea42c08f61d9
David <bi-crosseur@hotmail.fr>
2019-08-17 16:33:13 +0200

    PlaylistCmd: Add current track to the playlist

--- src/main/java/com/shadorc/shadbot/command/music/PlaylistCmd.java
+++ src/main/java/com/shadorc/shadbot/command/music/PlaylistCmd.java
@@ -5,6 +5,7 @@ import com.shadorc.shadbot.core.command.BaseCmd;
 import com.shadorc.shadbot.core.command.CommandCategory;
 import com.shadorc.shadbot.core.command.Context;
 import com.shadorc.shadbot.music.GuildMusic;
+import com.shadorc.shadbot.music.TrackScheduler;
 import com.shadorc.shadbot.object.help.HelpBuilder;
 import com.shadorc.shadbot.utils.DiscordUtils;
 import com.shadorc.shadbot.utils.FormatUtils;
@@ -12,7 +13,6 @@ import com.shadorc.shadbot.utils.StringUtils;
 import discord4j.core.spec.EmbedCreateSpec;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -30,24 +30,28 @@ public class PlaylistCmd extends BaseCmd {
         final Consumer<EmbedCreateSpec> embedConsumer = DiscordUtils.getDefaultEmbed()
                 .andThen(embed -> embed.setAuthor("Playlist", null, context.getAvatarUrl())
                         .setThumbnail("https://i.imgur.com/IG3Hj2W.png")
-                        .setDescription(PlaylistCmd.formatPlaylist(guildMusic.getTrackScheduler().getPlaylist())));
+                        .setDescription(PlaylistCmd.formatPlaylist(guildMusic.getTrackScheduler())));
 
         return context.getChannel()
                 .flatMap(channel -> DiscordUtils.sendMessage(embedConsumer, channel))
                 .then();
     }
 
-    private static String formatPlaylist(Collection<AudioTrack> queue) {
-        if (queue.isEmpty()) {
+    private static String formatPlaylist(TrackScheduler trackScheduler) {
+        final AudioTrack currentTrack = trackScheduler.getAudioPlayer().getPlayingTrack();
+        final int musicCount = trackScheduler.getPlaylist().size() + (currentTrack == null ? 0 : 1);
+        if (musicCount == 0) {
             return "**The playlist is empty.**";
         }
 
-        final StringBuilder playlistStr = new StringBuilder(String.format("**%s in the playlist:**%n", StringUtils.pluralOf(queue.size(), "music")));
+        final StringBuilder playlistStr = new StringBuilder(String.format("**%s in the playlist:**%n",
+                StringUtils.pluralOf(musicCount, "music")));
 
-        int count = 1;
-        for (final AudioTrack track : queue) {
-            final String name = String.format("%n\t**%d.** [%s](%s)",
-                    count, FormatUtils.trackName(track.getInfo()), track.getInfo().uri);
+        playlistStr.append(String.format("%n\t**1.** %s", FormatUtils.trackName(currentTrack.getInfo())));
+
+        int count = 2;
+        for (final AudioTrack track : trackScheduler.getPlaylist()) {
+            final String name = String.format("%n\t**%d.** %s", count, FormatUtils.trackName(track.getInfo()));
             if (playlistStr.length() + name.length() < 1800) {
                 playlistStr.append(name);
             } else {
