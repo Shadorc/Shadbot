c09848e9387ded4051c823872fe61a4e431fe1fe
Shadorc <bi-crosseur@hotmail.fr>
2020-01-31 20:37:30 +0100

    Doc

--- src/main/java/com/shadorc/shadbot/listener/music/TrackEventListener.java
+++ src/main/java/com/shadorc/shadbot/listener/music/TrackEventListener.java
@@ -102,9 +102,9 @@ public class TrackEventListener extends AudioEventAdapter {
     }
 
     /**
-     * Start the next track or end the guild music if this is the end of the playlist
+     * Start the next track or end the guild music if this is the end of the playlist.
      *
-     * @return A {@link Mono} that completes when a new track has been started or when the guild music ended
+     * @return A {@link Mono} that completes when a new track has been started or when the guild music ended.
      */
     private Mono<Void> nextOrEnd() {
         return Mono.justOrEmpty(MusicManager.getInstance().getMusic(this.guildId))
