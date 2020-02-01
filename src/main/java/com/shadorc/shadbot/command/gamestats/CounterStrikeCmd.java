6d361f2dbac38c87b8dabda3bf77212d465d8a4c
Shadorc <bi-crosseur@hotmail.fr>
2020-02-01 22:46:33 +0100

    CounterStrike: Improve display

--- src/main/java/com/shadorc/shadbot/command/gamestats/CounterStrikeCmd.java
+++ src/main/java/com/shadorc/shadbot/command/gamestats/CounterStrikeCmd.java
@@ -18,16 +18,20 @@ import com.shadorc.shadbot.object.help.HelpBuilder;
 import com.shadorc.shadbot.object.message.UpdatableMessage;
 import com.shadorc.shadbot.utils.*;
 import discord4j.core.spec.EmbedCreateSpec;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class CounterStrikeCmd extends BaseCmd {
 
     private static final String PRIVACY_HELP_URL = "https://support.steampowered.com/kb_article.php?ref=4113-YUDH-6401";
+    private static final String PLAYER_SUMMARIES_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
+    private static final String USER_STATS_FOR_GAME_URL = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/";
+    private static final String RESOLVE_VANITY_URL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/";
 
     public CounterStrikeCmd() {
         super(CommandCategory.GAMESTATS, List.of("cs", "csgo"));
@@ -43,65 +47,75 @@ public class CounterStrikeCmd extends BaseCmd {
                 .send()
                 .thenReturn(this.getIdentificator(arg))
                 .flatMap(this::getSteamId)
-                .map(steamId -> String.format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s",
-                        CredentialManager.getInstance().get(Credential.STEAM_API_KEY), steamId))
+                .map(steamId -> String.format("%s?key=%s&steamids=%s",
+                        PLAYER_SUMMARIES_URL, CredentialManager.getInstance().get(Credential.STEAM_API_KEY), steamId))
                 .flatMap(url -> NetUtils.get(url, PlayerSummariesResponse.class))
                 .map(PlayerSummariesResponse::getResponse)
                 // Search users matching the steamId
                 .map(PlayerSummaries::getPlayers)
-                .filter(players -> !players.isEmpty())
-                .map(players -> players.get(0))
-                .flatMap(player -> {
-                    if (player.getCommunityVisibilityState() != PlayerSummary.CommunityVisibilityState.PUBLIC) {
-                        return Mono.just(updatableMsg.setContent(
-                                String.format(Emoji.ACCESS_DENIED + " (**%s**) This profile is private, more info here: <%s>",
-                                        context.getUsername(), PRIVACY_HELP_URL)));
-                    }
-
-                    final String userStatsUrl = String.format("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=%s&steamid=%s",
-                            CredentialManager.getInstance().get(Credential.STEAM_API_KEY), player.getSteamId());
-
-                    return NetUtils.get(userStatsUrl)
-                            .flatMap(body -> {
-                                if (body.contains("500 Internal Server Error")) {
-                                    return Mono.just(updatableMsg.setContent(
-                                            String.format(Emoji.ACCESS_DENIED + " (**%s**) The game details of this profile are not public, more info here: <%s>",
-                                                    context.getUsername(), PRIVACY_HELP_URL)));
-                                }
-
-                                return Mono.fromCallable(() -> Utils.MAPPER.readValue(body, UserStatsForGameResponse.class))
-                                        .map(userStats -> {
-                                            if (userStats.getPlayerStats().flatMap(PlayerStats::getStats).isEmpty()) {
-                                                return updatableMsg.setContent(
-                                                        String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) This user doesn't play Counter-Strike: Global Offensive.",
-                                                                context.getUsername()));
-                                            }
-
-                                            final List<Stats> stats = userStats.getPlayerStats().flatMap(PlayerStats::getStats).get();
-
-                                            final Map<String, Integer> statsMap = new HashMap<>();
-                                            stats.forEach(stat -> statsMap.put(stat.getName(), stat.getValue()));
-
-                                            return updatableMsg.setEmbed(DiscordUtils.getDefaultEmbed()
-                                                    .andThen(embed -> embed.setAuthor("Counter-Strike: Global Offensive Stats",
-                                                            String.format("http://steamcommunity.com/profiles/%s", player.getSteamId()),
-                                                            context.getAvatarUrl())
-                                                            .setThumbnail(player.getAvatarFull())
-                                                            .setDescription(String.format("Stats for **%s**", player.getPersonaName()))
-                                                            .addField("Kills", FormatUtils.number(statsMap.get("total_kills")), true)
-                                                            .addField("Deaths", FormatUtils.number(statsMap.get("total_deaths")), true)
-                                                            .addField("Total wins", FormatUtils.number(statsMap.get("total_wins")), true)
-                                                            .addField("Total MVP", FormatUtils.number(statsMap.get("total_mvps")), true)
-                                                            .addField("Ratio", String.format("%.2f", (float) statsMap.get("total_kills") / statsMap.get("total_deaths")), false)));
-                                        });
-                            });
-                })
+                .flatMapMany(Flux::fromIterable)
+                .next()
+                .flatMap(player -> this.getStats(context, updatableMsg, player))
                 .switchIfEmpty(Mono.defer(() -> Mono.just(updatableMsg.setContent(
-                        String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) Steam player not found.",
-                                context.getUsername())))))
+                        String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) Steam player not found.", context.getUsername())))))
                 .flatMap(UpdatableMessage::send)
                 .onErrorResume(err -> updatableMsg.deleteMessage().then(Mono.error(err)))
                 .then();
+    }
+
+    private Mono<UpdatableMessage> getStats(Context context, UpdatableMessage updatableMsg, PlayerSummary player) {
+        if (player.getCommunityVisibilityState() != PlayerSummary.CommunityVisibilityState.PUBLIC) {
+            return Mono.just(updatableMsg.setContent(
+                    String.format(Emoji.ACCESS_DENIED + " (**%s**) This profile is private, more info here: <%s>",
+                            context.getUsername(), PRIVACY_HELP_URL)));
+        }
+
+        final String userStatsUrl = String.format("%s?appid=730&key=%s&steamid=%s",
+                USER_STATS_FOR_GAME_URL, CredentialManager.getInstance().get(Credential.STEAM_API_KEY), player.getSteamId());
+
+        return NetUtils.get(userStatsUrl)
+                .flatMap(body -> {
+                    if (body.contains("500 Internal Server Error")) {
+                        return Mono.just(updatableMsg.setContent(
+                                String.format(Emoji.ACCESS_DENIED + " (**%s**) The game details of this " +
+                                                "profile are not public, more info here: <%s>",
+                                        context.getUsername(), PRIVACY_HELP_URL)));
+                    }
+
+                    return Mono.fromCallable(() -> Utils.MAPPER.readValue(body, UserStatsForGameResponse.class))
+                            .map(userStats -> {
+                                if (userStats.getPlayerStats().flatMap(PlayerStats::getStats).isEmpty()) {
+                                    return updatableMsg.setContent(
+                                            String.format(Emoji.MAGNIFYING_GLASS + " (**%s**) This user " +
+                                                            "doesn't play Counter-Strike: Global Offensive.",
+                                                    context.getUsername()));
+                                }
+
+                                final Map<String, Integer> statsMap = userStats.getPlayerStats()
+                                        .flatMap(PlayerStats::getStats)
+                                        .get()
+                                        .stream()
+                                        .collect(Collectors.toMap(Stats::getName, Stats::getValue));
+
+                                final int kills = statsMap.get("total_kills");
+                                final int deaths = statsMap.get("total_deaths");
+                                final float ratio = (float) kills / deaths;
+                                final int wins = statsMap.get("total_wins");
+                                final int mvps = statsMap.get("total_mvps");
+
+                                return updatableMsg.setEmbed(DiscordUtils.getDefaultEmbed()
+                                        .andThen(embed -> embed.setAuthor("Counter-Strike: Global Offensive Stats",
+                                                String.format("http://steamcommunity.com/profiles/%s", player.getSteamId()),
+                                                context.getAvatarUrl())
+                                                .setThumbnail(player.getAvatarFull())
+                                                .setDescription(String.format("Stats for **%s**", player.getPersonaName()))
+                                                .addField("Kills", FormatUtils.number(kills), true)
+                                                .addField("Deaths", FormatUtils.number(deaths), true)
+                                                .addField("Ratio", String.format("%.2f", ratio), true)
+                                                .addField("Total wins", FormatUtils.number(wins), true)
+                                                .addField("Total MVP", FormatUtils.number(mvps), true)));
+                            });
+                });
     }
 
     /**
@@ -128,8 +142,8 @@ public class CounterStrikeCmd extends BaseCmd {
         }
         // The user provided a pseudo
         else {
-            final String url = String.format("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s",
-                    CredentialManager.getInstance().get(Credential.STEAM_API_KEY), NetUtils.encode(identificator));
+            final String url = String.format("%s?key=%s&vanityurl=%s",
+                    RESOLVE_VANITY_URL, CredentialManager.getInstance().get(Credential.STEAM_API_KEY), NetUtils.encode(identificator));
             return NetUtils.get(url, ResolveVanityUrlResponse.class)
                     .map(ResolveVanityUrlResponse::getResponse)
                     .map(Response::getSteamId)
