c681a8886382e3ad6f37c0a296f65e229e416699
David <bi-crosseur@hotmail.fr>
2020-03-24 23:16:24 +0100

    Sentry is now syncrhonized with logback to log all errors and warnings

--- src/main/java/com/shadorc/shadbot/utils/LogUtils.java
+++ src/main/java/com/shadorc/shadbot/utils/LogUtils.java
@@ -1,81 +1,26 @@
 package com.shadorc.shadbot.utils;
 
-import com.shadorc.shadbot.data.Config;
-import io.sentry.Sentry;
-import io.sentry.event.Event;
-import io.sentry.event.EventBuilder;
-import io.sentry.event.interfaces.ExceptionInterface;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
 public final class LogUtils {
 
-    private static final class TagName {
-
-        public static final String EXCEPTION_CLASS = "exception_class";
-        public static final String EXCEPTION_MESSAGE = "exception_message";
-    }
-
-    private static final class ExtraName {
-
-        public static final String INPUT = "input";
-    }
-
     private static final Logger LOGGER = Loggers.getLogger("shadbot");
 
     public static void error(Throwable error, String message, String input) {
         LOGGER.error(String.format("%s (Input: %s)", message, input), error);
-
-        if (!Config.IS_SNAPSHOT) {
-            Sentry.capture(new EventBuilder()
-                    .withRelease(Config.VERSION)
-                    .withLevel(Event.Level.ERROR)
-                    .withSentryInterface(new ExceptionInterface(error))
-                    .withMessage(message)
-                    .withTag(TagName.EXCEPTION_CLASS, error.getClass().getSimpleName())
-                    .withTag(TagName.EXCEPTION_MESSAGE, error.getMessage())
-                    .withExtra(ExtraName.INPUT, input)
-                    .build());
-        }
     }
 
     public static void error(Throwable error, String message) {
         LOGGER.error(message, error);
-
-        if (!Config.IS_SNAPSHOT) {
-            Sentry.capture(new EventBuilder()
-                    .withRelease(Config.VERSION)
-                    .withLevel(Event.Level.ERROR)
-                    .withSentryInterface(new ExceptionInterface(error))
-                    .withMessage(message)
-                    .withTag(TagName.EXCEPTION_CLASS, error.getClass().getSimpleName())
-                    .withTag(TagName.EXCEPTION_MESSAGE, error.getMessage())
-                    .build());
-        }
     }
 
     public static void error(String message) {
         LOGGER.error(message);
-
-        if (!Config.IS_SNAPSHOT) {
-            Sentry.capture(new EventBuilder()
-                    .withRelease(Config.VERSION)
-                    .withLevel(Event.Level.ERROR)
-                    .withMessage(message)
-                    .build());
-        }
     }
 
     public static void warn(String format, Object... args) {
         LOGGER.warn(String.format(format, args));
-
-        if (!Config.IS_SNAPSHOT) {
-            Sentry.capture(new EventBuilder()
-                    .withRelease(Config.VERSION)
-                    .withLevel(Event.Level.WARNING)
-                    .withMessage(String.format(format, args))
-                    .build());
-        }
     }
 
     public static void info(String format, Object... args) {
