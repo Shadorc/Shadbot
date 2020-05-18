82868abb3cfe1116ace40cab330628a876b4d774
David <bi-crosseur@hotmail.fr>
2020-05-18 15:36:39 +0200

    Revert "Send a DM when the bot leaves a server"
    This reverts commit 5fc7e9ff5c8d4673a63fe2b7e07d8b762634490e.

--- src/main/java/com/shadorc/shadbot/listener/MemberLeaveListener.java
+++ src/main/java/com/shadorc/shadbot/listener/MemberLeaveListener.java
@@ -10,15 +10,9 @@ import discord4j.core.event.domain.guild.MemberLeaveEvent;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
-import discord4j.rest.http.client.ClientException;
 import reactor.core.publisher.Mono;
 
 public class MemberLeaveListener implements EventListener<MemberLeaveEvent> {
-
-    private static final String TEXT = String.format("I'm not part of your server anymore, " +
-            "thanks for having tested me!" +
-            "%nIf you encountered an issue or want to let me know what could be improved, " +
-            "do not hesitate to join my support server and tell me! %s", Config.SUPPORT_SERVER_URL);
 
     @Override
     public Class<MemberLeaveEvent> getEventType() {
@@ -27,15 +21,6 @@ public class MemberLeaveListener implements EventListener<MemberLeaveEvent> {
 
     @Override
     public Mono<Void> execute(MemberLeaveEvent event) {
-        if (event.getUser().getId().equals(Shadbot.getSelfId())) {
-            return event.getGuild()
-                    .flatMap(Guild::getOwner)
-                    .flatMap(Member::getPrivateChannel)
-                    .onErrorResume(ClientException.class, err -> Mono.empty())
-                    .flatMap(channel -> DiscordUtils.sendMessage(TEXT, channel))
-                    .then();
-        }
-
         // Delete the member from the database
         final Mono<Void> deleteMember = Mono.justOrEmpty(event.getMember())
                 .flatMap(member -> DatabaseManager.getGuilds()
