4835285975a55d78cf1dfdcd73b4a7ba36ab3097
David <bi-crosseur@hotmail.fr>
2020-07-02 23:01:40 +0200

    Do not send message on guild delete

--- src/main/java/com/shadorc/shadbot/listener/GuildDeleteListener.java
+++ src/main/java/com/shadorc/shadbot/listener/GuildDeleteListener.java
@@ -1,25 +1,14 @@
 package com.shadorc.shadbot.listener;
 
-import com.shadorc.shadbot.cache.CacheManager;
-import com.shadorc.shadbot.data.Config;
 import com.shadorc.shadbot.db.DatabaseManager;
 import com.shadorc.shadbot.db.guilds.entity.DBGuild;
 import com.shadorc.shadbot.music.MusicManager;
-import com.shadorc.shadbot.utils.DiscordUtils;
 import discord4j.core.event.domain.guild.GuildDeleteEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
-import discord4j.rest.http.client.ClientException;
 import reactor.core.publisher.Mono;
 
 import static com.shadorc.shadbot.Shadbot.DEFAULT_LOGGER;
 
 public class GuildDeleteListener implements EventListener<GuildDeleteEvent> {
-
-    private static final String TEXT = String.format("I'm not part of your server anymore, thanks for having tested me!" +
-            "%nIf you encountered an issue or want to let me know what could be improved, " +
-            "do not hesitate to join my support server and tell me!" +
-            "%n%s", Config.SUPPORT_SERVER_URL);
 
     @Override
     public Class<GuildDeleteEvent> getEventType() {
@@ -34,17 +23,8 @@ public class GuildDeleteListener implements EventListener<GuildDeleteEvent> {
                 .getDBGuild(event.getGuildId())
                 .flatMap(DBGuild::delete);
 
-        final Mono<Message> sendMessage = Mono.defer(() ->
-                Mono.justOrEmpty(
-                        CacheManager.getInstance().getGuildOwnersCache().delete(event.getGuildId())))
-                .flatMap(event.getClient()::getUserById)
-                .flatMap(User::getPrivateChannel)
-                .flatMap(channel -> DiscordUtils.sendMessage(TEXT, channel))
-                .onErrorResume(ClientException.class, err -> Mono.empty());
-
         return MusicManager.getInstance().destroyConnection(event.getGuildId())
-                .and(deleteGuild)
-                .and(sendMessage);
+                .and(deleteGuild);
     }
 
 }
