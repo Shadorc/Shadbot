c19fffd329b9d847cb422a0c902def9f3766b0f1
David <bi-crosseur@hotmail.fr>
2020-07-03 12:05:13 +0200

    Trivia: Increase gains

--- src/main/java/com/shadorc/shadbot/command/game/trivia/Constants.java
+++ src/main/java/com/shadorc/shadbot/command/game/trivia/Constants.java
@@ -2,7 +2,7 @@ package com.shadorc.shadbot.command.game.trivia;
 
 class Constants {
 
-    protected static final int MIN_GAINS = 100;
-    protected static final int MAX_BONUS = 150;
+    protected static final int MIN_GAINS = 150;
+    protected static final int MAX_BONUS = 200;
 
 }
