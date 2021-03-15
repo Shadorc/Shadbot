04dc4ed4fe770f0b0b28b9912ec1bd6f9ea6306c
David <bi-crosseur@hotmail.fr>
2021-03-15 22:04:46 +0100

    I18ned WallhavenCmd

--- src/main/java/com/shadorc/shadbot/command/image/WallhavenCmd.java
+++ src/main/java/com/shadorc/shadbot/command/image/WallhavenCmd.java
@@ -5,6 +5,7 @@ import com.shadorc.shadbot.api.json.image.wallhaven.Wallpaper;
 import com.shadorc.shadbot.core.command.BaseCmd;
 import com.shadorc.shadbot.core.command.CommandCategory;
 import com.shadorc.shadbot.core.command.Context;
+import com.shadorc.shadbot.core.setting.Setting;
 import com.shadorc.shadbot.data.credential.Credential;
 import com.shadorc.shadbot.data.credential.CredentialManager;
 import com.shadorc.shadbot.object.Emoji;
@@ -21,62 +22,67 @@ import reactor.function.TupleUtils;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class WallhavenCmd extends BaseCmd {
 
     private static final String HOME_URL = "https://wallhaven.cc/api/v1/search";
+    private static final Pattern COMPILE = Pattern.compile("[, ]");
+
+    private final String apiKey;
 
     public WallhavenCmd() {
         super(CommandCategory.IMAGE, "wallhaven", "Search random wallpaper from Wallhaven");
         this.addOption("query", "Search for a wallpaper", false, ApplicationCommandOptionType.STRING);
+
+        this.apiKey = CredentialManager.getInstance().get(Credential.WALLHAVEN_API_KEY);
     }
 
     @Override
-    public Mono<?> execute(final Context context) {
+    public Mono<?> execute(Context context) {
         final String query = context.getOptionAsString("query").orElse("");
 
-        return context.createFollowupMessage(Emoji.HOURGLASS + " (**%s**) Loading wallpaper...", context.getAuthorName())
-                .flatMap(messageId -> Mono.zip(WallhavenCmd.getWallpaper(query), context.isChannelNsfw())
-                        .flatMap(TupleUtils.function((wallpaper, isNsfw) -> {
-                            if (!"sfw".equals(wallpaper.getPurity()) && !isNsfw) {
-                                return context.editReply(messageId, ShadbotUtil.mustBeNsfw());
-                            }
+        return context.reply(Emoji.HOURGLASS, context.localize("wallhaven.loading"))
+                .then(Mono.zip(this.getWallpaper(query), context.isChannelNsfw()))
+                .flatMap(TupleUtils.function((wallpaper, isNsfw) -> {
+                    if (!"sfw".equals(wallpaper.getPurity()) && !isNsfw) {
+                        return context.editReply(Emoji.GREY_EXCLAMATION,
+                                context.localize("must.be.nsfw").formatted(Setting.NSFW));
+                    }
 
-                            final String title = "Wallpaper: %s".formatted(query.isBlank() ? "random" : query);
-                            return context.editReply(messageId,
-                                    WallhavenCmd.formatEmbed(context.getAuthorAvatar(), title, wallpaper));
-                        }))
-                        .switchIfEmpty(context.editReply(messageId,
-                                Emoji.MAGNIFYING_GLASS + " (**%s**) No wallpapers found matching query `%s`",
-                                context.getAuthorName(), query)));
+                    final String title = context.localize("wallhaven.title")
+                            .formatted(query.isBlank() ? context.localize("wallhaven.random") : query);
+                    return context.editReply(WallhavenCmd.formatEmbed(context, title, wallpaper));
+                }))
+                .switchIfEmpty(context.editReply(Emoji.MAGNIFYING_GLASS,
+                        context.localize("wallhaven.not.found").formatted(query)));
     }
 
-    private static Consumer<EmbedCreateSpec> formatEmbed(final String avatarUrl, final String title, final Wallpaper wallpaper) {
+    private static Consumer<EmbedCreateSpec> formatEmbed(Context context, String title, Wallpaper wallpaper) {
         return ShadbotUtil.getDefaultEmbed(
-                embed -> embed.setAuthor(title, wallpaper.getUrl(), avatarUrl)
+                embed -> embed.setAuthor(title, wallpaper.getUrl(), context.getAuthorAvatar())
                         .setImage(wallpaper.getPath())
-                        .addField("Resolution", wallpaper.getResolution(), false));
+                        .addField(context.localize("wallhaven.resolution"), wallpaper.getResolution(), false));
     }
 
-    private static Mono<Wallpaper> getWallpaper(final String query) {
-        return RequestHelper.fromUrl(WallhavenCmd.buildUrl(query))
+    private Mono<Wallpaper> getWallpaper(final String query) {
+        return RequestHelper.fromUrl(this.buildUrl(query))
                 .to(WallhavenResponse.class)
                 .map(WallhavenResponse::getWallpapers)
                 .filter(Predicate.not(List::isEmpty))
                 .map(RandUtil::randValue);
     }
 
-    private static String buildUrl(final String query) {
+    private String buildUrl(final String query) {
         final StringBuilder urlBuilder = new StringBuilder(HOME_URL);
-        urlBuilder.append(String.format("?apikey=%s",
-                CredentialManager.getInstance().get(Credential.WALLHAVEN_API_KEY)));
+        urlBuilder.append("?apikey=%s".formatted(this.apiKey));
 
         if (query.isBlank()) {
             urlBuilder.append("&sorting=toplist")
                     .append("&purity=100");
         } else {
             final String keywords = FormatUtil.format(
-                    query.split("[, ]"),
+                    COMPILE.split(query),
                     keyword -> "+%s".formatted(NetUtil.encode(keyword.trim())),
                     "");
             urlBuilder.append("&q=%s".formatted(keywords))
