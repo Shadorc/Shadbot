4994aeb51cf94cfe451a81fd37b7bb66ffcfd359
David <bi-crosseur@hotmail.fr>
2021-03-21 23:17:40 +0100

    ThisDay: Cache value

--- src/main/java/com/shadorc/shadbot/command/fun/ThisDayCmd.java
+++ src/main/java/com/shadorc/shadbot/command/fun/ThisDayCmd.java
@@ -8,16 +8,26 @@ import com.shadorc.shadbot.object.Emoji;
 import com.shadorc.shadbot.object.RequestHelper;
 import com.shadorc.shadbot.utils.ShadbotUtil;
 import com.shadorc.shadbot.utils.StringUtil;
+import com.shadorc.shadbot.utils.TimeUtil;
 import discord4j.core.object.Embed;
 import discord4j.core.spec.EmbedCreateSpec;
 import org.jsoup.Jsoup;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.function.Consumer;
 
 class ThisDayCmd extends BaseCmd {
 
     private static final String HOME_URL = "https://www.onthisday.com/";
+
+    private static final Mono<ThisDay> GET_THIS_DAY = RequestHelper.request(HOME_URL)
+            .map(Jsoup::parse)
+            .map(ThisDay::new)
+            .cache(__ -> ThisDayCmd.getNextUpdate(), // Cache value until the next day
+                    __ -> Duration.ZERO, // Do not cache value on error
+                    () -> Duration.ZERO); // Do not cache value on empty
 
     public ThisDayCmd() {
         super(CommandCategory.FUN, "this_day", "Significant events of the day");
@@ -26,7 +36,7 @@ class ThisDayCmd extends BaseCmd {
     @Override
     public Mono<?> execute(Context context) {
         return context.reply(Emoji.HOURGLASS, context.localize("thisday.loading"))
-                .then(ThisDayCmd.getThisDay())
+                .then(GET_THIS_DAY)
                 .flatMap(thisDay -> context.editReply(ThisDayCmd.formatEmbed(context, thisDay)));
     }
 
@@ -38,11 +48,16 @@ class ThisDayCmd extends BaseCmd {
                         .setDescription(StringUtil.abbreviate(thisDay.getEvents(), Embed.MAX_DESCRIPTION_LENGTH)));
     }
 
-    // TODO: Cache the value and clean the cache once a day
-    private static Mono<ThisDay> getThisDay() {
-        return RequestHelper.request(HOME_URL)
-                .map(Jsoup::parse)
-                .map(ThisDay::new);
+    private static Duration getNextUpdate() {
+        ZonedDateTime nextDate = ZonedDateTime.now()
+                .withHour(0)
+                .withMinute(10) // Waits for the website to update, just in case
+                .withSecond(0);
+        if (nextDate.isBefore(ZonedDateTime.now())) {
+            nextDate = nextDate.plusDays(1);
+        }
+
+        return Duration.ofMillis(TimeUtil.elapsed(nextDate.toInstant()));
     }
 
 }
