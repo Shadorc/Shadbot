6e2b63c405fdc4ff01dc24189a68b6092d053116
David <bi-crosseur@hotmail.fr>
2021-03-23 15:19:15 +0100

    CounterStrikeCmd: Implements cache

--- src/main/java/com/shadorc/shadbot/command/gamestats/CounterStrikeCmd.java
+++ src/main/java/com/shadorc/shadbot/command/gamestats/CounterStrikeCmd.java
@@ -9,9 +9,12 @@ import com.shadorc.shadbot.api.json.gamestats.steam.stats.PlayerStats;
 import com.shadorc.shadbot.api.json.gamestats.steam.stats.Stats;
 import com.shadorc.shadbot.api.json.gamestats.steam.stats.UserStatsForGameResponse;
 import com.shadorc.shadbot.command.CommandException;
+import com.shadorc.shadbot.core.cache.MultiValueCache;
 import com.shadorc.shadbot.core.command.BaseCmd;
 import com.shadorc.shadbot.core.command.CommandCategory;
 import com.shadorc.shadbot.core.command.Context;
+import com.shadorc.shadbot.core.i18n.I18nContext;
+import com.shadorc.shadbot.data.Config;
 import com.shadorc.shadbot.data.credential.Credential;
 import com.shadorc.shadbot.data.credential.CredentialManager;
 import com.shadorc.shadbot.object.Emoji;
@@ -32,11 +35,15 @@ import java.util.stream.Collectors;
 class CounterStrikeCmd extends BaseCmd {
 
     private static final String PRIVACY_HELP_URL = "https://support.steampowered.com/kb_article.php?ref=4113-YUDH-6401";
+
+    private static final String RESOLVE_VANITY_URL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/";
     private static final String PLAYER_SUMMARIES_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
     private static final String USER_STATS_FOR_GAME_URL = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/";
-    private static final String RESOLVE_VANITY_URL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/";
 
     private final String apiKey;
+    private final MultiValueCache<String, String> steamIdCache;
+    private final MultiValueCache<String, PlayerSummary> playerSummaryCache;
+    private final MultiValueCache<String, String> userStatsCache;
 
     public CounterStrikeCmd() {
         super(CommandCategory.GAMESTATS, "csgo", "Search for Counter-Strike: Global Offensive statistics");
@@ -44,26 +51,31 @@ class CounterStrikeCmd extends BaseCmd {
                 ApplicationCommandOptionType.STRING);
 
         this.apiKey = CredentialManager.get(Credential.STEAM_API_KEY);
+        this.steamIdCache = MultiValueCache.Builder.<String, String>create().withTtl(Config.CACHE_TTL).build();
+        this.playerSummaryCache = MultiValueCache.Builder.<String, PlayerSummary>create().withTtl(Config.CACHE_TTL).build();
+        this.userStatsCache = MultiValueCache.Builder.<String, String>create().withTtl(Config.CACHE_TTL).build();
     }
 
     @Override
     public Mono<?> execute(Context context) {
         final String steamId = context.getOptionAsString("steamid").orElseThrow();
-        final String identificator = CounterStrikeCmd.getIdentificator(steamId);
+        final String identificator = CounterStrikeCmd.getIdentificator(context, steamId);
 
         return context.reply(Emoji.HOURGLASS, context.localize("cs.loading"))
-                .then(this.getSteamId(identificator))
-                .flatMap(this::getPlayerSummary)
+                .then(this.steamIdCache.getOrCache(identificator, this.getSteamId(identificator)))
+                .flatMap(id -> this.playerSummaryCache.getOrCache(id, this.getPlayerSummary(id)))
                 .flatMap(player -> {
                     if (player.getCommunityVisibilityState() != PlayerSummary.CommunityVisibilityState.PUBLIC) {
                         return context.editReply(Emoji.ACCESS_DENIED,
                                 context.localize("cs.profile.private").formatted(PRIVACY_HELP_URL));
                     }
 
-                    final String userStatsUrl = "%s?appid=730&key=%s&steamid=%s"
-                            .formatted(USER_STATS_FOR_GAME_URL, this.apiKey, player.getSteamId());
+                    final String userStatsUrl = "%s?".formatted(USER_STATS_FOR_GAME_URL)
+                            + "appid=730"
+                            + "&key=%s".formatted(this.apiKey)
+                            + "&steamid=%s".formatted(player.getSteamId());
 
-                    return RequestHelper.request(userStatsUrl)
+                    return this.userStatsCache.getOrCache(userStatsUrl, RequestHelper.request(userStatsUrl))
                             .flatMap(body -> {
                                 if (body.contains("500 Internal Server Error")) {
                                     return context.editReply(Emoji.ACCESS_DENIED,
@@ -83,12 +95,12 @@ class CounterStrikeCmd extends BaseCmd {
                 .switchIfEmpty(context.editReply(Emoji.MAGNIFYING_GLASS, context.localize("cs.player.not.found")));
     }
 
-    private static String getIdentificator(String arg) {
+    private static String getIdentificator(I18nContext context, String arg) {
         // The user provided an URL that can contains a pseudo or an ID
         if (arg.contains("/")) {
             final List<String> splittedUrl = StringUtil.split(arg, "/");
             if (splittedUrl.isEmpty()) {
-                throw new CommandException("Invalid steam ID.");
+                throw new CommandException(context.localize("counterstrike.invalid.id"));
             }
             return splittedUrl.get(splittedUrl.size() - 1);
         } else {
@@ -136,17 +148,17 @@ class CounterStrikeCmd extends BaseCmd {
 
         final int wins = statsMap.get("total_wins");
         final int roundsPlayed = statsMap.get("total_rounds_played");
-        final float win = (float) wins / roundsPlayed * 100;
+        final float winRate = (float) wins / roundsPlayed * 100;
 
         final int mvps = statsMap.get("total_mvps");
         final float timePlayed = statsMap.get("total_time_played") / 3600f;
 
         final int shotsHit = statsMap.get("total_shots_hit");
         final int shotsFired = statsMap.get("total_shots_fired");
-        final float accuracy = (float) shotsHit / shotsFired * 100;
+        final float accuracyRate = (float) shotsHit / shotsFired * 100;
 
         final int killsHeadshot = statsMap.get("total_kills_headshot");
-        final float headshot = (float) killsHeadshot / kills * 100;
+        final float headshotRate = (float) killsHeadshot / kills * 100;
 
         return ShadbotUtil.getDefaultEmbed(
                 embed -> embed.setAuthor(context.localize("cs.title"),
@@ -156,9 +168,9 @@ class CounterStrikeCmd extends BaseCmd {
                         .addField(context.localize("cs.kills"), context.localize(kills), true)
                         .addField(context.localize("cs.playtime"), context.localize(timePlayed), true)
                         .addField(context.localize("cs.mvp"), context.localize(mvps), true)
-                        .addField(context.localize("cs.win"), context.localize(win), true)
-                        .addField(context.localize("cs.accuracy"), context.localize(accuracy), true)
-                        .addField(context.localize("cs.headshot"), context.localize(headshot), true)
+                        .addField(context.localize("cs.win"), context.localize(winRate), true)
+                        .addField(context.localize("cs.accuracy"), context.localize(accuracyRate), true)
+                        .addField(context.localize("cs.headshot"), context.localize(headshotRate), true)
                         .addField(context.localize("cs.ratio"), context.localize(ratio), true));
     }
 
