8c2fbcc706fd96b843fbc6d1bf20c24ac56ca224
David <bi-crosseur@hotmail.fr>
2021-04-06 14:35:40 +0200

    Update GameGroup.java

--- src/main/java/com/shadorc/shadbot/command/game/GameGroup.java
+++ src/main/java/com/shadorc/shadbot/command/game/GameGroup.java
@@ -1,6 +1,9 @@
 package com.shadorc.shadbot.command.game;
 
+import com.shadorc.shadbot.command.game.blackjack.BlackjackCmd;
+import com.shadorc.shadbot.command.game.dice.DiceCmd;
 import com.shadorc.shadbot.command.game.hangman.HangmanCmd;
+import com.shadorc.shadbot.command.game.lottery.LotteryCmd;
 import com.shadorc.shadbot.command.game.roulette.RouletteCmd;
 import com.shadorc.shadbot.command.game.rps.RpsCmd;
 import com.shadorc.shadbot.command.game.russianroulette.RussianRouletteCmd;
@@ -16,7 +19,7 @@ public class GameGroup extends BaseCmdGroup {
     public GameGroup() {
         super(CommandCategory.GAME, "game", "Game commands",
                 List.of(new RpsCmd(), new RussianRouletteCmd(), new SlotMachineCmd(), new TriviaCmd(),
-                        new HangmanCmd(), new RouletteCmd()));
+                        new HangmanCmd(), new RouletteCmd(), new BlackjackCmd(), new DiceCmd(), new LotteryCmd()));
     }
 
 }
