ecb9e36322d5a75e7664622ef692db0c657308f0
David <bi-crosseur@hotmail.fr>
2021-04-29 17:28:40 +0200

    SettingShow: Fix and translate

--- src/main/java/com/shadorc/shadbot/command/setting/SettingShow.java
+++ src/main/java/com/shadorc/shadbot/command/setting/SettingShow.java
@@ -23,80 +23,92 @@ import java.util.function.Function;
 public class SettingShow extends BaseCmd {
 
     public SettingShow() {
-        super(CommandCategory.ADMIN, CommandPermission.USER, "show", "Show current settings");
+        super(CommandCategory.ADMIN, CommandPermission.USER,
+                "show", "Show current settings");
     }
 
     @Override
     public Mono<?> execute(Context context) {
         final Settings settings = context.getDbGuild().getSettings();
 
-        final Optional<Integer> defaultVolume = settings.getDefaultVol();
-        final Mono<Optional<Tuple2<Channel, String>>> getAutoJoinMessage = Mono.justOrEmpty(settings.getAutoJoinMessage())
-                .flatMap(autoMessage -> context.getClient().getChannelById(autoMessage.getChannelId())
-                        .zipWith(Mono.just(autoMessage.getMessage())))
-                .map(Optional::of)
-                .defaultIfEmpty(Optional.empty());
-        final Mono<Optional<Tuple2<Channel, String>>> getAutoLeaveMessage = Mono.justOrEmpty(settings.getAutoLeaveMessage())
-                .flatMap(autoMessage -> context.getClient().getChannelById(autoMessage.getChannelId())
-                        .zipWith(Mono.just(autoMessage.getMessage())))
-                .map(Optional::of)
-                .defaultIfEmpty(Optional.empty());
         final Set<String> blacklistedCmds = settings.getBlacklistedCmds();
-        final Flux<Role> getAllowedRoles = Flux.fromIterable(settings.getAllowedRoleIds())
-                .flatMap(id -> context.getClient().getRoleById(context.getGuildId(), id));
-        final Flux<Role> getAutoRoles = Flux.fromIterable(settings.getAutoRoleIds())
-                .flatMap(id -> context.getClient().getRoleById(context.getGuildId(), id));
-        final Mono<Map<Channel, Set<BaseCmd>>> getRestrictedChannels = Flux.fromIterable(settings.getRestrictedChannels().entrySet())
-                .flatMap(entry -> context.getClient().getChannelById(entry.getKey())
-                        .zipWith(Mono.just(entry.getValue())))
-                .collectMap(Tuple2::getT1, Tuple2::getT2);
-        final Mono<Map<Role, Set<BaseCmd>>> getRestrictedRoles = Flux.fromIterable(settings.getRestrictedRoles().entrySet())
-                .flatMap(entry -> context.getClient().getRoleById(context.getGuildId(), entry.getKey())
-                        .zipWith(Mono.just(entry.getValue())))
-                .collectMap(Tuple2::getT1, Tuple2::getT2);
+        final Optional<Integer> defaultVolume = settings.getDefaultVol();
+        final Mono<Optional<Tuple2<Channel, String>>> getAutoJoinMessage =
+                Mono.justOrEmpty(settings.getAutoJoinMessage())
+                        .flatMap(autoMessage -> context.getClient().getChannelById(autoMessage.getChannelId())
+                                .zipWith(Mono.just(autoMessage.getMessage())))
+                        .map(Optional::of)
+                        .defaultIfEmpty(Optional.empty());
+        final Mono<Optional<Tuple2<Channel, String>>> getAutoLeaveMessage =
+                Mono.justOrEmpty(settings.getAutoLeaveMessage())
+                        .flatMap(autoMessage -> context.getClient().getChannelById(autoMessage.getChannelId())
+                                .zipWith(Mono.just(autoMessage.getMessage())))
+                        .map(Optional::of)
+                        .defaultIfEmpty(Optional.empty());
+        final Mono<List<Role>> getAllowedRoles =
+                Flux.fromIterable(settings.getAllowedRoleIds())
+                        .flatMap(id -> context.getClient().getRoleById(context.getGuildId(), id))
+                        .collectList();
+        final Mono<List<Role>> getAutoRoles =
+                Flux.fromIterable(settings.getAutoRoleIds())
+                        .flatMap(id -> context.getClient().getRoleById(context.getGuildId(), id))
+                        .collectList();
+        final Mono<Map<Channel, Set<BaseCmd>>> getRestrictedChannels =
+                Flux.fromIterable(settings.getRestrictedChannels().entrySet())
+                        .flatMap(entry -> context.getClient().getChannelById(entry.getKey())
+                                .zipWith(Mono.just(entry.getValue())))
+                        .filter(tuple -> !tuple.getT2().isEmpty())
+                        .collectMap(Tuple2::getT1, Tuple2::getT2);
+        final Mono<Map<Role, Set<BaseCmd>>> getRestrictedRoles =
+                Flux.fromIterable(settings.getRestrictedRoles().entrySet())
+                        .flatMap(entry -> context.getClient().getRoleById(context.getGuildId(), entry.getKey())
+                                .zipWith(Mono.just(entry.getValue())))
+                        .filter(tuple -> !tuple.getT2().isEmpty())
+                        .collectMap(Tuple2::getT1, Tuple2::getT2);
 
         return Mono.zip(
-                getAllowedRoles.collectList(),
-                getAutoRoles.collectList(),
+                getAllowedRoles,
+                getAutoRoles,
                 getRestrictedChannels,
                 getRestrictedRoles,
                 getAutoJoinMessage,
                 getAutoLeaveMessage)
                 .map(TupleUtils.function((allowedRoles, autoRoles, restrictedChannels, restrictedRoles, autoJoinMessage, autoLeaveMessage) ->
                         ShadbotUtil.getDefaultEmbed(embed -> {
-                            embed.setAuthor("Shadbot Settings", "https://github.com/Shadorc/Shadbot/wiki/Settings",
+                            embed.setAuthor(context.localize("settings.title"),
+                                    "https://github.com/Shadorc/Shadbot/wiki/Settings",
                                     context.getAuthorAvatar());
-                            embed.setFooter("Documentation available by clicking on the title", null);
+                            embed.setFooter(context.localize("settings.footer"), null);
 
                             defaultVolume.ifPresent(volume ->
-                                    embed.addField("Default volume", "%d%%".formatted(volume), false));
+                                    embed.addField(context.localize("settings.volume"), "%d%%".formatted(volume), false));
                             autoJoinMessage.ifPresent(it ->
-                                    embed.addField("Auto join message", "%s\n%s"
+                                    embed.addField(context.localize("settings.auto.join.message"), "%s\n%s"
                                             .formatted(it.getT1().getMention(), it.getT2()), false));
                             autoLeaveMessage.ifPresent(it ->
-                                    embed.addField("Auto leave message", "%s\n%s"
+                                    embed.addField(context.localize("settings.auto.leave.message"), "%s\n%s"
                                             .formatted(it.getT1().getMention(), it.getT2()), false));
                             if (!blacklistedCmds.isEmpty()) {
-                                embed.addField("Blacklisted commands",
+                                embed.addField(context.localize("settings.blacklist"),
                                         FormatUtil.format(blacklistedCmds, Function.identity(), "\n"), false);
                             }
                             if (!allowedRoles.isEmpty()) {
-                                embed.addField("Allowed roles",
+                                embed.addField(context.localize("settings.allowed.roles"),
                                         FormatUtil.format(allowedRoles, Role::getMention, "\n"), false);
                             }
                             if (!autoRoles.isEmpty()) {
-                                embed.addField("Auto-roles",
+                                embed.addField(context.localize("settings.auto.roles"),
                                         FormatUtil.format(autoRoles, Role::getMention, "\n"), false);
                             }
                             if (!restrictedChannels.isEmpty()) {
-                                embed.addField("Restricted channels",
+                                embed.addField(context.localize("settings.restricted.channels"),
                                         FormatUtil.format(restrictedChannels.entrySet(),
                                                 entry -> FormatUtil.format(entry.getValue(), BaseCmd::getName, "\n - "),
                                                 "\n"),
                                         false);
                             }
                             if (!restrictedRoles.isEmpty()) {
-                                embed.addField("Restricted roles",
+                                embed.addField(context.localize("settings.restricted.roles"),
                                         FormatUtil.format(restrictedRoles.entrySet(),
                                                 entry -> FormatUtil.format(entry.getValue(), BaseCmd::getName, "\n - "),
                                                 "\n"),
@@ -104,7 +116,7 @@ public class SettingShow extends BaseCmd {
                             }
 
                             if (embed.asRequest().fields().toOptional().map(List::isEmpty).orElse(true)) {
-                                embed.setDescription("There is no settings currently set.");
+                                embed.setDescription(context.localize("settings.none"));
                             }
                         })))
                 .flatMap(context::reply);
