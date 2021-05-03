7820a2e9298a9a4b4023f8a3907e3f94438ee3be
David <bi-crosseur@hotmail.fr>
2021-05-03 10:44:17 +0200

    MessageCreateListener: Do not warm guilds multiple times

--- src/main/java/com/shadorc/shadbot/listener/MessageCreateListener.java
+++ src/main/java/com/shadorc/shadbot/listener/MessageCreateListener.java
@@ -8,13 +8,17 @@ import com.shadorc.shadbot.utils.DiscordUtil;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import reactor.core.publisher.Mono;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import static com.shadorc.shadbot.Shadbot.DEFAULT_LOGGER;
 
 @Deprecated
 public class MessageCreateListener implements EventListener<MessageCreateEvent> {
+
+    private static final Set<Long> WARNED_GUILDS = new HashSet<>();
 
     @Override
     public Class<MessageCreateEvent> getEventType() {
@@ -24,20 +28,24 @@ public class MessageCreateListener implements EventListener<MessageCreateEvent> 
     @Override
     public Mono<?> execute(MessageCreateEvent event) {
         return Mono.justOrEmpty(event.getGuildId())
-                .flatMap(guildId -> DatabaseManager.getGuilds().getDBGuild(guildId))
-                .filter(dbGuild -> {
-                    final Optional<String> prefixOpt = dbGuild.getSettings().getPrefix();
-                    final String content = event.getMessage().getContent();
-                    if (prefixOpt.isPresent() && MessageCreateListener.isCommand(prefixOpt.orElseThrow(), content)) {
-                        return true;
-                    }
-                    return MessageCreateListener.isCommand("/", content);
-                })
-                .flatMap(dbGuild -> event.getMessage().getChannel()
-                        .flatMap(channel -> DiscordUtil.sendMessage(Emoji.WARNING,
-                                I18nManager.localize(dbGuild.getLocale(), "warning.slash.commands"), channel))
-                        .doOnNext(message -> DEFAULT_LOGGER.info("{Guild ID: {}} Warning sent about message create event deprecation",
-                                event.getGuildId().orElseThrow())));
+                .filter(guildId -> !WARNED_GUILDS.contains(guildId.asLong()))
+                .flatMap(guildId -> DatabaseManager.getGuilds().getDBGuild(guildId)
+                        .filter(dbGuild -> {
+                            final Optional<String> prefixOpt = dbGuild.getSettings().getPrefix();
+                            final String content = event.getMessage().getContent();
+                            if (prefixOpt.isPresent() && MessageCreateListener.isCommand(prefixOpt.orElseThrow(), content)) {
+                                return true;
+                            }
+                            return MessageCreateListener.isCommand("/", content);
+                        })
+                        .flatMap(dbGuild -> event.getMessage().getChannel()
+                                .flatMap(channel -> DiscordUtil.sendMessage(Emoji.WARNING,
+                                        I18nManager.localize(dbGuild.getLocale(), "warning.slash.commands"), channel))
+                                .doOnNext(message -> {
+                                    WARNED_GUILDS.add(guildId.asLong());
+                                    DEFAULT_LOGGER.info("{Guild ID: {}} Warning sent about message create event deprecation",
+                                            guildId.asString());
+                                })));
     }
 
     private static boolean isCommand(String prefix, String content) {
