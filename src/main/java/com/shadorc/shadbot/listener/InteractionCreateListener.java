03dc8db42da8dcbaf8c544c782f451921b72ab79
David <bi-crosseur@hotmail.fr>
2021-05-03 20:54:39 +0200

    InteractionCreateListener: Send message when permissions are missing

--- src/main/java/com/shadorc/shadbot/listener/InteractionCreateListener.java
+++ src/main/java/com/shadorc/shadbot/listener/InteractionCreateListener.java
@@ -6,6 +6,8 @@ import com.shadorc.shadbot.core.i18n.I18nManager;
 import com.shadorc.shadbot.data.Config;
 import com.shadorc.shadbot.data.Telemetry;
 import com.shadorc.shadbot.database.DatabaseManager;
+import com.shadorc.shadbot.object.Emoji;
+import com.shadorc.shadbot.utils.ReactorUtil;
 import discord4j.core.event.domain.InteractionCreateEvent;
 import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.rest.util.Permission;
@@ -26,10 +28,15 @@ public class InteractionCreateListener implements EventListener<InteractionCreat
         if (event.getInteraction().getGuildId().isEmpty()) {
             return event.reply(I18nManager.localize(Config.DEFAULT_LOCALE, "interaction.dm"));
         }
+
         return event.getInteraction().getChannel()
-                .cast(TextChannel.class)
+                .ofType(TextChannel.class)
                 .flatMap(channel -> channel.getEffectivePermissions(event.getClient().getSelfId()))
-                .filter(permissions -> permissions.contains(Permission.SEND_MESSAGES) && permissions.contains(Permission.VIEW_CHANNEL))
+                .filterWhen(ReactorUtil.filterOrExecute(
+                        permissions -> permissions.contains(Permission.SEND_MESSAGES)
+                                && permissions.contains(Permission.VIEW_CHANNEL),
+                        event.replyEphemeral(Emoji.RED_CROSS
+                                + I18nManager.localize(Config.DEFAULT_LOCALE, "interaction.missing.permissions"))))
                 .flatMap(__ -> Mono.justOrEmpty(event.getInteraction().getGuildId()))
                 .flatMap(guildId -> DatabaseManager.getGuilds().getDBGuild(guildId))
                 .map(dbGuild -> new Context(event, dbGuild))
