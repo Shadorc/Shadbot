311cf22c12144804bc5a026aefe0cc83ee9062d1
David <bi-crosseur@hotmail.fr>
2021-05-05 17:06:06 +0200

    Remove unused settings

--- src/main/java/com/shadorc/shadbot/core/command/Setting.java
+++ src/main/java/com/shadorc/shadbot/core/command/Setting.java
@@ -2,26 +2,11 @@ package com.shadorc.shadbot.core.command;
 
 public enum Setting {
 
-    // TODO Clean-up: Deprecated
-
-    @Deprecated
-    PREFIX, // Removed
     DEFAULT_VOLUME,
-    NSFW,
-    @Deprecated
-    ALLOWED_CHANNELS, // Removed
     ALLOWED_TEXT_CHANNELS,
     ALLOWED_VOICE_CHANNELS,
     ALLOWED_ROLES,
     BLACKLIST,
-    @Deprecated
-    AUTO_MESSAGE, // Removed
-    @Deprecated
-    MESSAGE_CHANNEL_ID, // Removed
-    @Deprecated
-    JOIN_MESSAGE, // Replaced by AUTO_JOIN_MESSAGE
-    @Deprecated
-    LEAVE_MESSAGE, // Replaced by AUTO_LEAVE_MESSAGE
     AUTO_ROLES,
     IAM_MESSAGES,
     RESTRICTED_CHANNELS,
