65fbc13ff513ad43a3ec51d3eb96858373882b59
David <bi-crosseur@hotmail.fr>
2021-05-05 20:29:16 +0200

    Update RestrictedChannelsSetting on TextChannelDeleteEvent

--- src/main/java/com/shadorc/shadbot/listener/ChannelDeleteListener.java
+++ src/main/java/com/shadorc/shadbot/listener/ChannelDeleteListener.java
@@ -1,5 +1,6 @@
 package com.shadorc.shadbot.listener;
 
+import com.shadorc.shadbot.core.command.BaseCmd;
 import com.shadorc.shadbot.core.command.Setting;
 import com.shadorc.shadbot.database.DatabaseManager;
 import discord4j.common.util.Snowflake;
@@ -7,7 +8,9 @@ import discord4j.core.event.domain.channel.TextChannelDeleteEvent;
 import discord4j.core.event.domain.channel.VoiceChannelDeleteEvent;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ChannelDeleteListener {
 
@@ -46,13 +49,35 @@ public class ChannelDeleteListener {
             return DatabaseManager.getGuilds()
                     .getDBGuild(event.getChannel().getGuildId())
                     .flatMap(dbGuild -> {
+                        final Snowflake channelId = event.getChannel().getId();
+                        Mono<Void> request = Mono.empty();
+
+                        final var restrictedChannels = dbGuild.getSettings().getRestrictedChannels();
+                        // If the channel was a restricted channel...
+                        if (restrictedChannels.containsKey(channelId)) {
+                            // ...update settings to remove the deleted one
+                            restrictedChannels.get(channelId).clear();
+
+                            final var restrictedChannelsSeralized = restrictedChannels.entrySet().stream()
+                                    .collect(Collectors.toUnmodifiableMap(
+                                            entry -> entry.getKey().asString(),
+                                            entry -> entry.getValue().stream()
+                                                    .map(BaseCmd::getName)
+                                                    .collect(Collectors.toUnmodifiableSet())));
+
+                            request = request
+                                    .and(dbGuild.updateSetting(Setting.RESTRICTED_CHANNELS, restrictedChannelsSeralized));
+                        }
+
                         final Set<Snowflake> allowedTextChannelIds = dbGuild.getSettings().getAllowedTextChannelIds();
                         // If the channel was an allowed channel...
-                        if (allowedTextChannelIds.remove(event.getChannel().getId())) {
+                        if (allowedTextChannelIds.remove(channelId)) {
                             // ...update settings to remove the deleted one
-                            return dbGuild.updateSetting(Setting.ALLOWED_TEXT_CHANNELS, allowedTextChannelIds);
+                            request = request
+                                    .and(dbGuild.updateSetting(Setting.ALLOWED_TEXT_CHANNELS, allowedTextChannelIds));
                         }
-                        return Mono.empty();
+
+                        return request;
                     });
         }
     }
