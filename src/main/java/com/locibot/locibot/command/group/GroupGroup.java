cb020fc6f1b739ea416215cc29951de94defdf05
LokiStar <locistar.got@gmail.com>
2021-05-20 14:06:16 +0200

    GroupGroup added

--- src/main/java/com/locibot/locibot/command/group/GroupGroup.java
+++ src/main/java/com/locibot/locibot/command/group/GroupGroup.java
@@ -1,6 +1,7 @@
 package com.locibot.locibot.command.group;
 
-import com.locibot.locibot.core.command.BaseCmd;
+import com.locibot.locibot.command.group.amongUs.AmongUs;
+import com.locibot.locibot.command.group.clash.Clash;
 import com.locibot.locibot.core.command.BaseCmdGroup;
 import com.locibot.locibot.core.command.CommandCategory;
 import com.locibot.locibot.core.command.CommandPermission;
@@ -9,6 +10,6 @@ import java.util.List;
 
 public class GroupGroup extends BaseCmdGroup {
     public GroupGroup() {
-        super(CommandCategory.FUN, CommandPermission.USER_GUILD, "group", "Group Commands", List.of(new Create()));
+        super(CommandCategory.FUN, CommandPermission.USER_GUILD, "group", "Group Commands", List.of(new Clash(), new AmongUs()));
     }
 }
