fdd8c2d5b4cb95888e80508ae3cca8e38809c846
LokiStar <locistar.got@gmail.com>
2021-05-20 14:06:01 +0200

    GroupCreateTemplate added, to create different Groups

--- src/main/java/com/locibot/locibot/command/group/Create.java
+++ src/main/java/com/locibot/locibot/command/group/Create.java
@@ -9,35 +9,62 @@ import com.locibot.locibot.database.groups.GroupsCollection;
 import com.locibot.locibot.database.groups.entity.DBGroup;
 import com.locibot.locibot.database.groups.entity.DBGroupMember;
 import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.User;
 import discord4j.rest.util.ApplicationCommandOptionType;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Create extends BaseCmd {
-    protected Create() {
-        super(CommandCategory.FUN, "create_clash", "create a group");
+public abstract class Create extends BaseCmd {
+
+    private int min;
+    private int opt;
+    private int groupType;
+
+    /**
+     *
+     * @param min count of minimum required members
+     * @param opt count of optional members
+     * @param name command name
+     * @param description command description
+     */
+    protected Create(int min, int opt, String name, String description, int groupType) {
+        super(CommandCategory.FUN, name, description);
+        this.min = min;
+        this.opt = opt;
+        this.groupType = groupType;
         this.addOption("team_name", "Give us a Name", true, ApplicationCommandOptionType.STRING);
-        this.addOption("member1", "User", true, ApplicationCommandOptionType.USER);
-        this.addOption("member2", "User", true, ApplicationCommandOptionType.USER);
-        this.addOption("member3", "User", true, ApplicationCommandOptionType.USER);
-        this.addOption("member4", "User", true, ApplicationCommandOptionType.USER);
-        this.addOption("optional_member1", "User", false, ApplicationCommandOptionType.USER);
-        this.addOption("optional_member2", "User", false, ApplicationCommandOptionType.USER);
-        this.addOption("optional_member3", "User", false, ApplicationCommandOptionType.USER);
-        this.addOption("optional_member4", "User", false, ApplicationCommandOptionType.USER);
-        this.addOption("optional_member5", "User", false, ApplicationCommandOptionType.USER);
+        for (int i = 0; i < min; i++) {
+            this.addOption("member_" + i, "User", true, ApplicationCommandOptionType.USER);
+        }
+        for (int i = 0; i < opt; i++) {
+            this.addOption("optional_member_" + i, "User", false, ApplicationCommandOptionType.USER);
+        }
     }
 
     @Override
     public Mono<?> execute(Context context) {
-
-
         String groupName = context.getOptionAsString("team_name").get();
-        DBGroup group = new DBGroup(groupName);
-        Snowflake member1 = context.getOptionAsUser("member1").block().getId();
-        DBGroupMember groupMember = new DBGroupMember(member1 ,groupName);
-        return context.createFollowupMessage("Created").then(group.insert().then(groupMember.insert()));
+        DBGroup group = new DBGroup(groupName, groupType);
+        group.insert().block();
+        List<DBGroupMember> dbGroupMembers = new ArrayList<>();
+
+        dbGroupMembers.add(new DBGroupMember(context.getEvent().getInteraction().getUser().getId(), groupName, false, false, 2, true));
+
+        for (int i = 0; i < min; i++) {
+            Snowflake member = context.getOptionAsUser("member_" + i).block().getId();
+            dbGroupMembers.add(new DBGroupMember(member, groupName, false, false, 0, false));
+        }
+        for (int i = 0; i < opt; i++) {
+            User user = context.getOptionAsUser("optional_member_" + i).block();
+            if (user != null){
+                dbGroupMembers.add(new DBGroupMember(user.getId(), groupName, true, false, 0, false));
+            }
+        }
+        dbGroupMembers.forEach(dbGroupMember -> dbGroupMember.insert().block());
+
+        return context.createFollowupMessage("Created");
     }
 }
