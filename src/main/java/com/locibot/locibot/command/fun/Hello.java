740eacde96156e9b3c97176380f71b5767ea5c47
LokiStar <locistar.got@gmail.com>
2021-05-20 12:08:27 +0200

    Hello command added

---
+++ src/main/java/com/locibot/locibot/command/fun/Hello.java
@@ -0,0 +1,23 @@
+package com.locibot.locibot.command.fun;
+
+import com.locibot.locibot.core.command.BaseCmd;
+import com.locibot.locibot.core.command.CommandCategory;
+import com.locibot.locibot.core.command.CommandPermission;
+import com.locibot.locibot.core.command.Context;
+import com.locibot.locibot.database.DatabaseManager;
+import com.locibot.locibot.database.groups.entity.DBGroup;
+import com.locibot.locibot.object.Emoji;
+import discord4j.rest.util.ApplicationCommandOptionType;
+import reactor.core.publisher.Mono;
+
+public class Hello extends BaseCmd {
+    public Hello() {
+        super(CommandCategory.FUN, CommandPermission.USER_GLOBAL, "hello", "You might get greeted", ApplicationCommandOptionType.STRING);
+    }
+
+    @Override
+    public Mono<?> execute(Context context) {
+        DBGroup group = new DBGroup("TEST");
+        return context.getEvent().getInteractionResponse().createFollowupMessage("Hello").then(group.insert()).log();
+    }
+}
