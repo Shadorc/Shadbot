19d1bf7597a507e3cb275e1b67c71797bfdbfea6
LokiStar <locistar.got@gmail.com>
2021-05-27 11:52:35 +0200

    variable min_required added

--- src/main/java/com/locibot/locibot/command/group/GroupType.java
+++ src/main/java/com/locibot/locibot/command/group/GroupType.java
@@ -1,19 +1,21 @@
 package com.locibot.locibot.command.group;
 
 public enum GroupType {
-    DEFAULT("Test", "https://img.icons8.com/ios-filled/344/placeholder-thumbnail-xml.png", false), //DB-TeamType = 0
-    CLASH("clash", "https://img.icons8.com/doodle/344/league-of-legends.png", false),              //DB-TeamType = 1
-    AMONGUS("AmongUs", "https://img.icons8.com/doodle/344/among-us.png", false),                   //DB-TeamType = 2
-    VALORANT("Valorant", "https://img.icons8.com/doodle/344/valorant.png", true);                 //DB-TeamType = 3
+    DEFAULT("Test", "https://img.icons8.com/ios-filled/344/placeholder-thumbnail-xml.png", false, 1), //DB-TeamType = 0
+    CLASH("clash", "https://img.icons8.com/doodle/344/league-of-legends.png", false, 4),              //DB-TeamType = 1
+    AMONGUS("AmongUs", "https://img.icons8.com/doodle/344/among-us.png", false, 2),                   //DB-TeamType = 2
+    VALORANT("Valorant", "https://img.icons8.com/doodle/344/valorant.png", true, 1);                 //DB-TeamType = 3
 
     private final String name;
     private final String url;
     private final boolean inviteOptional;
+    private final int min_required;
 
-    GroupType(String name, String url, boolean inviteOptional) {
+    GroupType(String name, String url, boolean inviteOptional, int min_required) {
         this.name = name;
         this.url = url;
         this.inviteOptional = inviteOptional;
+        this.min_required = min_required;
     }
 
     public String getName() {
@@ -26,5 +28,9 @@ public enum GroupType {
 
     public boolean isInviteOptional() {
         return inviteOptional;
+    }
+
+    public int getMin_required() {
+        return min_required;
     }
 }
